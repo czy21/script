e0fc8e7207abcfabebb97d3a2cec3183219bab48
Bruce CHEN <805899926@qq.com>
2019-12-10 16:04:33 +0800

    Tmp

--- server/docker/compose/jenkins/config.sh
+++ server/docker/compose/jenkins/config.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
 set -e
+
+sudo mkdir -p /data/volumes/jenkins/
+
+sudo tee /data/volumes/jenkins/hudson.model.UpdateCenter.xml <<-'EOF'
+<?xml version='1.1' encoding='UTF-8'?>
+<sites>
+  <site>
+    <id>default</id>
+    <url>https://mirrors.tuna.tsinghua.edu.cn/jenkins/updates/update-center.json</url>
+  </site>
+</sites>
+EOF
