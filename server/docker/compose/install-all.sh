a99609099251027c7c104a3930d812599988daf6
Bruce CHEN <805899926@qq.com>
2019-12-12 22:26:40 +0800

    improve

--- server/docker/compose/install-all.sh
+++ server/docker/compose/install-all.sh
@@ -4,12 +4,12 @@
 # $2: --upload | --exec
 # $3: --config
 
-# ./install-all.sh user@host --upload --all
+# ./install-all.sh user@host --upload --config
 
 case "$2" in
         --upload)
         scp -r ././../compose/ $1:
-        ssh $1 '$HOME/compose/install-all.sh cent_a --exec '$3';'
+        ssh $1 '$HOME/compose/install-all.sh '$2' --exec '$3';'
         ;;
         --exec)
         for t in `ls -ld $HOME/compose/* | grep "^d" | awk '{print $9}'`
@@ -17,7 +17,7 @@ case "$2" in
           config_file=${t}/config.sh
           if [[ -f ${config_file} ]] && [[ $3 == '--config' ]]; then
               echo -e "\033[32m executing => ${config_file}\n\033[0m"
-              sudo ${config_file}
+              sudo sh ${config_file}
           fi
           compose_file=${t}/docker-compose.yml
           if [[ -f ${compose_file} ]];then
---
+++ server/docker/compose/portainer/config.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo mkdir -p /data/config/portainer/
+
+sudo echo -n Czy.190878 > /data/config/portainer/portainer_password
