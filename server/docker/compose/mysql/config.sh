b5700467ed0d97d49a9de613e3ae209f94930c42
Bruce CHEN <805899926@qq.com>
2019-12-22 18:40:16 +0800

    Tmp

--- server/docker/compose/mysql/config.sh
+++ server/docker/compose/mysql/config.sh
@@ -2,31 +2,23 @@
 
 set -e
 
-sudo mkdir -p /data/config/mysql/
+sudo mkdir -p /data/config/mysql/conf.d/
 
-sudo tee /data/config/mysql/mysql.cnf <<-'EOF'
+sudo tee /data/config/mysql/conf.d/mysql.cnf <<-'EOF'
 [client]
-default-character-set=utf8
+default-character-set=utf8mb4
 [mysql]
-default-character-set=utf8
+default-character-set=utf8mb4
 EOF
 
-sudo tee /data/config/mysql/mysqld.cnf <<-'EOF'
+sudo tee /data/config/mysql/my.cnf <<-'EOF'
 [mysqld]
-default-time-zone='+8:00'
-character-set-server=utf8
-default-storage-engine=INNODB
-lower_case_table_names=1
-pid-file  = /var/run/mysqld/mysqld.pid
-socket    = /var/run/mysqld/mysqld.sock
-datadir   = /var/lib/mysql
-sql_mode=STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION
-log-output=FILE
-general-log=1
-general_log_file="general.log"
-slow-query-log=1
-slow_query_log_file="slow.log"
-log-error="error.err"
-long_query_time=10
+pid-file        = /var/run/mysqld/mysqld.pid
+socket          = /var/run/mysqld/mysqld.sock
+datadir         = /var/lib/mysql
+default-time-zone = '+8:00'
+secure-file-priv= NULL
 symbolic-links=0
+# Custom config should go here
+!includedir /etc/mysql/conf.d/
 EOF
