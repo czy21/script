4ae577891b38ac62fda0fe306cd2f2fc2abc3a94
Bruce CHEN <805899926@qq.com>
2020-03-12 22:27:43 +0800

    tmp

--- other/mongo.sh
+++ other/mongo.sh
@@ -4,3 +4,5 @@ mongodump.exe --host 127.0.0.1 --port 30701 --db erp --gzip --archive=/d/Develop
 
 # 还原数据库
 mongorestore.exe --host 127.0.0.1 --port 30701 --db erp --gzip --archive=/d/Developer/JavaProject/erp/_temp/db_bak/mongo.gz
+
+#ss
