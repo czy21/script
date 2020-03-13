0ec472aa0259b637b8c4d738d622770a0a7d443d
Bruce CHEN <805899926@qq.com>
2020-03-13 23:21:48 +0800

    #Bruce update centos to 8

--- server/docker/docker-install.sh
+++ server/docker/docker-install.sh
@@ -11,15 +11,15 @@ sudo tee /etc/docker/daemon.json <<-'EOF'
 EOF
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-sudo yum makecache fast
-sudo yum -y install docker-ce
+sudo yum makecache timer
+sudo yum -y install docker-ce --nobest
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
  # docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # docker machine
 sudo curl -L "https://github.com/docker/machine/releases/download/v0.16.0/docker-machine-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-machine
