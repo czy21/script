fc1c9169542056a71f40327761dd871b0fba30df
Bruce CHEN <805899926@qq.com>
2020-03-15 22:10:23 +0800

    #Bruce Improve openwrt shell

---
+++ openwrt/machine-ubuntu-dev.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install -y subversion build-essential libncurses5-dev zlib1g-dev gawk git ccache gettext libssl-dev xsltproc zip
---
+++ server/init-machine-ubuntu-dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 请先给ubuntu root账户设置密码
+# linux version19.10
+# must as root
+
+groupadd wheel
+echo "%wheel ALL=(ALL:ALL)  NOPASSWD:ALL" >> /etc/sudoers
+
+usermod -aG wheel bruce
+su bruce bash -c "set -e;cd;mkdir .ssh;chmod 700 .ssh;echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3nTRJ/aVb67l1xMaN36jmIbabU7Hiv/xpZ8bwLVvNO3Bj7kUzYTp7DIbPcHQg4d6EsPC6j91E8zW6CrV2fo2Ai8tDO/rCq9Se/64F3+8oEIiI6E/OfUZfXD1mPbG7M/kcA3VeQP6wxNPhWBbKRisqgUc6VTKhl+hK6LwRTZgeShxSNcey+HZst52wJxjQkNG+7CAEY5bbmBzAlHCSl4Z0RftYTHR3q8LcEg7YLNZasUogX68kBgRrb+jw1pRMNo7o7RI9xliDAGX+E4C3vVZL0IsccKgr90222axsADoEjC9O+Q6uwKjahemOVaau+9sHIwkelcOcCzW5SuAwkezv 805899926@qq.com > .ssh/authorized_keys;chmod 644 .ssh/authorized_keys"
+shutdown -r now
