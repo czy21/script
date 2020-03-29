c99891bff3cc864af5620af3b490d708b054685d
Bruce CHEN <805899926@qq.com>
2020-03-29 21:44:54 +0800

    #Bruce Improve

--- openwrt/builder/docker-ubuntu-dev.sh
+++ openwrt/builder/docker-ubuntu-dev.sh
@@ -3,12 +3,8 @@
 sudo docker run -i -d --name ubuntu -p 4022:22 ubuntu:18.04
 
 sudo docker exec -i ubuntu /bin/bash -c "
-sed -i 's/http:\/\/archive.ubuntu.com/http:\/\/mirrors.aliyun.com/g' /etc/apt/sources.list;
 apt-get update;
-apt-get -y upgrade;
-apt-get install -y openssh-client openssh-server;
-apt-get install -y subversion build-essential libncurses5-dev zlib1g-dev gawk git ccache gettext libssl-dev xsltproc zip;
-useradd -m bruce -d /home/bruce -s /bin/bash;passwd -d bruce;
+apt-get install -y openssh-client openssh-server sudo;
 sed -i -r 's/^\s*UseDNS\s+\w+/#\0/; s/^\s*PermitRootLogin\s+\w+/#\0/; s/^\s*PasswordAuthentication\s+\w+/#\0/; s/^\s*ClientAliveInterval\s+\w+/#\0/' /etc/ssh/sshd_config;
 echo '
 UseDNS no
@@ -17,5 +13,9 @@ PasswordAuthentication yes
 ClientAliveInterval 30
 ' >> /etc/ssh/sshd_config;
 /etc/init.d/ssh start;
+useradd -m bruce -d /home/bruce -s /bin/bash;passwd -d bruce;
+groupadd wheel
+echo '%wheel ALL=(ALL:ALL)  NOPASSWD:ALL' >> /etc/sudoers
+usermod -aG wheel bruce
 su bruce bash -c 'cd;mkdir .ssh;chmod 700 .ssh;echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3nTRJ/aVb67l1xMaN36jmIbabU7Hiv/xpZ8bwLVvNO3Bj7kUzYTp7DIbPcHQg4d6EsPC6j91E8zW6CrV2fo2Ai8tDO/rCq9Se/64F3+8oEIiI6E/OfUZfXD1mPbG7M/kcA3VeQP6wxNPhWBbKRisqgUc6VTKhl+hK6LwRTZgeShxSNcey+HZst52wJxjQkNG+7CAEY5bbmBzAlHCSl4Z0RftYTHR3q8LcEg7YLNZasUogX68kBgRrb+jw1pRMNo7o7RI9xliDAGX+E4C3vVZL0IsccKgr90222axsADoEjC9O+Q6uwKjahemOVaau+9sHIwkelcOcCzW5SuAwkezv 805899926@qq.com > .ssh/authorized_keys;chmod 644 .ssh/authorized_keys'
 "
