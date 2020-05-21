85efaff0419bcd63efd68fff7d714064d08061cd
Bruce CHEN <805899926@qq.com>
2020-05-21 22:54:20 +0800

    #Bruce Improve

--- server/docker/compose/v2ray/config.sh
+++ server/docker/compose/v2ray/config.sh
@@ -16,25 +16,7 @@ sudo tee /data/volumes/v2ray/config.json <<-'EOF'
                         "id": "e85f98fb-83b4-4234-a51f-85df46403a8e",
                         "alterId": 4
                     }
-                ],
-                "detour": {
-                    "to": "dynamicPort"
-                }
-            }
-        },
-        {
-            "protocol": "vmess",
-            "port": "10000-20000",
-            "tag": "dynamicPort",
-            "settings": {
-                "default": {
-                    "alterId": 64
-                }
-            },
-            "allocate": {
-                "strategy": "random",
-                "concurrency": 2,
-                "refresh": 3
+                ]
             }
         }
     ],
