4565024f2b25f5ff76f88cc3e51616e5e72cc477
Bruce CHEN <805899926@qq.com>
2020-05-21 20:53:14 +0800

    #bruce improve

--- server/docker/compose/v2ray/config.sh
+++ server/docker/compose/v2ray/config.sh
@@ -6,25 +6,43 @@ sudo mkdir -p /data/volumes/v2ray/
 
 sudo tee /data/volumes/v2ray/config.json <<-'EOF'
 {
-  "inbounds": [
-    {
-      "port": 9000,
-      "protocol": "vmess",
-      "settings": {
-        "clients": [
-          {
-            "id": "e85f98fb-83b4-4234-a51f-85df46403a8e",
-            "alterId": 64
-          }
-        ]
-      }
-    }
-  ],
-  "outbounds": [
-    {
-      "protocol": "freedom",
-      "settings": {}
-    }
-  ]
+    "inbounds": [
+        {
+            "port": 9000,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "e85f98fb-83b4-4234-a51f-85df46403a8e",
+                        "alterId": 4
+                    }
+                ],
+                "detour": {
+                    "to": "dynamicPort"
+                }
+            }
+        },
+        {
+            "protocol": "vmess",
+            "port": "10000-20000",
+            "tag": "dynamicPort",
+            "settings": {
+                "default": {
+                    "alterId": 64
+                }
+            },
+            "allocate": {
+                "strategy": "random",
+                "concurrency": 2,
+                "refresh": 3
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "settings": {}
+        }
+    ]
 }
 EOF
