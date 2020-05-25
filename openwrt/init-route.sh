59f29c64930a8a1b291442a9afcb1053f6a5519a
Bruce CHEN <805899926@qq.com>
2020-05-25 15:48:09 +0800

    #Bruce add override_config.sh

--- openwrt/init-route.sh
+++ openwrt/init-route.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 ash -c "set -e;echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3nTRJ/aVb67l1xMaN36jmIbabU7Hiv/xpZ8bwLVvNO3Bj7kUzYTp7DIbPcHQg4d6EsPC6j91E8zW6CrV2fo2Ai8tDO/rCq9Se/64F3+8oEIiI6E/OfUZfXD1mPbG7M/kcA3VeQP6wxNPhWBbKRisqgUc6VTKhl+hK6LwRTZgeShxSNcey+HZst52wJxjQkNG+7CAEY5bbmBzAlHCSl4Z0RftYTHR3q8LcEg7YLNZasUogX68kBgRrb+jw1pRMNo7o7RI9xliDAGX+E4C3vVZL0IsccKgr90222axsADoEjC9O+Q6uwKjahemOVaau+9sHIwkelcOcCzW5SuAwkezv 805899926@qq.com > /etc/dropbear/authorized_keys"
+
+# network
+uci set network.lan.ipaddr=192.168.2.5
+uci commit network
+
+# wifi
+uci set wireless.@wifi-device[0].disabled=0
+uci set wireless.@wifi-iface[0].ssid=Bruce-Net
+uci commit wireless
