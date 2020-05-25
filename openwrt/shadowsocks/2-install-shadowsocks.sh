8ea9cc45b85a97eb673fd40fb3b1da36a0edcbaa
Bruce CHEN <805899926@qq.com>
2020-05-25 17:06:59 +0800

    #Bruce improve sh install

--- openwrt/shadowsocks/2-install-shadowsocks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget --no-check-certificate -O shadowsocks-libev.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev.sh
-sudo chmod +x shadowsocks-libev.sh
-sudo ./shadowsocks-libev.sh 2>&1 | tee shadowsocks-libev.log
--- openwrt/shadowsocks/3-install-bbr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
-sudo chmod +x bbr.sh
-
-sudo ./bbr.sh
