41c71730542d14c1aa9325d0d50545714ca45782
Bruce CHEN <805899926@qq.com>
2020-05-26 17:19:55 +0800

    #Bruce Improve create-user.sh

--- server/create-user.sh
+++ server/create-user.sh
@@ -2,11 +2,13 @@
 
 # sh create-user.sh -h [user@host] -u [username] -i [identity_path]
 # -i ./pubs/ | ./pubs/czy-rsa.pub
+# -a is_append to authorized_keys for exist user
 set -e
 
 host=
 username=
 public_key=
+is_append=0
 
 while [[ $# -ge 1 ]]; do
   case $1 in
@@ -25,9 +27,13 @@ while [[ $# -ge 1 ]]; do
       done
     fi
     if test -f $2; then
-      public_key=$(cat $f)
+      public_key=$(cat $2)
     fi
     shift 2
+    ;;
+  -a)
+    is_append=1
+    shift 1
     ;;
   *)
     echo -e "\033[31m$1 un_know input param \033[0m"
@@ -35,6 +41,15 @@ while [[ $# -ge 1 ]]; do
     ;;
   esac
 done
+
+echo "host -> $host"
+echo "username -> $username"
+echo "is_append -> $is_append"
+
+if [ $is_append = 1 ]; then
+  ssh $host "sudo -u '$username' bash -c "\""set -e;cd;echo -e '$public_key' >> .ssh/authorized_keys;"\"
+  exit
+fi
 
 ssh $host "sudo useradd -m '$username';
            sudo usermod -aG wheel '$username';
