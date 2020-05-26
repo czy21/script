9f1efcb23994d8e4c7f255c5c073944be34e9187
Bruce CHEN <805899926@qq.com>
2020-05-26 18:21:37 +0800

    #Bruce Improve

--- server/init-machine-centos.sh
+++ server/init-machine-centos.sh
@@ -12,8 +12,8 @@ while [[ $# -ge 1 ]]; do
     sh_file='init-machine-centos.sh'
     cp_path=$sh_file
     rm_path=$sh_file
-    upload_exec $@
     shift 2
+    upload_exec $@
     break
     ;;
   -t)
