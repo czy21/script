e736ff6434ef5e3a6740aa06658e2d4ef51662c3
Bruce CHEN <805899926@qq.com>
2020-05-26 17:32:14 +0800

    #Bruce Improve

--- server/init-machine-centos.sh
+++ server/init-machine-centos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#sh init-machine-centos.sh -h user@host -t ali
-# -t ali | offical
+#sh init-machine-centos.sh -h user@host -t ali local
+# -t ali | offical local|remote
 # must as root
 set -e
 
@@ -37,20 +37,18 @@ while [[ $# -ge 1 ]]; do
       yum makecache
     fi
 
-    if [ $1 == 'offical' ]; then
-      break
-    fi
-
     # add first user
     useradd -m bruce
     usermod -aG wheel bruce
     passwd -d bruce
     sudo -u bruce bash -c "set -e;cd;mkdir .ssh;chmod 700 .ssh;echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3nTRJ/aVb67l1xMaN36jmIbabU7Hiv/xpZ8bwLVvNO3Bj7kUzYTp7DIbPcHQg4d6EsPC6j91E8zW6CrV2fo2Ai8tDO/rCq9Se/64F3+8oEIiI6E/OfUZfXD1mPbG7M/kcA3VeQP6wxNPhWBbKRisqgUc6VTKhl+hK6LwRTZgeShxSNcey+HZst52wJxjQkNG+7CAEY5bbmBzAlHCSl4Z0RftYTHR3q8LcEg7YLNZasUogX68kBgRrb+jw1pRMNo7o7RI9xliDAGX+E4C3vVZL0IsccKgr90222axsADoEjC9O+Q6uwKjahemOVaau+9sHIwkelcOcCzW5SuAwkezv 805899926@qq.com > .ssh/authorized_keys;chmod 644 .ssh/authorized_keys"
-    systemctl disable firewalld
-    sed -i -r "s/SELINUX=enforcing/SELINUX=disabled/" /etc/selinux/config
+    if [ $2 == 'local' ]; then
+      systemctl disable firewalld
+      sed -i -r "s/SELINUX=enforcing/SELINUX=disabled/" /etc/selinux/config
+    fi
     shutdown -r now
 
-    shift 1
+    shift 2
     ;;
   *)
     echo -e "\033[31m$1 un_know input param \033[0m"
