8d4312445ae0f04760a8d75f3a13c4dabd5b01c4
Bruce CHEN <805899926@qq.com>
2020-05-26 10:42:29 +0800

    #Bruce Improve

--- openwrt/init-route.sh
+++ openwrt/init-route.sh
@@ -8,9 +8,39 @@ uci set network.lan.gateway=192.168.2.1
 uci set network.lan.dns=114.114.114.114
 uci commit network
 
-/etc/init.d/network reload
+# google clound
+uci add shadowsocksr servers
+uci set shadowsocksr.@servers[0].alias='gg_hk'
+uci set shadowsocksr.@servers[0].type='v2ray'
+uci set shadowsocksr.@servers[0].server='34.96.138.30'
+uci set shadowsocksr.@servers[0].server_port='40879'
+uci set shadowsocksr.@servers[0].alter_id='4'
+uci set shadowsocksr.@servers[0].vmess_id='e85f98fb-83b4-4234-a51f-85df46403a8e'
+uci set shadowsocksr.@servers[0].local_port='1234'
+uci set shadowsocksr.@servers[0].security='auto'
+uci set shadowsocksr.@servers[0].transport='tcp'
+uci set shadowsocksr.@servers[0].tcp_guise='http'
+uci set shadowsocksr.@servers[0].switch_enable='0'
+
+# ali clound
+uci add shadowsocksr servers
+uci set shadowsocksr.@servers[1].alias='ali_hk'
+uci set shadowsocksr.@servers[1].type='v2ray'
+uci set shadowsocksr.@servers[1].server='149.129.125.40'
+uci set shadowsocksr.@servers[1].server_port='40879'
+uci set shadowsocksr.@servers[1].alter_id='64'
+uci set shadowsocksr.@servers[1].vmess_id='e85f98fb-83b4-4234-a51f-85df46403a8e'
+uci set shadowsocksr.@servers[1].local_port='1234'
+uci set shadowsocksr.@servers[1].security='auto'
+uci set shadowsocksr.@servers[1].transport='tcp'
+uci set shadowsocksr.@servers[1].tcp_guise='http'
+uci set shadowsocksr.@servers[1].switch_enable='0'
+
+uci commit shadowsocksr
 
 # wifi
 #uci set wireless.@wifi-device[0].disabled=0
 #uci set wireless.@wifi-iface[0].ssid=Bruce-Net
 #uci commit wireless
+
+reboot
--- openwrt/override_config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# sh override_config.sh user@host
-
-host=$1
-ssh $host 'rm -rf $HOME/lede/files/ && mkdir -p $HOME/lede/files/'
-scp -r ./etc $host:'$HOME/lede/files/'
