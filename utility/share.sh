3bc43f08ee22487ad4b7233000e329461668fff2
Bruce CHEN <805899926@qq.com>
2020-05-27 18:45:08 +0800

    #Bruce Improve

--- utility/share.sh
+++ utility/share.sh
@@ -6,7 +6,7 @@ function upload_exec() {
   ssh $host 'rm -rf $HOME/'$rm_path';'
 
   scp -r $cp_path $host:
-  ssh $host 'sh -x $HOME/'$sh_file' '$@';'
+  ssh $host 'sh $HOME/'$sh_file' '$@';'
 
-  ssh $host 'rm -rf $HOME/'$rm_path';'
+#  ssh $host 'rm -rf $HOME/'$rm_path';'
 }
