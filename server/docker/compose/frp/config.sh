6c4fee986b3a1cae8ae6fe7b6a069f998ea74f7a
Bruce CHEN <805899926@qq.com>
2020-05-27 11:24:18 +0800

    #Bruce Improve

--- server/docker/compose/frp/config.sh
+++ server/docker/compose/frp/config.sh
@@ -9,13 +9,6 @@ sudo tee /data/config/frp/conf/frps.ini <<-'EOF'
 [common]
 bind_addr = 0.0.0.0
 bind_port = 7000
-kcp_bind_port = 7000
-vhost_http_port = 80
-vhost_https_port = 443
 dashboard_addr = 0.0.0.0
 dashboard_port = 7500
-dashboard_user = admin
-dashboard_pwd = admin
-authentication_timeout = 0
-subdomain_host = frp.thyiad.top
 EOF
