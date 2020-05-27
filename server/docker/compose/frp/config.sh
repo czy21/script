2c8baa0abb15fefefa2ac725946aab5324aacc2c
Bruce CHEN <805899926@qq.com>
2020-05-27 23:58:27 +0800

    #Bruce frp server finished

--- server/docker/compose/frp/config.sh
+++ server/docker/compose/frp/config.sh
@@ -7,8 +7,11 @@ sudo mkdir -p /data/config/frp/conf/
 
 sudo tee /data/config/frp/conf/frps.ini <<-'EOF'
 [common]
-bind_addr = 0.0.0.0
 bind_port = 7000
+vhost_http_port = 80
+vhost_https_port = 443
+token = 12345678
+
 dashboard_addr = 0.0.0.0
 dashboard_port = 7500
 EOF
