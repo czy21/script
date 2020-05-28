9883f74a4966caafdda09cff5a007b8c58ca754d
Bruce CHEN <805899926@qq.com>
2020-05-29 00:17:47 +0800

    #Bruce frp v2ray nginx Unfinished

--- server/docker/compose/nginx/config.sh
+++ server/docker/compose/nginx/config.sh
@@ -4,6 +4,8 @@ set -e
 
 sudo mkdir -p /data/config/nginx/conf.d/
 
+sudo docker network create nginx-proxy
+
 sudo tee /data/config/nginx/nginx.conf <<-'EOF'
 user  nginx;
 worker_processes  1;
@@ -50,5 +52,14 @@ server {
     location = /50x.html {
         root   /usr/share/nginx/html;
     }
+
+    location /v2ray/ {
+        proxy_redirect off;
+        proxy_pass http://127.0.0.1:40879;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_set_header Host $http_host;
+  }
 }
 EOF
