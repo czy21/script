cd3f404aeb652d0c2f2619b1ea43bd30af6f6d17
Bruce CHEN <805899926@qq.com>
2020-05-30 18:58:54 +0800

    #Bruce nginx v2ray proxy finished

--- server/docker/compose/nginx/config.sh
+++ server/docker/compose/nginx/config.sh
@@ -2,8 +2,12 @@
 
 set -e
 
+dir=$(cd "$(dirname "$0")"; pwd)
+
 sudo rm -rf /data/config/nginx/conf.d/
 sudo mkdir -p /data/config/nginx/conf.d/
+
+sudo cp -r $dir/cert/ /data/config/nginx/
 
 sudo tee /data/config/nginx/nginx.conf <<-'EOF'
 user  nginx;
@@ -40,7 +44,7 @@ EOF
 sudo tee /data/config/nginx/conf.d/default.conf <<-'EOF'
 server {
     listen       80;
-    server_name  localhost;
+    server_name  czy-home.cn;
 
     location / {
         root   /usr/share/nginx/html;
@@ -54,44 +58,37 @@ server {
 }
 EOF
 
-#sudo tee /data/config/nginx/conf.d/custom.conf <<-'EOF'
-#server {
-#		listen 80;
-#		server_name czy-home.cn;
-#
-#    location /ray {
-#        root   /usr/share/nginx/html;
-#        index  index.html index.htm;
-#    }
-#    location /frp-dashboard {
-#        proxy_pass http://127.0.0.1:7500;
-#    }
-#}
-#EOF
+sudo tee /data/config/nginx/conf.d/custom.conf <<-'EOF'
+server {
+      listen 443 ssl;
+      listen [::]:443 ssl;
+      server_name czy-home.cn;
 
-#sudo tee /data/config/nginx/conf.d/custom_https.conf <<-'EOF'
-#server {
-#      listen  443 ssl;
-#      ssl on;
-#      ssl_certificate       /etc/v2ray/v2ray.crt;
-#      ssl_certificate_key   /etc/v2ray/v2ray.key;
-#      ssl_protocols         TLSv1 TLSv1.1 TLSv1.2;
-#      ssl_ciphers           HIGH:!aNULL:!MD5;
-#      server_name           czy-home.cn;
-#
-#      location /ray {
-#          proxy_redirect off;
-#          proxy_pass http://127.0.0.1:9000;
-#          proxy_http_version 1.1;
-#          proxy_set_header Upgrade $http_upgrade;
-#          proxy_set_header Connection "upgrade";
-#          proxy_set_header Host $http_host;
-#
-#          proxy_set_header X-Real-IP $remote_addr;
-#          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-#      }
-#}
-#EOF
+      ssl_certificate       /etc/nginx/cert/cert.pem;
+      ssl_certificate_key   /etc/nginx/cert/key.pem;
+      ssl_session_timeout 1d;
+      ssl_session_cache shared:MozSSL:10m;
+      ssl_session_tickets off;
+
+      ssl_protocols         TLSv1.2 TLSv1.3;
+      ssl_ciphers           ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
+      ssl_prefer_server_ciphers off;
+
+      location /ray {
+        if ($http_upgrade != "websocket") {
+            return 404;
+        }
+        proxy_redirect off;
+        proxy_pass http://127.0.0.1:9000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      }
+}
+EOF
 
 #sudo tee /data/config/nginx/conf.d/frp.conf <<-'EOF'
 #server {
