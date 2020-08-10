a12dae36de08bf00c446238955518d38301421c6
Bruce CHEN <805899926@qq.com>
2020-08-10 14:25:57 +0800

    #Bruce Improve

--- server/docker/compose/neo4j/init_config.sh
+++ server/docker/compose/neo4j/init_config.sh
@@ -2,4 +2,12 @@
 
 set -e
 
-sudo mkdir -p /data/volumes/neo4j/
+dir=$(cd "$(dirname "$0")"; pwd)
+
+volume_path=/data/volumes/neo4j/
+config_path=/data/config/neo4j/
+
+sudo mkdir -p ${config_path}
+sudo mkdir -p ${volume_path}
+
+sudo cp -r $dir/conf/ ${config_path}
