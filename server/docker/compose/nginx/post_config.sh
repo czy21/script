eba553b7b7dd77a3bdf067b05b2297530e19ad0d
Bruce CHEN <805899926@qq.com>
2020-08-10 16:18:15 +0800

    #Bruce Improve

--- server/docker/compose/nginx/post_config.sh
+++ server/docker/compose/nginx/post_config.sh
@@ -4,6 +4,6 @@ set -e
 
 dir=$(cd "$(dirname "$0")"; pwd)
 sudo rm -rf /data/config/nginx/conf.d/
-sudo cp -r $dir/cert/ /data/config/nginx/
-sudo cp -r $dir/conf.d/ /data/config/nginx/
+sudo cp -r $dir/override/cert/ /data/config/nginx/
+sudo cp -r $dir/override/conf.d/ /data/config/nginx/
 sudo docker restart nginx
