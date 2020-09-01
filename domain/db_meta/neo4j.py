83916623ef2c9d4ea7f45c3b9983494942dec489
Bruce CHEN <805899926@qq.com>
2020-09-01 14:27:30 +0800

    #Bruce Improve

--- domain/db_meta/neo4j.py
+++ domain/db_meta/neo4j.py
@@ -1,8 +1,8 @@
 # !/usr/bin/env python
 
 self = {
-    "header": "return {file:'${{{file_path}}}',status:'executing'} as msg;",
-    "footer": "return {file:'${{{file_path}}}',status:'executed'} as msg;",
+    "header": "return 'executing: ${{{file_path}}}' as msg;",
+    "footer": "return 'executed: ${{{file_path}}}' as msg;",
     "substitution": {
 
     }
--- domain/db_source.py
+++ domain/db_source.py
@@ -3,15 +3,11 @@ import io
 import math
 import os
 import re
-import json
-import pickle
+
 from colorama import init, Fore
 
 from script.domain import default_common as common
-# from script.domain.db_meta import mysql as mysql_meta
-from script.domain.db_meta import \
-    mysql as mysql_meta, \
-    neo4j as neo4j_meta
+from script.domain.db_meta import mysql as mysql_meta, neo4j as neo4j_meta
 from script.utility import path, template
 
 init(autoreset=True)
@@ -33,136 +29,133 @@ def assemble_ql(s_path, t_file_name, db_meta, file_suffix):
             t_file.write(u'{}'.format(footer.safe_substitute(file_path=s) + "\n\n"))
 
 
-# regex filter item of list
 def filter_execution(iterator):
     return list(filter(re.compile(r"^(executing:|executed:)").search, iterator))
 
 
-def assemble_mysql():
-    assemble_ql(common.param_main_db_mysql_file_path, common.param_main_db_mysql_output_file_name, mysql_meta, "sql")
-
-
-# execute import sql file
-def upgrade_mysql():
-    command = recreate_mysql_command(common.param_main_db_host,
-                                     common.param_main_db_mysql_port,
-                                     common.param_main_db_mysql_user,
-                                     common.param_main_db_mysql_pass,
-                                     common.param_main_db_name) + \
-              " && mysql" \
-              " --default-character-set=utf8mb4" \
-              " --database=" + common.param_main_db_name + \
-              " --host=" + common.param_main_db_host + \
-              " --port=" + common.param_main_db_mysql_port + \
-              " --user=" + common.param_main_db_mysql_user + \
-              " --password=" + common.param_main_db_mysql_pass + \
-              " -N < " + common.param_main_db_mysql_output_file_name
-    print(Fore.CYAN + upgrade_mysql.__name__ + " => " + Fore.WHITE + command)
-    mysql_msg = os.popen(command).readlines()
-    callback = filter_execution(mysql_msg)
+def print_msg(msg_lines):
+    callback = filter_execution(msg_lines)
     for m in callback[1::2]:
         print(Fore.GREEN + m.replace("\n", ""))
     if math.modf(len(callback) / 2)[0] > 0:
         print(Fore.RED + callback[len(callback) - 1])
 
 
-# backup db
-def backup_mysql():
-    command = recreate_mysql_command(common.param_main_db_host,
-                                     common.param_main_db_mysql_port,
-                                     common.param_main_db_mysql_user,
-                                     common.param_main_db_mysql_pass,
-                                     common.param_main_db_bak_name) + \
-              " && mysqldump " + common.param_main_db_name + \
-              " --default-character-set=utf8mb4" \
-              " --host=" + common.param_main_db_host + \
-              " --port=" + common.param_main_db_mysql_port + \
-              " --user=" + common.param_main_db_mysql_user + \
-              " --password=" + common.param_main_db_mysql_pass + \
-              "| mysql " \
-              " --default-character-set=utf8mb4" \
-              " --database=" + common.param_main_db_bak_name + \
-              " --host=" + common.param_main_db_host + \
-              " --port=" + common.param_main_db_mysql_port + \
-              " --user=" + common.param_main_db_mysql_user + \
-              " --password=" + common.param_main_db_mysql_pass
-    print(Fore.CYAN + "restoring => " + Fore.WHITE + command)
-    os.system(command)
+class Mysql:
+    @staticmethod
+    def assemble():
+        assemble_ql(common.param_main_db_mysql_file_path, common.param_main_db_mysql_output_file_name, mysql_meta, "sql")
+
+    @staticmethod
+    def recreate():
+        command = Mysql.recreate_command(common.param_main_db_host,
+                                         common.param_main_db_mysql_port,
+                                         common.param_main_db_mysql_user,
+                                         common.param_main_db_mysql_pass,
+                                         common.param_main_db_name)
+        print(Fore.CYAN + Mysql.recreate.__name__ + " => " + Fore.WHITE + command)
+        os.system(command)
+
+    @staticmethod
+    def upgrade():
+        command = Mysql.recreate_command(common.param_main_db_host,
+                                         common.param_main_db_mysql_port,
+                                         common.param_main_db_mysql_user,
+                                         common.param_main_db_mysql_pass,
+                                         common.param_main_db_name) + \
+                  " && mysql" \
+                  " --default-character-set=utf8mb4" \
+                  " --database=" + common.param_main_db_name + \
+                  " --host=" + common.param_main_db_host + \
+                  " --port=" + common.param_main_db_mysql_port + \
+                  " --user=" + common.param_main_db_mysql_user + \
+                  " --password=" + common.param_main_db_mysql_pass + \
+                  " -N < " + common.param_main_db_mysql_output_file_name
+        print(Fore.CYAN + Mysql.upgrade.__name__ + " => " + Fore.WHITE + command)
+        mysql_msg = os.popen(command).readlines()
+        print_msg(mysql_msg)
+
+    @staticmethod
+    def recreate_command(host, port, user, password, db_name):
+        return "mysql" \
+               " --default-character-set=utf8mb4" \
+               " --host=" + host + \
+               " --port=" + port + \
+               " --user=" + user + \
+               " --password=" + password + \
+               " -e \"drop database if exists " + db_name + ";create database if not exists " + db_name + " default charset utf8mb4 collate utf8mb4_0900_ai_ci;\""
+
+    @staticmethod
+    def backup_mysql():
+        command = Mysql.recreate_command(common.param_main_db_host,
+                                         common.param_main_db_mysql_port,
+                                         common.param_main_db_mysql_user,
+                                         common.param_main_db_mysql_pass,
+                                         common.param_main_db_bak_name) + \
+                  " && mysqldump " + common.param_main_db_name + \
+                  " --default-character-set=utf8mb4" \
+                  " --host=" + common.param_main_db_host + \
+                  " --port=" + common.param_main_db_mysql_port + \
+                  " --user=" + common.param_main_db_mysql_user + \
+                  " --password=" + common.param_main_db_mysql_pass + \
+                  "| mysql " \
+                  " --default-character-set=utf8mb4" \
+                  " --database=" + common.param_main_db_bak_name + \
+                  " --host=" + common.param_main_db_host + \
+                  " --port=" + common.param_main_db_mysql_port + \
+                  " --user=" + common.param_main_db_mysql_user + \
+                  " --password=" + common.param_main_db_mysql_pass
+        print(Fore.CYAN + "restoring => " + Fore.WHITE + command)
+        os.system(command)
 
 
-def recreate_mysql_command(host, port, user, password, db_name):
-    return "mysql" \
-           " --default-character-set=utf8mb4" \
-           " --host=" + host + \
-           " --port=" + port + \
-           " --user=" + user + \
-           " --password=" + password + \
-           " -e \"drop database if exists " + db_name + ";create database if not exists " + db_name + " default charset utf8mb4 collate utf8mb4_0900_ai_ci;\""
+class Neo4j:
+    @staticmethod
+    def recreate_command(host, port, user, password, db_name):
+        return "cypher-shell" \
+               " --address neo4j://" + host + ":" + port + \
+               " --username " + user + \
+               " --password " + password + \
+               " --database " + db_name + " \"match(n) detach delete n;\""
 
+    @staticmethod
+    def upgrade():
+        command = Neo4j.recreate_command(common.param_main_db_host,
+                                         common.param_main_db_neo4j_port,
+                                         common.param_main_db_neo4j_user,
+                                         common.param_main_db_neo4j_pass,
+                                         common.param_main_db_neo4j_db_name) + \
+                  " && cypher-shell" \
+                  " --address neo4j://" + common.param_main_db_host + ":" + common.param_main_db_neo4j_port + \
+                  " --database=" + common.param_main_db_name + \
+                  " --username " + common.param_main_db_neo4j_user + \
+                  " --password " + common.param_main_db_neo4j_pass + \
+                  " --database " + common.param_main_db_neo4j_db_name + \
+                  " --file " + common.param_main_db_neo4j_output_file_name
+        print(Fore.CYAN + Neo4j.upgrade.__name__ + " => " + Fore.WHITE + command)
+        neo4j_msg = [elem.replace("\"", '') for elem in os.popen(command).readlines() if elem != "msg\n"]
+        print_msg(neo4j_msg)
 
-def recreate_neo4j_command(host, port, user, password, db_name):
-    return "cypher-shell" \
-           " --address neo4j://" + host + ":" + port + \
-           " --username " + user + \
-           " --password " + password + \
-           " --database " + db_name + " \"match(n) detach delete n;\""
+    @staticmethod
+    def recreate():
+        command = Neo4j.recreate_command(common.param_main_db_host,
+                                         common.param_main_db_neo4j_port,
+                                         common.param_main_db_neo4j_user,
+                                         common.param_main_db_neo4j_pass,
+                                         common.param_main_db_neo4j_db_name)
+        print(Fore.CYAN + Neo4j.recreate.__name__ + " => " + Fore.WHITE + command)
+        os.system(command)
 
-
-def upgrade_neo4j():
-    command = recreate_neo4j_command(common.param_main_db_host,
-                                     common.param_main_db_neo4j_port,
-                                     common.param_main_db_neo4j_user,
-                                     common.param_main_db_neo4j_pass,
-                                     common.param_main_db_neo4j_db_name) + \
-              " && cypher-shell" \
-              " --address neo4j://" + common.param_main_db_host + ":" + common.param_main_db_neo4j_port + \
-              " --database=" + common.param_main_db_name + \
-              " --username " + common.param_main_db_neo4j_user + \
-              " --password " + common.param_main_db_neo4j_pass + \
-              " --database " + common.param_main_db_neo4j_db_name + \
-              " --file " + common.param_main_db_neo4j_output_file_name
-    print(Fore.CYAN + upgrade_mysql.__name__ + " => " + Fore.WHITE + command)
-    callback = [elem for elem in os.popen(command).readlines() if elem != "msg\n"]
-    for t in callback:
-        print(t)
-    # callback = filter_execution(os.popen(command).readlines())
-    print(callback)
-    # for m in callback[1::2]:
-    #     print(Fore.GREEN + m.replace("\n", ""))
-    # if math.modf(len(callback) / 2)[0] > 0:
-    #     print(Fore.RED + callback[len(callback) - 1])
-
-
-def recreate_neo4j():
-    command = recreate_neo4j_command(common.param_main_db_host,
-                                     common.param_main_db_neo4j_port,
-                                     common.param_main_db_neo4j_user,
-                                     common.param_main_db_neo4j_pass,
-                                     common.param_main_db_neo4j_db_name)
-    print(Fore.CYAN + recreate_neo4j.__name__ + " => " + Fore.WHITE + command)
-    os.system(command)
-
-
-# recreate target db
-def recreate_mysql():
-    command = recreate_mysql_command(common.param_main_db_host,
-                                     common.param_main_db_mysql_port,
-                                     common.param_main_db_mysql_user,
-                                     common.param_main_db_mysql_pass,
-                                     common.param_main_db_name)
-    print(Fore.CYAN + recreate_mysql.__name__ + " => " + Fore.WHITE + command)
-    os.system(command)
-
-
-def assemble_neo4j():
-    assemble_ql(common.param_main_db_neo4j_file_path, common.param_main_db_neo4j_output_file_name, neo4j_meta, "cql")
+    @staticmethod
+    def assemble():
+        assemble_ql(common.param_main_db_neo4j_file_path, common.param_main_db_neo4j_output_file_name, neo4j_meta, "cql")
 
 
 def rebuild_mysql():
-    assemble_mysql()
-    upgrade_mysql()
+    Mysql.assemble()
+    Mysql.upgrade()
 
 
 def rebuild_neo4j():
-    assemble_neo4j()
-    upgrade_neo4j()
+    Neo4j.assemble()
+    Neo4j.upgrade()
