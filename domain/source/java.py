320587ed1d11207c9acc4aaedb921a1c64c0e353
Bruce CHEN <805899926@qq.com>
2020-09-01 20:45:52 +0800

    #Bruce Improve

--- domain/source/java.py
+++ domain/source/java.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from colorama import Fore, init
 
-from script.domain.default import default_common as common
+from script.domain.default import common as default_common
 from script.utility import template
 
 init(autoreset=True)
@@ -16,24 +16,24 @@ def build_by_template(template_name, output_path):
     with io.open(template_name, "r", encoding="utf-8", newline="\n") as source_template:
         with io.open(output_name, "w+", encoding="utf-8", newline="\n") as target_output:
             target_output.write(template.StringTemplate(source_template.read() + "\n")
-                                .safe_substitute(common.get_params()))
+                                .safe_substitute(default_common.get_params()))
     return output_name
 
 
 def build_extra_config():
     print(Fore.CYAN + build_extra_config.__name__)
-    return build_by_template(common.param_api_extra_config_template_name, common.path_default.output_tmp)
+    return build_by_template(default_common.param_api_extra_config_template_name, default_common.default_path.output_tmp)
 
 
 def build_override_yml():
     print(Fore.CYAN + build_override_yml.__name__)
-    return build_by_template(common.param_api_yml_override_template_name, common.param_api_output_resource_path)
+    return build_by_template(default_common.param_api_yml_override_template_name, default_common.param_api_output_resource_path)
 
 
 def build_api():
     output_extra_config_name = build_extra_config()
     command = "gradle clean build" \
-              " --build-file " + Path(common.param_api_root_project_path).joinpath("build.gradle").as_posix() + \
+              " --build-file " + Path(default_common.param_api_root_project_path).joinpath("build.gradle").as_posix() + \
               " --project-prop extraConfig=" + output_extra_config_name
     print(Fore.CYAN + build_api.__name__ + " => " + Fore.WHITE + command)
     os.system(command)
