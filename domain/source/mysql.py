9036be6a915ffebe0a63e36b41e92e5f9bc5283c
Bruce CHEN <805899926@qq.com>
2020-09-01 22:22:25 +0800

    #Bruce Improve

--- domain/source/mysql.py
+++ domain/source/mysql.py
@@ -1,4 +1,4 @@
-# !/usr/bin/env python
+#!/usr/bin/env python3
 import os
 
 from script.domain.default import common as default_common
--- utility/list.py
+++ utility/list.py
@@ -1,4 +1,4 @@
-# !/usr/bin/env python
+#!/usr/bin/env python3
 
 
 def arr_param_to_str(*items):
