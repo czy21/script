42543cf277c24cdfdeee3ea729efe2a01c2c62fc
Bruce CHEN <805899926@qq.com>
2020-09-15 00:27:18 +0800

    #Bruce add sleep print

--- utility/db.py
+++ utility/db.py
@@ -5,7 +5,7 @@ import re
 
 from colorama import init, Fore
 
-from script.utility import path as path_util, template as template_util
+from script.utility import path as path_util, template as template_util, basic as basic_util
 
 init(autoreset=True)
 
@@ -16,7 +16,7 @@ def assemble_ql(s_path, t_file_name, db_meta, file_suffix) -> None:
         for s in db_file_paths:
             header = template_util.StringTemplate(db_meta.self["header"])
             footer = template_util.StringTemplate(db_meta.self["footer"])
-            print(Fore.GREEN + "loading => " + Fore.WHITE + s)
+            basic_util.print(Fore.GREEN + "loading => " + Fore.WHITE + s)
             t_file.write(u'{}'.format(header.safe_substitute(file_path=s) + "\n"))
             with io.open(s, "r", encoding="utf-8", newline="\n") as current_sql_file:
                 t_file.write(template_util.StringTemplate(current_sql_file.read() + "\n")
@@ -31,10 +31,10 @@ def filter_execution(iterator) -> list:
 def print_msg(msg_lines) -> None:
     callback = filter_execution(msg_lines)
     for m in callback[1::2]:
-        print(Fore.GREEN + m.replace("\n", ""))
+        basic_util.print(Fore.GREEN + m.replace("\n", ""))
     if math.modf(len(callback) / 2)[0] > 0:
-        print(Fore.RED + callback[len(callback) - 1])
+        basic_util.print(Fore.RED + callback[len(callback) - 1])
 
 
 def print_cmd(class_name, method_name, command) -> None:
-    print(Fore.CYAN + class_name + "_" + method_name + " => " + Fore.WHITE + command)
+    basic_util.print(Fore.CYAN + class_name + "_" + method_name + " => " + Fore.WHITE + command)
