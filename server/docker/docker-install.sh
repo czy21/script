cb269979491bc594bbaa36179cb0e56f9a1f167d
Bruce CHEN <805899926@qq.com>
2020-09-27 21:59:31 +0800

    #Bruce

--- server/docker/docker-install.sh
+++ server/docker/docker-install.sh
@@ -25,7 +25,7 @@ do
         {
           "registry-mirrors": ["https://idyylogn.mirror.aliyuncs.com","https://registry.docker-cn.com"]
         }
-        EOF
+EOF
         sudo yum install -y yum-utils device-mapper-persistent-data lvm2
         sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
         sudo yum makecache timer
