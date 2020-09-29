576810f86394e05a414b053b88ac63ba8c9190d2
Bruce CHEN <805899926@qq.com>
2020-09-29 14:30:28 +0800

    #Bruce fix color

--- utility/basic.py
+++ utility/basic.py
@@ -5,5 +5,5 @@ from colorama import Fore
 def action_formatter(action_name, msg=None, action_color=Fore.YELLOW):
     action_name_msg = action_color + action_name
     if msg:
-        action_name_msg += Fore.BLACK + " => " + msg
+        action_name_msg += Fore.WHITE + " => " + msg
     return action_name_msg
