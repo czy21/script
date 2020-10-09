de271ff1b06d95c3fb77fb5d8184d447276757e7
Bruce CHEN <805899926@qq.com>
2020-10-09 21:54:18 +0800

    #Bruce Improve

--- utility/manual.sh
+++ utility/manual.sh
@@ -11,7 +11,6 @@ neo4j-etl generate-metadata-mapping --rdbms:url "jdbc:mysql://192.168.2.4:3306/c
 
 neo4j-etl export --rdbms:url "jdbc:mysql://192.168.2.4:3306/cy_db?autoReconnect=true&useSSL=false&useCursorFetch=true&allowPublicKeyRetrieval=true" --rdbms:user admin --rdbms:password Czy.190815 --neo4j:url "bolt://127.0.0.1:7687" --neo4j:user neo4j --neo4j:password Czy.190815 --using cypher:batch --unwind-batch-size 1000 --tx-batch-size 10000 --mapping-file /d/mysql_cy_db_mapping.json --csv-directory /d/Temp --debug
 
-
-jenkins_home_des=`echo $WORKSPACE | awk -F "/workspace" '{print $1}'` &&
-jenkins_home_source=`sudo docker inspect --format '{{range $t :=.Mounts}}{{if eq $t.Destination "'${jenkins_home_des}'"}}{{$t.Source}}{{end}}{{end}}' jenkins` &&
-HOME=$jenkins_home_source"/workspace" && echo $HOME
+jenkins_home_des=$(echo $WORKSPACE | awk -F "/workspace" '{print $1}') &&
+jenkins_home_source=$(sudo docker inspect --format '{{range $t :=.Mounts}}{{if eq $t.Destination "'${jenkins_home_des}'"}}{{$t.Source}}{{end}}{{end}}' jenkins) &&
+HOME=$(echo ${WORKSPACE/${jenkins_home_des}/${jenkins_home_source}})
