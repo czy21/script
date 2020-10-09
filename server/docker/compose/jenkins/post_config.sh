03e8200cf726b2166d1ce404db1aa4c9bc08fe8e
Bruce CHEN <805899926@qq.com>
2020-10-09 14:14:16 +0800

    #Bruce Improve

--- server/docker/compose/jenkins/post_config.sh
+++ server/docker/compose/jenkins/post_config.sh
@@ -4,10 +4,9 @@ set -e
 
 dir=$(cd "$(dirname "$0")"; pwd)
 container_name="jenkins"
-sudo docker exec -i ${container_name} bash -c "sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories \
-                                            && apk update"
+sudo docker exec -i ${container_name} bash -c "sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories && apk update"
 
-sudo docker exec -i ${container_name} bash -c "apk add python3 py3-pip"
+sudo docker exec -i ${container_name} bash -c "apk add python3 py3-pip sudo"
 
 sudo docker exec -i ${container_name} bash -c "mkdir ~/.pip/ && tee ~/.pip/pip.conf <<-'EOF'
 [global]
