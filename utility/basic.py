58fd8c288d5502e0a3fec2df0f20a301f128d929
Bruce CHEN <805899926@qq.com>
2020-10-11 23:36:43 +0800

    #Bruce Improve

--- utility/basic.py
+++ utility/basic.py
@@ -16,16 +16,18 @@ def action_formatter(action_name, msg=None, action_color=Fore.YELLOW):
     return action_name_msg
 
 
-def print_default(msg_lines) -> None:
+def print_default(msg_lines, proc, func_param) -> None:
     for line in msg_lines:
         line = line.strip()
         if line:
             logger.info(line, is_sleep=False)
 
 
-def execute(cmd, func=print_default):
+def execute(cmd, func=print_default, func_param=None):
+    if func_param is None:
+        func_param = {}
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True, encoding="utf-8")
-    func(iter(proc.stdout.readline, ''))
+    func(iter(proc.stdout.readline, ''), proc, func_param)
     proc.stdout.close()
     proc.wait()
     if proc.returncode == 1:
--- utility/manual.sh
+++ utility/manual.sh
@@ -12,3 +12,10 @@ neo4j-etl generate-metadata-mapping --rdbms:url "jdbc:mysql://192.168.2.4:3306/c
 neo4j-etl export --rdbms:url "jdbc:mysql://192.168.2.4:3306/cy_db?autoReconnect=true&useSSL=false&useCursorFetch=true&allowPublicKeyRetrieval=true" --rdbms:user admin --rdbms:password Czy.190815 --neo4j:url "bolt://127.0.0.1:7687" --neo4j:user neo4j --neo4j:password Czy.190815 --using cypher:batch --unwind-batch-size 1000 --tx-batch-size 10000 --mapping-file /d/mysql_cy_db_mapping.json --csv-directory /d/Temp --debug
 
 HOME=$WORKSPACE && pip3 install -r ./erp/script/requirements.txt && cd ./erp/shell/play && python3 build_api_image.py --param param_api_image_tag=1.0.0 && cd $HOME
+
+
+docker rm -f $(docker ps --filter ancestor=erp:play -q)
+
+docker rmi erp:play
+
+docker ps --filter ancestor=erp:play -q | xargs docker rm
