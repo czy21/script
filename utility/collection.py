0b8eb4c3633c588596db291f7798a763eb0c7e2c
Bruce CHEN <805899926@qq.com>
2020-10-12 01:04:33 +0800

    #Bruce Improve

--- utility/collection.py
+++ utility/collection.py
@@ -4,8 +4,8 @@
 flat = lambda L: sum(map(flat, L), []) if isinstance(L, list) else [L]
 
 
-def arr_param_to_str(*items) -> str:
-    return " ".join(flat(list(items)))
+def arr_param_to_str(*items, separator=" ") -> str:
+    return separator.join(flat(list(items)))
 
 
 def flat_dict(src, target=None, prefix=""):
