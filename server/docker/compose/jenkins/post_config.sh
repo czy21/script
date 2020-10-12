3b50c5fb77565600d7f5791224b4a1cfa3ec07b6
Bruce CHEN <805899926@qq.com>
2020-10-13 01:52:51 +0800

    #Bruce Improve

--- server/docker/compose/jenkins/post_config.sh
+++ server/docker/compose/jenkins/post_config.sh
@@ -5,6 +5,3 @@ set -e
 dir=$(cd "$(dirname "$0")"; pwd)
 container_name="jenkins"
 sudo docker exec -i ${container_name} bash < $dir/install_tool.sh
-
-# /server/install_tool.sh
-#sudo docker exec -i ${container_name} bash < ../../../install_tool.sh
