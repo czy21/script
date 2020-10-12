c861cdd49418101643547f3b15f168eb9765caec
Bruce CHEN <805899926@qq.com>
2020-10-13 01:57:59 +0800

    #Bruce add make

--- server/docker/compose/jenkins/install_tool.sh
+++ server/docker/compose/jenkins/install_tool.sh
@@ -8,7 +8,7 @@ wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-
 sed -i -e '/mirrors.cloud.aliyuncs.com/d' -e '/mirrors.aliyuncs.com/d' /etc/yum.repos.d/CentOS-Base.repo
 yum clean all
 yum makecache
-yum -y install vim gcc epel-release zlib-devel bzip2-devel readline-devel sqlite-devel openssl-devel sudo
+yum -y install vim gcc epel-release zlib-devel bzip2-devel readline-devel sqlite-devel openssl-devel make sudo
 
 # pyenv
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
