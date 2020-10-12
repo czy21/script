cf3960fc0482bf6c1415378df4c8a65ebb02ca40
Bruce CHEN <805899926@qq.com>
2020-10-12 21:17:15 +0800

    #Bruce IMprove

--- server/docker/compose/jenkins/post_config.sh
+++ server/docker/compose/jenkins/post_config.sh
@@ -4,14 +4,21 @@ set -e
 
 dir=$(cd "$(dirname "$0")"; pwd)
 container_name="jenkins"
-sudo docker exec -i ${container_name} bash -c "sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories && apk update"
+sudo docker exec -i ${container_name} bash -c "
+  yum -y install wget
+  sudo mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+  wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-8.repo
+  sed -i -e '/mirrors.cloud.aliyuncs.com/d' -e '/mirrors.aliyuncs.com/d' /etc/yum.repos.d/CentOS-Base.repo
+  yum clean all
+  yum makecache
+  yum -y install vim gcc epel-release zlib-devel bzip2-devel readline-devel sqlite-devel openssl-devel sudo
 
-sudo docker exec -i ${container_name} bash -c "apk add python3 py3-pip sudo"
+  wget https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-community-common-8.0.21-1.el8.x86_64.rpm
+  rpm -ivh mysql-community-common-8.0.21-1.el8.x86_64.rpm
 
-sudo docker exec -i ${container_name} bash -c "mkdir ~/.pip/ && tee ~/.pip/pip.conf <<-'EOF'
-[global]
-index-url = https://mirrors.aliyun.com/pypi/simple/
+  wget https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-community-libs-8.0.21-1.el8.x86_64.rpm
+  rpm -ivh mysql-community-libs-8.0.21-1.el8.x86_64.rpm
 
-[install]
-trusted-host=mirrors.aliyun.com
-EOF"
+  wget https://cdn.mysql.com//Downloads/MySQL-8.0/mysql-community-client-8.0.21-1.el8.x86_64.rpm
+  rpm -ivh mysql-community-client-8.0.21-1.el8.x86_64.rpm
+"
