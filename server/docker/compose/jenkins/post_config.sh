f23d2c202957a9f7c61543d394baa8b3b1224c9f
Bruce CHEN <805899926@qq.com>
2020-10-13 14:03:17 +0800

    #Bruce Improve

--- server/docker/compose/jenkins/post_config.sh
+++ server/docker/compose/jenkins/post_config.sh
@@ -4,4 +4,12 @@ set -e
 
 dir=$(cd "$(dirname "$0")"; pwd)
 container_name="jenkins"
-sudo docker exec -i ${container_name} bash < $dir/install_tool.sh
+sudo docker exec -i ${container_name} bash -c "
+yum -y install wget
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-8.repo
+sed -i -e '/mirrors.cloud.aliyuncs.com/d' -e '/mirrors.aliyuncs.com/d' /etc/yum.repos.d/CentOS-Base.repo
+yum clean all
+yum makecache
+yum -y install vim sudo
+"
