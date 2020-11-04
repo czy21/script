75b57cdb63353282c50a770f78975b65810b71cb
Bruce CHEN <805899926@qq.com>
2020-11-05 02:09:37 +0800

    #

--- server/init-machine-centos.sh
+++ server/init-machine-centos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#sh init-machine-centos.sh -h user@host -t ali local
-# -t ali | offical local|remote
+#sh init-machine-centos.sh -h user@host -t ali
+# -t ali | offical
 # must as root
 set -e
 
@@ -21,7 +21,7 @@ while [[ $# -ge 1 ]]; do
 
     # install tools
     yum -y install wget
-    dnf -y install python38
+#    dnf -y install python38
     # edit sshd_config
     sed -i -r "s/ll='ls\s+-l/\0va/" /etc/profile.d/colorls.sh
     sed -i -r "s/\s*#\s*(%wheel\s+ALL=\(ALL\)\s+ALL)/\1/" /etc/sudoers
@@ -32,7 +32,7 @@ while [[ $# -ge 1 ]]; do
       PasswordAuthentication no
       ClientAliveInterval 30
       " >>/etc/ssh/sshd_config
-    wget -O /etc/yum.repos.d/VirtualBox.repo https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo
+#    wget -O /etc/yum.repos.d/VirtualBox.repo https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo
     if [ $1 == 'ali' ]; then
       # use ali yum.repo
       sudo mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
@@ -48,10 +48,9 @@ while [[ $# -ge 1 ]]; do
     passwd -d bruce
     sudo -u root bash -c "set -e;cd;mkdir .ssh;chmod 700 .ssh;echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3nTRJ/aVb67l1xMaN36jmIbabU7Hiv/xpZ8bwLVvNO3Bj7kUzYTp7DIbPcHQg4d6EsPC6j91E8zW6CrV2fo2Ai8tDO/rCq9Se/64F3+8oEIiI6E/OfUZfXD1mPbG7M/kcA3VeQP6wxNPhWBbKRisqgUc6VTKhl+hK6LwRTZgeShxSNcey+HZst52wJxjQkNG+7CAEY5bbmBzAlHCSl4Z0RftYTHR3q8LcEg7YLNZasUogX68kBgRrb+jw1pRMNo7o7RI9xliDAGX+E4C3vVZL0IsccKgr90222axsADoEjC9O+Q6uwKjahemOVaau+9sHIwkelcOcCzW5SuAwkezv 805899926@qq.com > .ssh/authorized_keys;chmod 644 .ssh/authorized_keys"
     sudo -u bruce bash -c "set -e;cd;mkdir .ssh;chmod 700 .ssh;echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3nTRJ/aVb67l1xMaN36jmIbabU7Hiv/xpZ8bwLVvNO3Bj7kUzYTp7DIbPcHQg4d6EsPC6j91E8zW6CrV2fo2Ai8tDO/rCq9Se/64F3+8oEIiI6E/OfUZfXD1mPbG7M/kcA3VeQP6wxNPhWBbKRisqgUc6VTKhl+hK6LwRTZgeShxSNcey+HZst52wJxjQkNG+7CAEY5bbmBzAlHCSl4Z0RftYTHR3q8LcEg7YLNZasUogX68kBgRrb+jw1pRMNo7o7RI9xliDAGX+E4C3vVZL0IsccKgr90222axsADoEjC9O+Q6uwKjahemOVaau+9sHIwkelcOcCzW5SuAwkezv 805899926@qq.com > .ssh/authorized_keys;chmod 644 .ssh/authorized_keys"
-    if [ $2 == 'local' ]; then
-      systemctl disable firewalld
-      sed -i -r "s/SELINUX=enforcing/SELINUX=disabled/" /etc/selinux/config
-    fi
+
+    systemctl disable firewalld
+    sed -i -r "s/SELINUX=enforcing/SELINUX=disabled/" /etc/selinux/config
 
     yum -y install vim git
 
