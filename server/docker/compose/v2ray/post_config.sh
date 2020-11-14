3fc76933e6ad70e49283b19e1ee9b8abf66063b5
Bruce CHEN <805899926@qq.com>
2020-11-14 17:32:20 +0800

    #Bruce Improve

--- server/docker/compose/v2ray/post_config.sh
+++ server/docker/compose/v2ray/post_config.sh
@@ -2,4 +2,10 @@
 
 set -e
 
+dir=$(cd "$(dirname "$0")"; pwd)
+
 sudo docker network connect nginx_default v2ray
+
+sudo cp -r $dir/nginx/v2ray.conf /data/config/nginx/conf.d/
+
+sudo docker restart nginx
