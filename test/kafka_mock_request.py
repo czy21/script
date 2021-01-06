d32fbbafa3855924126d64c378c254c8d25c8f40
Bruce CHEN <805899926@qq.com>
2021-01-06 17:50:10 +0800

    #

--- test/kafka_mock_request.py
+++ test/kafka_mock_request.py
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import json
+
 import requests
 
 
-def publish():
-    r = requests.post(url="http://127.0.0.1:8075/user/publish",
-                      data=json.dumps({'message': '1'}),
-                      headers={'Content-Type': 'application/json'})
-    print(r)
+def publish(max):
+    for i in range(0, max):
+        r = requests.post(url="http://127.0.0.1:8075/user/publish",
+                          data=json.dumps({'message': (i + 1).__str__()}),
+                          headers={'Content-Type': 'application/json'})
 
 
 if __name__ == '__main__':
-    publish()
+    publish(10)
