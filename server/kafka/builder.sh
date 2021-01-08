996f067b4e1f3d255b6062a47e5530151ec355c3
Bruce CHEN <805899926@qq.com>
2021-01-08 15:55:24 +0800

    #Bruce Improve

---
+++ server/kafka/builder.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# sh builder.sh --init user@host --tag <image_tag>
+# sh builder.sh --tag <image_tag>
+
+while [[ $# -ge 1 ]];
+do
+	case $1 in
+		--init)
+			if [[ ! $2 ]] || [[ "$2" =~ ^"--".* ]]; then
+			  echo -e "\033[31m$1 value is null \033[0m"
+			  shift 1
+        continue
+      fi
+      host=$2
+      shift 2
+      ssh $host "rm -rf ./kafka/" && scp -r ../kafka/ $host:
+      exec_sh='$HOME/kafka/builder.sh '$@''
+      ssh ${host} "${exec_sh}"
+      break
+			;;
+		--tag)
+			if [[ ! $2 ]] || [[ "$2" =~ ^"--".* ]]; then
+			  echo -e "\033[31m$1 value is null \033[0m"
+			  shift 1
+        continue
+      fi
+      ls -al
+      sudo docker-compose --file kafka/docker-compose.yml up -d
+#      sudo docker-compose up -d
+      rm -rf kafka/
+      shift 2
+			;;
+		*)
+		  echo -e "\033[31m$1 un_know input param \033[0m"
+			break
+			;;
+	esac
+done
