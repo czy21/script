c2a6ae608828601a1f7f25691e19d94088f8bd2d
Bruce CHEN <805899926@qq.com>
2021-01-25 21:35:53 +0800

    #Bruce Improve

--- server/docker/compose/kafka/init_config.sh
+++ server/docker/compose/kafka/init_config.sh
@@ -8,3 +8,8 @@ sudo rm -rf /data/config/zookeeper/
 sudo mkdir -p /data/config/zookeeper/conf/
 
 sudo cp -r $dir/zookeeper_conf/* /data/config/zookeeper/
+
+sudo rm -rf /data/config/kafka_eagle/
+sudo mkdir -p /data/config/kafka_eagle/conf/
+
+sudo cp -r $dir/kafka_eagle_conf/* /data/config/kafka_eagle/
