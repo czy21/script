b78342473b451e2f9449175f937a1b51dcf2e19a
Bruce CHEN <805899926@qq.com>
2021-01-28 11:23:36 +0800

    #Bruce Improve

--- server/docker/compose/hadoop/init_config.sh
+++ server/docker/compose/hadoop/init_config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 set -e
+dir=$(cd "$(dirname "$0")"; pwd)
+source ${dir}/../.env.global
 
-sudo mkdir -p /data/volumes/hadoop/sys/fs/cgroup/
+sudo mkdir -p ${GLOBAL_VOLUMES_DIR}/hadoop/sys/fs/cgroup/
--- server/docker/compose/portainer/init_config.sh
+++ server/docker/compose/portainer/init_config.sh
@@ -3,3 +3,4 @@
 set -e
 
 dir=$(cd "$(dirname "$0")"; pwd)
+source ${dir}/../.env.global
