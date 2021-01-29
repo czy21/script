534b924edb70dd1e8706ed64c474d6e72884dd99
Bruce CHEN <805899926@qq.com>
2021-01-29 13:15:03 +0800

    #

--- server/docker/compose/kafka/kafka_eagle/run.sh
+++ server/docker/compose/kafka/kafka_eagle/run.sh
@@ -1,37 +1,38 @@
-#!/bin/bash
-echo "" >> "$KE_HOME/conf/system-config.properties"
+#!/bin/bash -e
 
-(
-    function updateConfig() {
-        key=$1
-        value=$2
-        file=$3
+echo "" > "$KE_HOME/conf/system-config.properties"
 
-        # Omit $value here, in case there is sensitive information
-        echo "[Configuring] '$key' in '$file'"
+function updateConfig() {
+    key=$1
+    value=$2
+    file=$3
 
-        # If config exists in file, replace it. Otherwise, append to file.
-        if grep -E -q "^#?$key=" "$file"; then
-            sed -r -i "s@^#?$key=.*@$key=$value@g" "$file" #note that no config values may contain an '@' char
-        else
-            echo "$key=$value" >> "$file"
-        fi
-    }
+    # Omit $value here, in case there is sensitive information
+    echo "[Configuring] '$key' in '$file'"
 
-    IFS=$'\n'
-    for VAR in $(env)
-    do
-        env_var=$(echo "$VAR" | cut -d= -f1)
-        if [[ $env_var =~ ^SYS_ ]]; then
-            sys_name=$(echo "$env_var" | cut -d_ -f2- | tr '[:upper:]' '[:lower:]' | tr _ .)
-            updateConfig "$sys_name" "${!env_var}" "$KE_HOME/conf/system-config.properties"
-        fi
+    # If config exists in file, replace it. Otherwise, append to file.
+    if grep -E -q "^#?$key=" "$file"; then
+        sed -r -i "s@^#?$key=.*@$key=$value@g" "$file" #note that no config values may contain an '@' char
+    else
+        echo "$key=$value" >> "$file"
+    fi
+}
 
-        if [[ $env_var =~ ^LOG4J_ ]]; then
-            log4j_name=$(echo "$env_var" | tr '[:upper:]' '[:lower:]' | tr _ .)
-            updateConfig "$log4j_name" "${!env_var}" "$KE_HOME/conf/log4j.properties"
-        fi
-    done
-)
+IFS=$'\n'
+for VAR in $(env)
+do
+    env_var=$(echo "$VAR" | cut -d= -f1)
+    if [[ $env_var =~ ^SYS_ ]]; then
+        sys_name=$(echo "$env_var" | cut -d_ -f2- | tr '[:upper:]' '[:lower:]' | tr _ .)
+        updateConfig "$sys_name" "${!env_var}" "$KE_HOME/conf/system-config.properties"
+    fi
 
-exec "$KE_HOME/bin/ke.sh start"
+    if [[ $env_var =~ ^LOG4J_ ]]; then
+        log4j_name=$(echo "$env_var" | tr '[:upper:]' '[:lower:]' | tr _ .)
+        updateConfig "$log4j_name" "${!env_var}" "$KE_HOME/conf/log4j.properties"
+    fi
+done
+
+$KE_HOME/bin/ke.sh start
+sleep 3
+tail -f /usr/local/kafka_eagle/logs/log.log
