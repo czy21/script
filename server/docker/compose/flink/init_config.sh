7400e25810779eb9eae299871598725ecf2f1b0a
Bruce CHEN <805899926@qq.com>
2021-02-23 09:37:52 +0800

    #

--- server/docker/compose/flink/init_config.sh
+++ server/docker/compose/flink/init_config.sh
@@ -4,3 +4,8 @@ set -e
 
 dir=$(cd "$(dirname "$0")"; pwd)
 source ${dir}/../.env.global
+
+sudo rm -rf ${GLOBAL_VOLUMES_DIR}/jobmanager/
+sudo mkdir -p ${GLOBAL_VOLUMES_DIR}/jobmanager/
+
+sudo cp -r $dir/___temp/jar/* ${GLOBAL_VOLUMES_DIR}/jobmanager/
