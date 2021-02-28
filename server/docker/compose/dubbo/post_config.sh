607ce729eb945c5e71d01f8a203bf49d76789856
Bruce CHEN <805899926@qq.com>
2021-03-01 00:26:00 +0800

    #

---
+++ server/docker/compose/dubbo/post_config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+dir=$(cd "$(dirname "$0")"; pwd)
+source ${dir}/../.env.global
+
+sudo docker network connect dubbo_default zookeeper1
+sudo docker network connect dubbo_default zookeeper2
