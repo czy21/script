23d08bff686662c567245d4f2a7caec9bd06079f
Bruce CHEN <805899926@qq.com>
2021-03-07 14:59:07 +0800

    #

--- test/resolve_excel.py
+++ test/resolve_excel.py
@@ -3,9 +3,10 @@ import datetime, time, uuid
 from sqlalchemy import create_engine
 from pandas import DataFrame
 
-engine = create_engine('mysql+pymysql://admin:***REMOVED***@192.168.168.140:3306/erp_local')
+engine = create_engine('mysql+pymysql://admin:***REMOVED***@192.168.2.4:3306/erp_local')
 
 startTime = datetime.datetime.now()
+
 
 def read_original_excel(
         df: DataFrame,
@@ -13,24 +14,15 @@ def read_original_excel(
         column_mapping: dict,
         table_name: str
 ):
-    df = pd.read_excel(f, sheet_name)
+    df = pd.read_excel(df, sheet_name)
     df.rename(columns=column_mapping, inplace=True)
     df["file_id"] = uuid.uuid4()
     df.to_sql(table_name, engine, if_exists="append")
 
 with pd.ExcelFile(path_or_buffer="demo.xlsx") as f:
-    df1 = pd.read_excel(f, "man")
-    df1.rename(columns={"姓名": "name", "年龄": "age", "性别": "gender", "地址": "address"}, inplace=True)
-    df1["file_id"] = uuid.uuid4()
-    df1.to_sql('ent_datacenter_original_man', engine, if_exists="append")
-    read_original_excel()
-
-    df2 = pd.read_excel(f, "woman")
-    df2.rename(columns={"姓名": "name", "年龄": "age", "性别": "gender", "地址": "address"}, inplace=True)
-    df2["file_id"] = uuid.uuid4()
-    df2.to_sql('ent_datacenter_original_woman', engine, if_exists="append")
+    read_original_excel(df=f, sheet_name="man", column_mapping={"姓名": "name", "年龄": "age", "性别": "gender", "地址": "address"}, table_name="ent_sys_man")
+    read_original_excel(df=f, sheet_name="woman", column_mapping={"姓名": "name", "年龄": "age", "性别": "gender", "地址": "address"}, table_name="ent_sys_woman")
     print("读取指定行的数据：\n{0}".format(f))
-
 
 endtime = datetime.datetime.now()
 
