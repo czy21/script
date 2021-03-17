76f39d9b8e476f97c14578aba0c6a7f241924b50
Bruce CHEN <805899926@qq.com>
2021-03-17 21:54:44 +0800

    #

--- test/docker_api.py
+++ test/docker_api.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 import docker
 from docker import errors
+from compose import config
+import logging
 
 client = docker.from_env()
 app_network_name = "jenkins_default"
-try:
-    app_network = client.api.inspect_network(app_network_name)
-except errors.NotFound:
-    print("创建", app_network_name)
 
-else:
-    print("存在", app_network_name)
 
-app_network_containers = [c["Name"] for c in app_network["Containers"].values()]
+def test_network():
+    try:
+        client.api.remove_image(app_network_name)
+    except errors.NotFound:
+        print("创建", app_network_name)
+
+    else:
+        print("存在", app_network_name)
+
+
+def test_compose():
+    config.find(".",)
 
 print("sss")
-# for item in client.networks.list():
-#     print(item)
