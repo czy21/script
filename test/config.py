b340fbf6e9ef357b872a5bbd7515452e9911fbfa
Bruce CHEN <805899926@qq.com>
2021-03-18 22:47:57 +0800

    #

---
+++ test/config.py
@@ -0,0 +1,2 @@
+MONGO_HOST = "mongodb://admin:Czy.190815@192.168.2.11:27017/"
+MYSQL_HOST = "mysql+pymysql://admin:Czy.190815@192.168.2.11:3306/spi_local"
--- test/resolve_excel.py
+++ test/resolve_excel.py
@@ -1,25 +1,40 @@
 import profile
 
 import pandas as pd
-import datetime, time, uuid
+import datetime, time, uuid, config
 from sqlalchemy import create_engine
 from pymongo import MongoClient
 
-engine = create_engine('mysql+pymysql://admin:Czy.190815@192.168.168.140:3306/spi_local')
-mongoClient = MongoClient(host="mongodb://admin:Czy.190815@192.168.168.140:27017/")
+engine = create_engine(config.MYSQL_HOST)
+mongoClient = MongoClient(host=config.MONGO_HOST)
 
 startTime = datetime.datetime.now()
 
-with pd.ExcelFile(path_or_buffer="test_inspect.xlsx") as f:
+
+def validate_null(cell):
+    print(cell)
+
+
+def validate(series):
+    error_msgs = []
+    if pd.isna(series["to_institution_address"]):
+        error_msgs.append("客户地址为空")
+    if pd.isna(series["remark"]):
+        error_msgs.append("无备注")
+    return ",".join(error_msgs)
+
+
+with pd.ExcelFile(path_or_buffer="demo.xlsx") as f:
     table = mongoClient["spi_local"]["ent_file_column_mapping"]
     sd_mapping_list = table.find_one({"businessType": "SD"})["fields"]
     sd_mapping_dict = {}
     for t in sd_mapping_list:
         sd_mapping_dict["*" + t["header"] if "required" in t and t["required"] else t["header"]] = t["column"]
-    df = pd.read_excel(f, "SD")
+    df = pd.read_excel(f, "SD", usecols=lambda x: x in sd_mapping_dict.keys())
     df.rename(columns=sd_mapping_dict, inplace=True)
     df["file_id"] = uuid.uuid4()
-    df.to_sql( "ent_sfl_inspect_sale", engine, if_exists="append",schema={"from_institution_code":"dd"})
+    df["error"] = df.apply(validate, axis=1)
+    df.to_sql("ent_sfl_inspect_sale", engine, if_exists="append")
 
 endtime = datetime.datetime.now()
 
