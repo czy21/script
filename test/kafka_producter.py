032d31cbf87f48e371c7650224eeeb1715707dc7
Bruce CHEN <805899926@qq.com>
2021-04-04 23:03:45 +0800

    #

--- test/kafka_producter.py
+++ test/kafka_producter.py
@@ -6,5 +6,5 @@ bootstrap_servers = ['192.168.110.140:19092']
 topic_name = 'person'
 
 producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
-producer.send(topic_name, b'Hello World')
+producer.send(topic_name, b'')
 producer.flush()
