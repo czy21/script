630f96bdb3b3a57178e46d868e125bb1cf4df606
Bruce CHEN <805899926@qq.com>
2021-04-11 02:08:00 +0800

    #

--- domain/default/common.py
+++ domain/default/common.py
@@ -51,7 +51,7 @@ param_api_network_containers = []
 param_api_image = ""
 param_api_root_project_path = ""
 param_api_docker_gradle_command = True
-param_api_gradle_init_script_file_path = path_util.pure_path_join(default_path.script_template, "init.gradle")
+param_api_gradle_init_script_file_path = path_util.pure_path_join(default_path.script_template, "java/init.gradle")
 param_api_gradle_plugin_file_path = path_util.pure_path_join(default_path.project_plugin, "build.gradle")
 
 param_api_jmx_port = 3010
