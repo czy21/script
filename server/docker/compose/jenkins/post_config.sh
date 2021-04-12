72115701ec62ed2e2254279495ae25a0fc7d9cd5
Bruce CHEN <805899926@qq.com>
2021-04-13 01:52:25 +0800

    #

--- server/docker/compose/jenkins/post_config.sh
+++ server/docker/compose/jenkins/post_config.sh
@@ -3,7 +3,10 @@
 set -e
 
 dir=$(cd "$(dirname "$0")"; pwd)
+source ${dir}/../../.env.global
 container_name="jenkins"
+
+sudo docker cp ${dir}/___temp/ ${container_name}:/var/jenkins_home/
 sudo docker exec -i ${container_name} bash -c "
 yum -y install wget
 yum -y install vim sudo
