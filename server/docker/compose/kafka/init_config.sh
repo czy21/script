75ca656ea88f17c825cc221259127588fe703874
Bruce CHEN <805899926@qq.com>
2021-04-15 00:20:28 +0800

    #

--- server/docker/compose/kafka/init_config.sh
+++ server/docker/compose/kafka/init_config.sh
@@ -4,13 +4,3 @@ set -e
 
 dir=$(cd "$(dirname "$0")"; pwd)
 source ${dir}/../../.env.global
-
-sudo rm -rf ${GLOBAL_CONFIG_DIR}/zookeeper/
-sudo mkdir -p ${GLOBAL_CONFIG_DIR}/zookeeper/
-
-sudo cp -r $dir/zookeeper_conf/* ${GLOBAL_CONFIG_DIR}/zookeeper/
-
-sudo rm -rf ${GLOBAL_CONFIG_DIR}/kafka_eagle/
-sudo mkdir -p ${GLOBAL_CONFIG_DIR}/kafka_eagle/
-
-sudo cp -r $dir/kafka_eagle/conf/* ${GLOBAL_CONFIG_DIR}/kafka_eagle/
