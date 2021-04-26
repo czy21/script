11652feca6e60796edaad6984e176cea4e706415
Bruce CHEN <805899926@qq.com>
2021-04-27 00:37:18 +0800

    #

--- server/docker/compose/nexus/post_config.sh
+++ server/docker/compose/nexus/post_config.sh
@@ -4,14 +4,7 @@ set -e
 
 dir=$(cd "$(dirname "$0")"; pwd)
 source ${dir}/../../.env.global
-container_name="jenkins"
-
-sudo docker cp ${dir}/___temp/ ${container_name}:/var/jenkins_home/
-sudo docker exec -i ${container_name} bash -c "
-yum -y install wget
-yum -y install vim sudo
-dnf -y install python38
-"
+container_name="nexus"
 
 sudo cat $dir/nginx/nginx.conf > ${GLOBAL_CONFIG_DIR}/nginx/conf.d/${container_name}.conf
 
