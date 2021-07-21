619fddc01aefdb64bd5011b583524621851a981a
Bruce CHEN <805899926@qq.com>
2021-07-22 01:03:55 +0800

    #

--- server/docker/compose/jdk11/compile.sh
+++ server/docker/compose/jdk11/compile.sh
@@ -6,5 +6,6 @@ cd /opt/jvm
 rm -rf /opt/jvm/*
 git clone https://gitee.com/czyhome/jdk11.git
 apt-get install -y build-essential zip unzip make autoconf libx11-dev libxext-dev libxrender-dev libxtst-dev libxt-dev libcups2-dev libfontconfig1-dev libasound2-dev
+cd jdk11
 bash configure
 make
