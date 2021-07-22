901d658a4cb964c4a8f1a68c15c41cf4f7a86cbe
Bruce CHEN <805899926@qq.com>
2021-07-22 12:33:39 +0800

    #

--- server/docker/compose/jdk11/compile.sh
+++ server/docker/compose/jdk11/compile.sh
@@ -4,8 +4,10 @@ set -e
 
 cd /opt/jvm
 rm -rf /opt/jvm/*
-git clone https://gitee.com/czyhome/jdk11.git
+mkdir -p /opt/jvm/jdk11
+git clone https://gitee.com/czyhome/jdk11u.git jdk11
 apt-get install -y build-essential zip unzip make autoconf libx11-dev libxext-dev libxrender-dev libxtst-dev libxt-dev libcups2-dev libfontconfig1-dev libasound2-dev
 cd jdk11
+#bash configure --with-num-cores=4 --with-memory-size=8192 --disable-warnings-as-errors
 bash configure
-make images --disable-warnings-as-errors
+make images
