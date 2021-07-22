5ab55375b0426862af9bd5ca136a03cadcafe8c8
Bruce CHEN <805899926@qq.com>
2021-07-22 20:31:18 +0800

    #

--- server/docker/compose/jdk11/compile.sh
+++ server/docker/compose/jdk11/compile.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 set -e
-
-cd /opt/jvm
-rm -rf /opt/jvm/*
-mkdir -p /opt/jvm/jdk11
-git clone --branch jdk-11.0.11-ga https://gitee.com/czyhome/jdk11u.git jdk11
-apt-get install -y openjdk-11-jdk cmake gdb build-essential zip unzip make autoconf libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev libcups2-dev libfontconfig1-dev libasound2-dev
-cd jdk11
-echo echo -e "\033[32m start build"
-bash configure
-make images
+jvm_dir=/opt/jvm
+jdk11_dir=${jvm_dir}/jdk11
+if [ ! -d "${jdk11_dir}/build/" ];then
+  rm -rf ${jdk11_dir} && mkdir -p ${jdk11_dir}
+  git clone --branch jdk-11.0.11-ga https://gitee.com/czyhome/jdk11u.git ${jdk11_dir}
+  apt-get install -y openjdk-11-jdk cmake gdb build-essential zip unzip make autoconf libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev libcups2-dev libfontconfig1-dev libasound2-dev
+  cd ${jdk11_dir}
+  echo echo -e "\033[32m start build"
+  bash configure
+  make images
+fi
