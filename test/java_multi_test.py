8709070689198aaadee40eccf249e5c05253b1a3
Bruce CHEN <805899926@qq.com>
2021-07-26 19:08:20 +0800

    #

--- test/java_multi_test.py
+++ test/java_multi_test.py
@@ -7,28 +7,32 @@ import requests
 
 
 def detail(x):
-    r = requests.post(url="http://192.168.2.21:8076/user/detail",
+    r = requests.post(url="http://127.0.0.1:8080/user/detail",
                       data=json.dumps({
                           "id": "2f5aa878-352d-49a9-a18e-188449e9e649"
                       }),
                       headers={
                           'Content-Type': 'application/json'
                       })
-    ret = " ".join([
-        time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
-        str(x),
-        str(r.json()),
-        str(time.time())
-    ])
-    print(ret)
-    return ret
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    current_timestamp = str(time.time())
+    current_user = str(x + 1)
+    response_data = r.json()
+    map = {
+        "current_user": current_user,
+        "current_time": current_time,
+        "response_data": response_data,
+        "current_timestamp": current_timestamp
+    }
+    return map
 
 
 if __name__ == '__main__':
     print('MainThread %s is running...' % threading.current_thread().name)
     result = []
     with ThreadPoolExecutor(16) as executor:
-        for data in executor.map(detail, range(10000)):
+        for data in executor.map(detail, range(100)):
             result.append(data)
-    print(result)
+    for t in result:
+        print(t)
     print('MainThread %s ended.' % threading.current_thread().name)
