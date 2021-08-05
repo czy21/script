80e3bc665d1921058ffcf816816d108e3d3a003d
Bruce CHEN <805899926@qq.com>
2021-08-06 01:46:49 +0800

    #

---
+++ server/docker/compose/hadoop/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+dir=$(cd "$(dirname "$0")"; pwd)
+source ${dir}/../../.env.global
+
+docker build --tag registry:5000/hadoop --file ${dir}/Dockerfile ${dir}/ --no-cache --force-rm
+
+docker push registry:5000/hadoop
+
+docker image prune -f
--- server/docker/compose/hadoop/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-dir=$(cd "$(dirname "$0")"; pwd)
-source ${dir}/../../.env.global
-
-sudo mkdir -p ${GLOBAL_VOLUMES_DIR}/hadoop/sys/fs/cgroup/
