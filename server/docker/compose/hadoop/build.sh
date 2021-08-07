f907d0723b322f85116bf39f65b6b752d758db29
Bruce CHEN <805899926@qq.com>
2021-08-07 19:13:01 +0800

    #

--- server/docker/compose/hadoop/build.sh
+++ server/docker/compose/hadoop/build.sh
@@ -5,7 +5,7 @@ set -e
 dir=$(cd "$(dirname "$0")"; pwd)
 source ${dir}/../../.env.global
 
-docker build --tag registry:5000/hadoop --file ${dir}/Dockerfile ${dir}/ --no-cache --force-rm
+docker build --tag registry:5000/hadoop --file ${dir}/Dockerfile ${dir}/
 
 docker push registry:5000/hadoop
 
--- server/docker/compose/hadoop/docker-entrypoint.sh
+++ server/docker/compose/hadoop/docker-entrypoint.sh
@@ -2,5 +2,10 @@
 set -e
 
 service ssh start
+# start hadoop
+$HADOOP_HOME/bin/hdfs namenode -format
+$HADOOP_HOME/sbin/start-dfs.sh
 
+# start hbase
+$HBASE_HOME/bin/start-hbase.sh
 exec "$@"
