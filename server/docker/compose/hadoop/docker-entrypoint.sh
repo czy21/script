48a4b4062e886e5d817e4293454b9765c68861f7
Bruce CHEN <805899926@qq.com>
2021-08-09 15:18:31 +0800

    #

--- server/docker/compose/hadoop/docker-entrypoint.sh
+++ server/docker/compose/hadoop/docker-entrypoint.sh
@@ -3,9 +3,19 @@ set -e
 
 service ssh start
 
-$HADOOP_HOME/bin/hdfs namenode -format
+hdfs_dir=$HADOOP_HOME/hdfs/
+
+if [ "$(ls -A ${hdfs_dir})" ]; then
+  echo -e "\033[32m hdfs is not empty \033[0m"
+else
+  echo -e "\033[32m hdfs namenode format... \033[0m"
+  $HADOOP_HOME/bin/hdfs namenode -format
+fi
+
+echo -e "\033[32m start dfs... \033[0m"
 $HADOOP_HOME/sbin/start-dfs.sh
 
+echo -e "\033[32m start hbase... \033[0m"
 $HBASE_HOME/bin/start-hbase.sh
 
 exec "$@"
