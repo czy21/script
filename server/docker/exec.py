10941e9a7c0d5db189ec8815ee68d9d6caada265
Bruce CHEN <805899926@qq.com>
2021-08-25 13:56:30 +0800

    #

--- server/docker/exec.py
+++ server/docker/exec.py
@@ -2,45 +2,8 @@
 import argparse
 import subprocess
 import sys
-from itertools import zip_longest
+import share
 from pathlib import Path
-
-
-def get_install_tuple(join_path):
-    app_paths = [p for p in Path(__file__).parent.joinpath(join_path).iterdir() if p.is_dir()]
-    # group by
-    list_str = [list(t) for t in zip_longest(*[iter([".".join([str(i), p.name]) for i, p in enumerate(app_paths, start=1)])] * 8, fillvalue='')]
-    # get every column max length
-    column_widths = [len(max([t[p] for t in list_str for p in range(len(t)) if p == i], key=len, default='')) for i in range(len(list_str[0]))]
-    for t in list_str:
-        print("".join([str(t[p]).ljust(column_widths[o] + 2) for p in range(len(t)) for o in range(len(column_widths)) if p == o]))
-    app_options = input("please select app number(example:1 2 3) ").strip().split()
-    return [(int(t), app_paths.__getitem__(int(t) - 1)) for t in app_options if t in [str(i) for i, p in enumerate(app_paths, start=1)]]
-
-
-def select_one_option():
-    list_dir = [p for p in Path(__file__).parent.iterdir() if p.is_dir()]
-
-    print("\n==========")
-
-    for i, p in enumerate(list_dir, start=1):
-        print(" ".join([str(i), p.name]))
-
-    one_option = input("please select one option(example:1) ").strip()
-
-    if one_option == '':
-        return []
-
-    elif not one_option.isnumeric():
-        print("\ninvalid option")
-        sys.exit()
-
-    one_option = int(one_option)
-
-    if not one_option in [i for i, p in enumerate(list_dir, start=1)]:
-        print(" ".join(["\n", one_option, "not exist"]))
-        sys.exit()
-    return list_dir[one_option - 1]
 
 
 def execute(app_tuples, func, args):
@@ -132,17 +95,17 @@ if __name__ == '__main__':
                 "&& echo ${" + t + "}"
             ]))
     if args.b:
-        selected_option = select_one_option()
+        selected_option = share.select_one_option()
         if selected_option:
-            selected_tuple = get_install_tuple(Path(selected_option).name)
+            selected_tuple = share.get_install_tuple(Path(selected_option).name)
             execute(selected_tuple, build, args)
     if args.i:
-        selected_option = select_one_option()
+        selected_option = share.select_one_option()
         if selected_option:
-            selected_tuple = get_install_tuple(Path(selected_option).name)
+            selected_tuple = share.get_install_tuple(Path(selected_option).name)
             execute(selected_tuple, init, args)
     if args.c:
-        selected_option = select_one_option()
+        selected_option = share.select_one_option()
         if selected_option:
-            selected_tuple = get_install_tuple(Path(selected_option).name)
+            selected_tuple = share.get_install_tuple(Path(selected_option).name)
             execute(selected_tuple, post, args)
--- utility/share.sh
+++ utility/share.sh
@@ -4,14 +4,15 @@
 function upload_exec_py() {
   pwd_path=$(pwd)
   name_path=`basename ${pwd_path}`
+  utility_dir=$(cd ${pwd_path}/../../utility; pwd)
   ssh $host 'rm -rf $HOME/'${name_path}';'
   scp -r ${pwd_path} $host:
-  ssh $host 'python3 $HOME/'${name_path}/'exec.py '$@';'
 
+  scp -r "${utility_dir}/share.py" $host:${name_path}
+
+  ssh $host 'python3 -B $HOME/'${name_path}/'exec.py '$@';'
   ssh $host 'rm -rf $HOME/'${name_path}';'
 }
-
-
 
 function upload_exec_sh() {
   pwd_path=$(pwd)
