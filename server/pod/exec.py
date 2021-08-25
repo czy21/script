7e1f90a2f5a8ba3b60f3e4d5700f4d1105430950
Bruce CHEN <805899926@qq.com>
2021-08-25 13:19:57 +0800

    #

--- server/pod/exec.py
+++ server/pod/exec.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 import subprocess
 import sys
+from itertools import zip_longest
 from pathlib import Path
 
 
 def get_install_tuple(join_path):
     app_paths = [p for p in Path(__file__).parent.joinpath(join_path).iterdir() if p.is_dir()]
-    for i, p in enumerate(app_paths, start=1):
-        print(" ".join([str(i), p.name]))
+    # group by
+    list_str = [list(t) for t in zip_longest(*[iter([".".join([str(i), p.name]) for i, p in enumerate(app_paths, start=1)])] * 8, fillvalue='')]
+    # get every column max length
+    column_widths = [len(max([t[p] for t in list_str for p in range(len(t)) if p == i], key=len, default='')) for i in range(len(list_str[0]))]
+    for t in list_str:
+        print("".join([str(t[p]).ljust(column_widths[o] + 2) for p in range(len(t)) for o in range(len(column_widths)) if p == o]))
     app_options = input("please select app number(example:1 2 3) ").strip().split()
     return [(int(t), app_paths.__getitem__(int(t) - 1)) for t in app_options if t in [str(i) for i, p in enumerate(app_paths, start=1)]]
 
@@ -16,10 +21,8 @@ def select_one_option():
     list_dir = [p for p in Path(__file__).parent.iterdir() if p.is_dir()]
 
     print("\n==========")
-
     for i, p in enumerate(list_dir, start=1):
         print(" ".join([str(i), p.name]))
-
     print("\n==========")
     one_option = input("please select one option(example:1) ").strip()
 
@@ -32,7 +35,7 @@ def select_one_option():
 
     one_option = int(one_option)
 
-    if not one_option in [i for i, p in enumerate(list_dir, start=1)]:
+    if one_option not in [i for i, p in enumerate(list_dir, start=1)]:
         print(" ".join(["\n", one_option, "not exist"]))
         sys.exit()
     return list_dir[one_option - 1]
