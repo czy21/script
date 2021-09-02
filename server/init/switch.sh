7b13e153164a7e6f9b5189754af84ed576b62640
Bruce CHEN <805899926@qq.com>
2021-09-02 23:59:49 +0800

    #

--- server/init/switch.sh
+++ server/init/switch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # bash switch.sh -h user@host -t [target]
-# -t centos | ubuntu | docker | set-hosts | k8s-master | k8s-nodes | cp-certs
+# -t centos | ubuntu | docker | set-hosts | k8s-master | k8s-nodes | cp-certs | calico-prune
 set -e
 
 while getopts ":h:t:" opt
