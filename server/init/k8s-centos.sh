a5b655e7577bebde51a5d63d915617822a04a342
Bruce CHEN <805899926@qq.com>
2021-09-04 13:39:50 +0800

    #

--- server/init/k8s-centos.sh
+++ server/init/k8s-centos.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
 sudo cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
--- server/init/k8s-ubuntu.sh
+++ server/init/k8s-ubuntu.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e
 
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+# install k8s
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
