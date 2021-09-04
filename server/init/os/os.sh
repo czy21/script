8fd4f9ed8341117045b096f0ce313213c8a9d94d
Bruce CHEN <805899926@qq.com>
2021-09-04 17:59:27 +0800

    #

--- server/init/os/os.sh
+++ server/init/os/os.sh
@@ -4,9 +4,7 @@ set -e
 dir=$(cd "$(dirname "$0")"; pwd)
 
 timedatectl set-timezone Asia/Shanghai
-
 os_type=$(awk -F= '/^ID=/{gsub("\"","",$2);print $2}' /etc/os-release)
-echo $os_type
 if [ ${os_type} == 'centos' ]; then
     bash ${dir}/os-centos.sh
 elif [ ${os_type} == 'ubuntu' ]; then
