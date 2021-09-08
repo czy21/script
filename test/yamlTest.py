33513c2968a80a7808ead752a2e14f77eb530834
Bruce CHEN <805899926@qq.com>
2021-09-08 22:07:16 +0800

    #

--- test/yamlTest.py
+++ test/yamlTest.py
@@ -6,6 +6,16 @@ from pathlib import Path
 from ruamel import yaml
 
 
+def _str_representer(dumper: yaml.Dumper, data):
+    style = ''
+    if '\n' in data:
+        style = '|'
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style)
+
+
+yaml.RoundTripRepresenter.add_representer(str, _str_representer)
+
+
 def split(root_path: str):
     rp = Path(root_path)
     for d in rp.iterdir():
@@ -30,15 +40,16 @@ def split(root_path: str):
             templates_path = d.joinpath("templates")
             shutil.rmtree(path=templates_path, ignore_errors=True)
             templates_path.mkdir(parents=True, exist_ok=True)
-            fo = open(f, "r", encoding="utf-8")
-            y = yaml.load_all(fo, Loader=yaml.UnsafeLoader)
-            for content in y:
-                if content and content["kind"]:
-                    kind_path = f.joinpath(templates_path).joinpath(str(content["kind"]).lower() + ".yaml")
-                    with io.open(kind_path, "w+", encoding="utf-8") as y_file:
-                        yaml.dump(content, y_file, default_flow_style=False)
-            fo.close()
-            os.remove(f)
+            shutil.move(f, templates_path)
+            # fo = open(f, "r", encoding="utf-8")
+            # y = yaml.load_all(fo, Loader=yaml.UnsafeLoader)
+            # for content in y:
+            #     if content and content["kind"]:
+            #         kind_path = f.joinpath(templates_path).joinpath(str(content["kind"]).lower() + ".yaml")
+            #         with io.open(kind_path, "w+", encoding="utf-8") as y_file:
+            #             yaml.dump(content, y_file, default_flow_style=False, explicit_start=True, Dumper=yaml.RoundTripDumper)
+            # fo.close()
+            # os.remove(f)
 
 
 if __name__ == '__main__':
