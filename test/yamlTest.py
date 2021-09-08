01e8cda58d477f39faceb0cc8a81cdb1d5565245
Bruce CHEN <805899926@qq.com>
2021-09-08 23:28:07 +0800

    #

--- test/yamlTest.py
+++ test/yamlTest.py
@@ -1,6 +1,7 @@
 import io
 import os
 import shutil
+import sys
 from pathlib import Path
 
 from ruamel import yaml
@@ -33,25 +34,30 @@ def split(root_path: str):
             values_content = {
                 "namespace": rp.name
             }
-            with io.open(values_path, "w+", encoding="utf-8") as v_file:
+            with io.open(values_path, "w+", encoding="utf-8", newline="\n") as v_file:
                 yaml.dump(values_content, v_file, default_flow_style=False)
-            with io.open(chart_path, "w+", encoding="utf-8") as c_file:
+            with io.open(chart_path, "w+", encoding="utf-8", newline="\n") as c_file:
                 yaml.dump(chart_content, c_file, default_flow_style=False)
             templates_path = d.joinpath("templates")
             shutil.rmtree(path=templates_path, ignore_errors=True)
             templates_path.mkdir(parents=True, exist_ok=True)
-            shutil.move(f, templates_path)
-            # fo = open(f, "r", encoding="utf-8")
-            # y = yaml.load_all(fo, Loader=yaml.UnsafeLoader)
-            # for content in y:
-            #     if content and content["kind"]:
-            #         kind_path = f.joinpath(templates_path).joinpath(str(content["kind"]).lower() + ".yaml")
-            #         with io.open(kind_path, "w+", encoding="utf-8") as y_file:
-            #             yaml.dump(content, y_file, default_flow_style=False, explicit_start=True, Dumper=yaml.RoundTripDumper)
-            # fo.close()
-            # os.remove(f)
+            fo = open(f, "r", encoding="utf-8", newline="\n")
+            y = yaml.load_all(fo, Loader=yaml.UnsafeLoader)
+            template_deploy_path = f.joinpath(templates_path).joinpath("deploy.yaml")
+            with io.open(template_deploy_path, "w+", encoding="utf-8", newline="\n") as y_file:
+                all_doc = []
+                for content in y:
+                    if content and content["kind"]:
+                        if "namespace" in content["metadata"].keys():
+                            content["metadata"]["namespace"] = "{{ .Values.namespace }}"
+                    all_doc.append(content)
+                yaml.dump_all(all_doc, y_file, Dumper=yaml.RoundTripDumper,default_flow_style=False, explicit_start=True)
+            fo.close()
+            os.remove(f)
 
 
 if __name__ == '__main__':
-    root_path = Path(__file__).joinpath("../../server/pod/db").resolve().as_posix()
-    split(root_path)
+    split(Path(__file__).joinpath("../../server/pod/app").resolve().as_posix())
+    split(Path(__file__).joinpath("../../server/pod/db").resolve().as_posix())
+    split(Path(__file__).joinpath("../../server/pod/init").resolve().as_posix())
+    split(Path(__file__).joinpath("../../server/pod/ops").resolve().as_posix())
