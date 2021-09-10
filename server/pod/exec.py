4b9e2d842bc3c4ceee2a5494fca42101a08d4a2a
zhaoyu.chen <805899926@qq.com>
2021-09-10 14:23:41 +0800

    #

--- server/pod/exec.py
+++ server/pod/exec.py
@@ -20,25 +20,25 @@ def get_kube_cmd(action: str, yaml_path: str):
 
 def apply(app_id: str, app_name: str, source_path: Path, **kwargs):
     args = kwargs["args"]
+    kubectl_action = 'delete' if args.d else 'apply'
     chart_path = source_path.joinpath("Chart.yaml")
     manual_path = source_path.joinpath("manual.sh")
     temp_all_in_one_path = source_path.joinpath("___temp/deploy.yaml")
     temp_all_in_one_path.parent.mkdir(parents=True, exist_ok=True)
     temp_all_in_one_path.touch()
     echo_cmd = 'echo -e "{}\033[32m deploy\033[0m"'.format(app_id)
+    kube_cmd = get_kube_cmd(kubectl_action, temp_all_in_one_path.as_posix())
     cmd_func = lambda x: 'bash -xc \'{}\''.format(x)
-    action = 'delete' if args.d else 'apply'
+
+    cmd = [echo_cmd]
     if chart_path.exists():
         helm_cmd = 'helm template {} --values {} --debug > {}'.format(source_path.as_posix(), env_path.as_posix(), temp_all_in_one_path.as_posix())
-        kube_cmd = get_kube_cmd(action, temp_all_in_one_path.as_posix())
-        cmd = cmd_func("&&".join([echo_cmd, helm_cmd, kube_cmd]))
+        cmd.append(helm_cmd)
+        cmd.append(kube_cmd)
     elif manual_path.exists():
-        kube_cmd = get_kube_cmd(action, temp_all_in_one_path.as_posix())
-        cmd = cmd_func("&&".join([echo_cmd, manual_path.as_posix(),kube_cmd]))
-    else:
-        kube_cmd = "&&".join([get_kube_cmd(action, t.as_posix()) for t in source_path.glob("*.yaml")])
-        cmd = cmd_func("&&".join([echo_cmd, kube_cmd]))
-    execute_shell(cmd)
+        cmd.append(manual_path.as_posix())
+        cmd.append(kube_cmd)
+    execute_shell(cmd_func("&&".join(cmd)))
 
 
 def execute_shell(cmd: str):
