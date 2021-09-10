38fe789ce67ca286d91672b0040b34f301322f63
Bruce CHEN <805899926@qq.com>
2021-09-10 21:38:08 +0800

    #

--- server/pod/exec.py
+++ server/pod/exec.py
@@ -21,19 +21,20 @@ def get_kube_cmd(action: str, yaml_path: str):
 def apply(app_id: str, app_name: str, source_path: Path, **kwargs):
     args = kwargs["args"]
     kubectl_action = 'delete' if args.d else 'apply'
-    chart_path = source_path.joinpath("Chart.yaml")
-    manual_path = source_path.joinpath("manual.sh")
     temp_all_in_one_path = source_path.joinpath("___temp/deploy.yaml")
     temp_all_in_one_path.parent.mkdir(parents=True, exist_ok=True)
     temp_all_in_one_path.touch()
     echo_cmd = 'echo -e "{}\033[32m deploy\033[0m"'.format(app_id)
     kube_cmd = get_kube_cmd(kubectl_action, temp_all_in_one_path.as_posix())
     cmd_func = lambda x: 'bash -xc \'{}\''.format(x)
-
     cmd = [echo_cmd]
-    helm_cmd = 'helm template {} --values {} --debug > {}'.format(source_path.as_posix(), env_path.as_posix(), temp_all_in_one_path.as_posix())
-    cmd.append(helm_cmd)
-    # cmd.append(kube_cmd)
+    helm_dep_update_cmd = 'helm dep up {}'.format(source_path.as_posix())
+    helm_template_cmd = 'helm template {} --namespace {} --values {} --debug > {}'.format(source_path.as_posix(), args.n,
+                                                                                          ",".join([source_path.joinpath("values.yaml").as_posix(), env_path.as_posix()]),
+                                                                                          temp_all_in_one_path.as_posix())
+    cmd.append(helm_dep_update_cmd)
+    cmd.append(helm_template_cmd)
+    cmd.append(kube_cmd)
     execute_shell(cmd_func("&&".join(cmd)))
 
 
@@ -51,10 +52,11 @@ def execute_shell(cmd: str):
 
 if __name__ == '__main__':
     env_path = Path(__file__).parent.joinpath("env.yaml")
+    selected_init_option = share.select_one_option()
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', action="store_true")
+    parser.add_argument('-n', default=Path(selected_init_option).name)
     args = parser.parse_args()
-    selected_init_option = share.select_one_option()
     if selected_init_option:
         selected_init_install = share.get_install_tuple(Path(selected_init_option).name)
         execute(selected_init_install, apply, env_path=env_path, args=args)
