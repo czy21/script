8d9cd728518216982bb8051109c810de14d7f076
zhaoyu.chen <805899926@qq.com>
2021-09-10 13:44:40 +0800

    #

--- server/pod/exec.py
+++ server/pod/exec.py
@@ -14,33 +14,31 @@ def execute(app_tuples, func, **kwargs):
         func(app_id, source_name, source_path, **kwargs)
 
 
+def get_kube_cmd(action: str, yaml_path: str):
+    return 'kubectl {} --filename={}'.format(action, yaml_path)
+
+
 def apply(app_id: str, app_name: str, source_path: Path, **kwargs):
     args = kwargs["args"]
-    if args.m:
-        manual_cmd = 'bash {}'.format(source_path.joinpath("manual.sh").as_posix())
-        execute_shell("&&".join([
-            manual_cmd,
-            "echo \n"
-        ]))
-    if args.i:
-        chart_path = source_path.joinpath("Chart.yaml")
-        temp_all_in_one_path = source_path.joinpath("___temp/deploy.yaml")
-        temp_all_in_one_path.parent.mkdir(parents=True, exist_ok=True)
-        temp_all_in_one_path.touch()
-        echo_cmd = 'echo -e "{}\033[32m deploy \033[0m"'.format(app_id)
-        if chart_path.exists():
-            helm_cmd = 'helm template --values {} {} --debug > {}'.format(env_path.as_posix(), source_path.as_posix(), temp_all_in_one_path.as_posix())
-            kube_cmd = 'kubectl apply --filename={}'.format(temp_all_in_one_path)
-            execute_shell("&&".join([
-                echo_cmd,
-                helm_cmd,
-                kube_cmd,
-                "echo \n"
-            ]))
-        else:
-            for t in source_path.glob("*.yaml"):
-                yaml = source_path.joinpath(t.name).as_posix()
-                execute_shell(".".join(['echo -e "{}\033[32m deploy => \033[0m {}"'.format(app_id, yaml), '&& kubectl apply --filename={}'.format(yaml)]))
+    chart_path = source_path.joinpath("Chart.yaml")
+    manual_path = source_path.joinpath("manual.sh")
+    temp_all_in_one_path = source_path.joinpath("___temp/deploy.yaml")
+    temp_all_in_one_path.parent.mkdir(parents=True, exist_ok=True)
+    temp_all_in_one_path.touch()
+    echo_cmd = 'echo -e "{}\033[32m deploy\033[0m"'.format(app_id)
+    cmd_func = lambda x: 'bash -xc \'{}\''.format(x)
+    action = 'delete' if args.d else 'apply'
+    if chart_path.exists():
+        helm_cmd = 'helm template {} --values {} --debug > {}'.format(source_path.as_posix(), env_path.as_posix(), temp_all_in_one_path.as_posix())
+        kube_cmd = get_kube_cmd(action, temp_all_in_one_path.as_posix())
+        cmd = cmd_func("&&".join([echo_cmd, helm_cmd, kube_cmd]))
+    elif manual_path.exists():
+        kube_cmd = get_kube_cmd(action, temp_all_in_one_path.as_posix())
+        cmd = cmd_func("&&".join([echo_cmd, manual_path.as_posix(),kube_cmd]))
+    else:
+        kube_cmd = "&&".join([get_kube_cmd(action, t.as_posix()) for t in source_path.glob("*.yaml")])
+        cmd = cmd_func("&&".join([echo_cmd, kube_cmd]))
+    execute_shell(cmd)
 
 
 def execute_shell(cmd: str):
@@ -58,8 +56,6 @@ def execute_shell(cmd: str):
 if __name__ == '__main__':
     env_path = Path(__file__).parent.joinpath("env.yaml")
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', action="store_true")
-    parser.add_argument('-m', action="store_true")
     parser.add_argument('-d', action="store_true")
     args = parser.parse_args()
     selected_init_option = share.select_one_option()
--- server/pod/ops/consul/manual.sh
+++ server/pod/ops/consul/manual.sh
@@ -2,10 +2,11 @@
 set -e
 
 dir=$(cd "$(dirname "$0")"; pwd)
+own_name=$(basename $(dirname "${dir}"))
 app_name="$(basename ${dir})"
 values_yaml=${dir}/values.yaml
 
 if test -z "$(helm repo list | grep 'hashicorp' | awk '{print $1}')"; then
   helm repo add hashicorp https://helm.releases.hashicorp.com
 fi
-helm install ${app_name} hashicorp/consul --namespace ops --values ${values_yaml}
+helm template hashicorp/consul --namespace ${own_name} --values ${values_yaml} > ${dir}/___temp/deploy.yaml
