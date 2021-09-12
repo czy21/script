1a5547989240f31303ae76f5bc257dec65d2525f
Bruce CHEN <805899926@qq.com>
2021-09-12 17:22:23 +0800

    #

--- server/docker/compose/atlassian/build.sh
+++ server/docker/compose/atlassian/build.sh
@@ -5,10 +5,10 @@ set -e
 dir=$(cd "$(dirname "$0")"; pwd)
 source ${dir}/../../.env.global
 
-docker build --tag ${GLOBAL_REGISTRY_URL}/confluence:1.0 --file ${dir}/image/confluence-dockerfile ${dir}/image/ --no-cache --force-rm
-docker build --tag ${GLOBAL_REGISTRY_URL}/jira:1.0 --file ${dir}/image/jira-dockerfile ${dir}/image/ --no-cache --force-rm
+docker build --tag ${GLOBAL_REGISTRY_URL}/confluence --file ${dir}/image/confluence-dockerfile ${dir}/image/ --no-cache --force-rm
+docker build --tag ${GLOBAL_REGISTRY_URL}/jira --file ${dir}/image/jira-dockerfile ${dir}/image/ --no-cache --force-rm
 
 #docker login ${GLOBAL_REGISTRY_URL} --username admin --password ***REMOVED***
-docker push ${GLOBAL_REGISTRY_URL}/confluence:1.0
-docker push ${GLOBAL_REGISTRY_URL}/jira:1.0
+docker push ${GLOBAL_REGISTRY_URL}/confluence
+docker push ${GLOBAL_REGISTRY_URL}/jira
 #docker logout
