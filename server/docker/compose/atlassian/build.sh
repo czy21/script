2c176917d05671e0ea971c1eb7cf7addea01f098
zhaoyu.chen <805899926@qq.com>
2021-09-17 12:54:38 +0800

    #

--- server/docker/compose/atlassian/build.sh
+++ server/docker/compose/atlassian/build.sh
@@ -8,7 +8,6 @@ source ${dir}/../../.env.global
 docker build --tag ${GLOBAL_REGISTRY_URL}/${GLOBAL_REGISTRY_DIR}/confluence --file ${dir}/image/confluence-dockerfile ${dir}/image/ --no-cache --force-rm
 docker build --tag ${GLOBAL_REGISTRY_URL}/${GLOBAL_REGISTRY_DIR}/jira --file ${dir}/image/jira-dockerfile ${dir}/image/ --no-cache --force-rm
 
-#docker login ${GLOBAL_REGISTRY_URL}/${GLOBAL_REGISTRY_DIR} --username admin --password Czy20210314.
+docker login ${GLOBAL_REGISTRY_URL}/${GLOBAL_REGISTRY_DIR} --username admin --password Czy20210314.
 docker push ${GLOBAL_REGISTRY_URL}/${GLOBAL_REGISTRY_DIR}/confluence
 docker push ${GLOBAL_REGISTRY_URL}/${GLOBAL_REGISTRY_DIR}/jira
-#docker logout
