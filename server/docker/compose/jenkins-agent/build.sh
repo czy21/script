0a04c29ff3888b00b70552c969bba9b3056a8eaf
Bruce CHEN <805899926@qq.com>
2021-09-20 23:14:35 +0800

    #

---
+++ server/docker/compose/jenkins-agent/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+dir=$(cd "$(dirname "$0")"; pwd)
+dir_name="$(basename ${dir})"
+
+source ${dir}/../../.env.global
+
+docker login ${GLOBAL_REGISTRY_URL} --username ${GLOBAL_REGISTRY_USERNAME} --password ${GLOBAL_REGISTRY_PASSWORD}
+
+docker build --tag ${GLOBAL_REGISTRY_URL}/${GLOBAL_REGISTRY_DIR}/${dir_name} --file ${dir}/Dockerfile ${dir}/
+docker push ${GLOBAL_REGISTRY_URL}/${GLOBAL_REGISTRY_DIR}/${dir_name}
+
+
