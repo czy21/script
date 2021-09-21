92f482a27688b30b209ca788d6bac5a0279ebcc6
Bruce CHEN <805899926@qq.com>
2021-09-21 17:03:54 +0800

    #

---
+++ groovy/k8s.groovy
@@ -0,0 +1,5 @@
+def apply(){
+    withKubeConfig([credentialsId: "${KUBE_CONFIG}", serverUrl: "${KUBE_SERVER}"]) {
+        sh 'kubectl delete -f deploy.yaml --ignore-not-found=true && kubectl apply -f deploy.yaml'
+    }
+}
