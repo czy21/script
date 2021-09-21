601e8c5a664e913012ed56580f2765f0c3b4f30a
Bruce CHEN <805899926@qq.com>
2021-09-21 22:15:25 +0800

    #

--- groovy/helm.groovy
+++ groovy/helm.groovy
@@ -1,6 +1,6 @@
 def build(){
     env.HELM_REPO = env.HELM_REPO?: 'http://nexus.cluster2.com/repository/helm/'
-    sh 'helm template ${RELEASE_NAME} ${RELEASE_TEMPLATE_NAME} --version ${RELEASE_TEMPLATE_VERSION} --namespace ${RELEASE_NAMESPACE} --set appVersion=${RELEASE_VERSION} --repo ${HELM_REPO}  2>&1 | tee deploy.yaml'
+    sh 'helm template ${RELEASE_NAME} ${RELEASE_CHART_NAME} --version ${RELEASE_CHART_VERSION} --namespace ${RELEASE_NAMESPACE} --repo ${HELM_REPO} --set appVersion=${RELEASE_VERSION},envName=${ENV_NAME} 2>&1 | tee deploy.yaml'
 }
 
 
