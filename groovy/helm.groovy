8ba27adf390424f253582f38f73a891a7e526c82
Bruce CHEN <805899926@qq.com>
2021-09-21 19:54:26 +0800

    #

--- groovy/helm.groovy
+++ groovy/helm.groovy
@@ -1,4 +1,4 @@
-def templateRender(){
+def build(){
     sh 'helm template ${RELEASE_NAME} ${RELEASE_TEMPLATE} --namespace ${RELEASE_NAMESPACE} --set appVersion="dev-bruce" --repo ${HELM_REPO} 2>&1 | tee deploy.yaml'
 }
 
