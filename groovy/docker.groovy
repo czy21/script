299dac8910df06f9b4cd180c09470d11ba461932
zhaoyu.chen <805899926@qq.com>
2021-09-22 19:08:34 +0800

    #

--- groovy/docker.groovy
+++ groovy/docker.groovy
@@ -1,7 +1,7 @@
 def build(){
-    sh 'docker login ${REGISTRY_URL} --username ${REGISTRY_USERNAME} --password ${REGISTRY_PASSWORD}'
+    sh 'docker login ${REGISTRY_REPO} --username ${REGISTRY_USERNAME} --password ${REGISTRY_PASSWORD}'
     sh 'docker build --tag ${IMAGE_NAME}:${RELEASE_VERSION} --file ${DOCKER_FILE} ${DOCKER_CONTEXT} --no-cache --force-rm'
-    sh 'docker push ${IMAGE_NAME}:${BRANCH}'
+    sh 'docker push ${IMAGE_NAME}:${RELEASE_VERSION}'
 }
 
 return this;
