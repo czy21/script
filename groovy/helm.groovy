a144c9cb788683eb3f5d6ead9e35f5b9e40cea14
zhaoyu.chen <805899926@qq.com>
2021-09-22 19:08:34 +0800

    #

--- groovy/helm.groovy
+++ groovy/helm.groovy
@@ -1,5 +1,4 @@
 def build(){
-    env.HELM_REPO = env.HELM_REPO?: 'http://nexus.cluster2.com/repository/helm/'
     sh 'helm template ${RELEASE_NAME} ${RELEASE_CHART_NAME} --version ${RELEASE_CHART_VERSION} --namespace ${RELEASE_NAMESPACE} --repo ${HELM_REPO} --set appVersion=${RELEASE_VERSION},envName=${ENV_NAME} 2>&1 | tee deploy.yaml'
 }
 
