e95d84c1dd40d21d6c2c923a620f8bcca1ec3c5f
zhaoyu.chen <805899926@qq.com>
2021-09-22 20:45:37 +0800

    #

--- groovy/java.groovy
+++ groovy/java.groovy
@@ -1,5 +1,9 @@
-def build(){
+def gradlew(){
     sh 'chmod +x ${PROJECT_ROOT}/gradlew && ${PROJECT_ROOT}/gradlew --init-script ${GRADLE_INIT_FILE} --build-file ${PROJECT_ROOT}/build.gradle ${PROJECT_MODULE}:clean ${PROJECT_MODULE}:build -x test'
+}
+
+def mvn(){
+    sh 'echo mvn'
 }
 
 return this;
