d485606b47edaeec4c29cdc432739bd40c5d90b5
zhaoyu.chen <805899926@qq.com>
2021-09-23 16:20:29 +0800

    #

---
+++ groovy/share/vars/Deploy.groovy
@@ -0,0 +1,60 @@
+#!/usr/bin/env groovy
+
+def call(Map map) {
+    print map
+
+    pipeline{
+      agent {
+        kubernetes {
+            cloud map.ENV_NAME
+            yaml '''
+              apiVersion: v1
+              kind: Pod
+              spec:
+                volumes:
+                - name: docker-sock
+                  hostPath:
+                    path: /var/run/docker.sock
+                - name: docker-cmd
+                  hostPath:
+                    path: /usr/bin/docker
+                - name: kubectl
+                  hostPath:
+                    path: /bin/kubectl
+                containers:
+                - name: jnlp
+                  image: 'registry.cluster2.com/library/jenkins-agent'
+                  securityContext:
+                    runAsUser: 0
+                  volumeMounts:
+                  - name: docker-sock
+                    mountPath: /var/run/docker.sock
+                  - name: docker-cmd
+                    mountPath: /usr/bin/docker
+                  - name: kubectl
+                    mountPath: /bin/kubectl
+        '''
+        }
+      }
+      stages{
+        stage('deploy') {
+          steps {
+            script {
+                configFileProvider([configFile(fileId: "${GLOBAL_ENV_FILE_ID}", targetLocation: 'env.groovy', variable: 'ENV_CONFIG')]) {
+                    load "env.groovy";
+                }
+            env.ENV_NAME = "${map.ENV_NAME}"
+            env.RELEASE_NAMESPACE = "${map.RELEASE_NAMESPACE}"
+            env.RELEASE_NAME = "${map.RELEASE_NAME}"
+            env.RELEASE_VERSION = "${map.RELEASE_VERSION}"
+            env.RELEASE_CHART_NAME    = env.HELM_JAVA_CHART_NAME
+            env.RELEASE_CHART_VERSION = env.HELM_JAVA_CHART_VERSION
+            helm.build()
+            k8s.apply()
+            }
+          }
+        }
+      }
+    }
+}
+
