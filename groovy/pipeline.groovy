14e617c3b013cccabd9c72139b7f37866f2b8754
zhaoyu.chen <805899926@qq.com>
2021-09-23 11:20:26 +0800

    #

--- groovy/pipeline.groovy
+++ groovy/pipeline.groovy
@@ -14,36 +14,43 @@ def docker_build(Map map){
         parameters {
           gitParameter branchFilter: 'origin/(.*)', name: 'BRANCH', type: 'PT_BRANCH',defaultValue: 'master',useRepository: "${GIT_REPOSITORY_URL}"
         }
-        stages {
-            stage('clone'){
+        stages{
+            stage('bbb'){
                 steps{
-                    checkout([$class: 'GitSCM',
-                    branches: [[name: "${BRANCH}"]],
-                    extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]],
-                    userRemoteConfigs: [[credentialsId: "${GIT_REPOSITORY_CREDENTIAL_ID}", url: "${GIT_REPOSITORY_URL}"]]])
-                }
-            }
-            stage('build'){
-                when{
-                    expression { BRANCH != 'master' }
-                }
-                steps{
-                    script{
-                        configFileProvider([configFile(fileId: "${GLOBAL_ENV_FILE_ID}", targetLocation: 'env.groovy', variable: 'ENV_CONFIG')]) {
-                            load "env.groovy";
-                        }
-                        env.RELEASE_VERSION = params.BRANCH
-                        env.IMAGE_NAME = "${REGISTRY_REPO}/${REGISTRY_DIR}/${PROJECT_NAME}-${PROJECT_MODULE}"
-                        env.DOCKER_FILE = "${PROJECT_ROOT}/${PROJECT_MODULE}/Dockerfile"
-                        env.DOCKER_FILE_CONTEXT = "${PROJECT_ROOT}/${PROJECT_MODULE}/"
-
-                        sh 'docker login ${REGISTRY_REPO} --username ${REGISTRY_USERNAME} --password ${REGISTRY_PASSWORD}'
-                        sh 'docker build --tag ${IMAGE_NAME}:${RELEASE_VERSION} --file ${DOCKER_FILE} ${DOCKER_FILE_CONTEXT} --no-cache --force-rm'
-                        sh 'docker push ${IMAGE_NAME}:${RELEASE_VERSION}'
-                    }
+                    sh 'echo hello'
                 }
             }
         }
+//         stages {
+//             stage('clone'){
+//                 steps{
+//                     checkout([$class: 'GitSCM',
+//                     branches: [[name: "${BRANCH}"]],
+//                     extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]],
+//                     userRemoteConfigs: [[credentialsId: "${GIT_REPOSITORY_CREDENTIAL_ID}", url: "${GIT_REPOSITORY_URL}"]]])
+//                 }
+//             }
+//             stage('build'){
+//                 when{
+//                     expression { BRANCH != 'master' }
+//                 }
+//                 steps{
+//                     script{
+//                         configFileProvider([configFile(fileId: "${GLOBAL_ENV_FILE_ID}", targetLocation: 'env.groovy', variable: 'ENV_CONFIG')]) {
+//                             load "env.groovy";
+//                         }
+//                         env.RELEASE_VERSION = params.BRANCH
+//                         env.IMAGE_NAME = "${REGISTRY_REPO}/${REGISTRY_DIR}/${PROJECT_NAME}-${PROJECT_MODULE}"
+//                         env.DOCKER_FILE = "${PROJECT_ROOT}/${PROJECT_MODULE}/Dockerfile"
+//                         env.DOCKER_FILE_CONTEXT = "${PROJECT_ROOT}/${PROJECT_MODULE}/"
+//
+//                         sh 'docker login ${REGISTRY_REPO} --username ${REGISTRY_USERNAME} --password ${REGISTRY_PASSWORD}'
+//                         sh 'docker build --tag ${IMAGE_NAME}:${RELEASE_VERSION} --file ${DOCKER_FILE} ${DOCKER_FILE_CONTEXT} --no-cache --force-rm'
+//                         sh 'docker push ${IMAGE_NAME}:${RELEASE_VERSION}'
+//                     }
+//                 }
+//             }
+        // }
     }
 }
 
