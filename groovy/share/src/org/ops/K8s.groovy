e10da4f9aaad6a7d05aad194e4df54cd4371175f
zhaoyu.chen <805899926@qq.com>
2021-09-23 17:11:14 +0800

    #

--- groovy/share/src/org/ops/K8s.groovy
+++ groovy/share/src/org/ops/K8s.groovy
@@ -2,13 +2,15 @@
 package org.ops
 
 def build(){
+    sh 'env > values.yaml'
     sh 'helm template ${RELEASE_NAME} ${RELEASE_CHART_NAME} --version ${RELEASE_CHART_VERSION} --namespace ${RELEASE_NAMESPACE} --repo ${HELM_REPO} --set appVersion=${RELEASE_VERSION},envName=${ENV_NAME} 2>&1 | tee deploy.yaml'
 }
 
 
 def apply(){
     withKubeConfig([credentialsId: env.KUBE_CREDENTIAL, serverUrl: env.KUBE_SERVER]) {
-        sh 'kubectl delete -f deploy.yaml --ignore-not-found=true && kubectl apply -f deploy.yaml'
+        sh 'sleep 9999'
+        // sh 'kubectl delete -f deploy.yaml --ignore-not-found=true && kubectl apply -f deploy.yaml'
     }
 }
 
