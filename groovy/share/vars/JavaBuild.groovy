2f8eda721a7cb9177f9f8a2d4e41174f25fd44e6
zhaoyu.chen <805899926@qq.com>
2021-09-23 14:38:51 +0800

    #

--- groovy/share/vars/JavaBuild.groovy
+++ groovy/share/vars/JavaBuild.groovy
@@ -11,6 +11,7 @@ def call(Map map) {
             PROJECT_MODULE  = "${map.PROJECT_MODULE}"
             GIT_REPOSITORY_URL = "${map.GIT_REPOSITORY_URL}"
             GIT_CREDENTIAL_ID  = "${map.GIT_CREDENTIAL_ID}"
+            GLOBAL_ENV_FILE_ID = "${map.GLOBAL_ENV_FILE_ID}"
         }
         parameters {
           gitParameter branchFilter: 'origin/(.*)', name: 'BRANCH', type: 'PT_BRANCH',defaultValue: 'master',useRepository: "${map.GIT_REPOSITORY_URL}"
@@ -21,30 +22,26 @@ def call(Map map) {
                     checkout([$class: 'GitSCM', branches: [[name: "${BRANCH}"]], extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], userRemoteConfigs: [[credentialsId: "${GIT_CREDENTIAL_ID}", url: "${GIT_REPOSITORY_URL}"]]])
                 }
             }
-//             stage('build'){
-//                 when{
-//                     expression { BRANCH != 'master' }
-//                 }
-//                 steps{
-//                     script{
-//                         configFileProvider([configFile(fileId: "dev.env", targetLocation: 'env.groovy', variable: 'ENV_CONFIG')]) {
-//                             load "env.groovy";
-//                         }
-//                         env.RELEASE_VERSION = params.BRANCH
-//                         env.IMAGE_NAME="${REGISTRY_REPO}/${REGISTRY_DIR}/${PROJECT_NAME}-${PROJECT_MODULE}"
-//                         env.DOCKER_FILE = "${PROJECT_ROOT}/${PROJECT_MODULE}/Dockerfile"
-//                         env.DOCKER_FILE_CONTEXT = "${PROJECT_ROOT}/${PROJECT_MODULE}/"
-//
-//                         def docker,java
-//                         fileLoader.withGit('git@gitee.com:czyhome/script.git', 'master', 'bruce', '') {
-//                             docker = fileLoader.load('groovy/docker.groovy');
-//                             java = fileLoader.load('groovy/java.groovy');
-//                         }
-//                         java.gradlew()
-//                         docker.build()
-//                     }
-//                 }
-//             }
+            stage('build'){
+                when{
+                    expression { BRANCH != 'master' }
+                }
+                steps{
+                    script{
+                        configFileProvider([configFile(fileId: "${GLOBAL_ENV_FILE_ID}", targetLocation: 'env.groovy', variable: 'ENV_CONFIG')]) {
+                            load "env.groovy";
+                        }
+                        env.RELEASE_VERSION = params.BRANCH
+                        env.IMAGE_NAME="${REGISTRY_REPO}/${REGISTRY_DIR}/${PROJECT_NAME}-${PROJECT_MODULE}"
+                        env.DOCKER_FILE = "${PROJECT_ROOT}/${PROJECT_MODULE}/Dockerfile"
+                        env.DOCKER_FILE_CONTEXT = "${PROJECT_ROOT}/${PROJECT_MODULE}/"
+
+                        sh 'docker login ${REGISTRY_REPO} --username ${REGISTRY_USERNAME} --password ${REGISTRY_PASSWORD}'
+                        sh 'docker build --tag ${IMAGE_NAME}:${RELEASE_VERSION} --file ${DOCKER_FILE} ${DOCKER_FILE_CONTEXT} --no-cache --force-rm'
+                        sh 'docker push ${IMAGE_NAME}:${RELEASE_VERSION}'
+                    }
+                }
+            }
         }
     }
 }
