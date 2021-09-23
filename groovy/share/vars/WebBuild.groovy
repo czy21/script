6e19ddc00ca0d42eed187a111c065d302c255b2f
zhaoyu.chen <805899926@qq.com>
2021-09-23 19:53:44 +0800

    #

--- groovy/share/vars/WebBuild.groovy
+++ groovy/share/vars/WebBuild.groovy
@@ -5,18 +5,18 @@ def call(Map map) {
     pipeline{
         agent any
         environment {
-            GRADLE_INIT_FILE = "/var/jenkins_home/tools/gradle/init.d/init.gradle"
-            PROJECT_ROOT    = "${WORKSPACE}/${map.PROJECT_ROOT}"
-            PROJECT_NAME    = "${map.PROJECT_NAME}"
-            PROJECT_MODULE  = "${map.PROJECT_MODULE}"
-            GIT_REPOSITORY_URL = "${map.GIT_REPOSITORY_URL}"
-            GIT_CREDENTIAL_ID  = "${map.GIT_CREDENTIAL_ID}"
-            GLOBAL_ENV_FILE_ID = "${map.GLOBAL_ENV_FILE_ID}"
-            NODEJS_HOME = "${tool 'node-v14.17.5'}"
-            PATH="${NODEJS_HOME}/bin:${PATH}"
+            PARAM_GRADLE_INIT_FILE = "/var/jenkins_home/tools/gradle/init.d/init.gradle"
+            PARAM_PROJECT_ROOT    = "${WORKSPACE}/${map.PARAM_PROJECT_ROOT}"
+            PARAM_PROJECT_NAME    = "${map.PARAM_PROJECT_NAME}"
+            PARAM_PROJECT_MODULE  = "${map.PARAM_PROJECT_MODULE}"
+            PARAM_GIT_REPOSITORY_URL = "${map.PARAM_GIT_REPOSITORY_URL}"
+            PARAM_GIT_CREDENTIAL_ID  = "${map.PARAM_GIT_CREDENTIAL_ID}"
+            PARAM_GLOBAL_ENV_FILE_ID = "${map.PARAM_GLOBAL_ENV_FILE_ID}"
+            PARAM_NODEJS_HOME = "${tool 'node-v14.17.5'}"
+            PARAM_PATH="${NODEJS_HOME}/bin:${PATH}"
         }
         parameters {
-          gitParameter branchFilter: 'origin/(.*)', name: 'BRANCH', type: 'PT_BRANCH',defaultValue: 'master',useRepository: "${map.GIT_REPOSITORY_URL}"
+          gitParameter branchFilter: 'origin/(.*)', name: 'BRANCH', type: 'PT_BRANCH',defaultValue: 'master',useRepository: "${map.PARAM_GIT_REPOSITORY_URL}"
         }
         stages {
             stage('clone'){
@@ -35,7 +35,7 @@ def call(Map map) {
                     script{
                         def d = new org.ops.Docker()
                         d.prepare()
-                        sh 'nrm use taobao && yarn --cwd ${PROJECT_ROOT}/${PROJECT_MODULE} install && yarn --cwd ${PROJECT_ROOT}/${PROJECT_MODULE} --ignore-engines build'
+                        sh 'nrm use taobao && yarn --cwd ${PARAM_PROJECT_ROOT}/${PARAM_PROJECT_MODULE} install && yarn --cwd ${PARAM_PROJECT_ROOT}/${PARAM_PROJECT_MODULE} --ignore-engines build'
                         d.build()
                     }
                 }
