ff5ef551a282bbedf124c496b14d778e356e5f36
zhaoyu.chen <805899926@qq.com>
2021-09-23 16:39:35 +0800

    #

--- groovy/docker.groovy
+++
@@ -1,7 +0,0 @@
-def build(){
-    sh 'docker login ${REGISTRY_REPO} --username ${REGISTRY_USERNAME} --password ${REGISTRY_PASSWORD}'
-    sh 'docker build --tag ${IMAGE_NAME}:${RELEASE_VERSION} --file ${DOCKER_FILE} ${DOCKER_FILE_CONTEXT} --no-cache --force-rm'
-    sh 'docker push ${IMAGE_NAME}:${RELEASE_VERSION}'
-}
-
-return this;
--- groovy/java.groovy
+++
@@ -1,9 +0,0 @@
-def gradlew(){
-    sh 'chmod +x ${PROJECT_ROOT}/gradlew && ${PROJECT_ROOT}/gradlew --init-script ${GRADLE_INIT_FILE} --build-file ${PROJECT_ROOT}/build.gradle ${PROJECT_MODULE}:clean ${PROJECT_MODULE}:build -x test'
-}
-
-def mvn(){
-    sh 'echo mvn'
-}
-
-return this;
