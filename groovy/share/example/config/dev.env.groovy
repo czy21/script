ae129e9d42dd014e437c8f4b6c91a7de5bb6f980
zhaoyu.chen <805899926@qq.com>
2021-09-24 12:43:38 +0800

    #

---
+++ groovy/share/example/config/dev.env.groovy
@@ -0,0 +1,14 @@
+env.param_kube_credential = 'kube-config-dev'
+env.param_kube_server = 'https://192.168.2.21:6443'
+
+env.param_helm_repo = 'http://nexus.cluster2.com/repository/helm/'
+env.param_helm_java_chart_name = "java-template"
+env.param_helm_java_chart_version = "0.1.0"
+
+env.param_registry_repo= 'registry.cluster2.com'
+env.param_registry_dir= 'library'
+env.param_registry_username = '*'
+env.param_registry_password = '*'
+
+env.param_helm_web_chart_name="web-template"
+env.param_helm_web_chart_version = "0.1.0"
