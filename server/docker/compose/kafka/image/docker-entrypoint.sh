8ec87c60474add6c1ec0633e59ad9d74eb7760a7
zhaoyu.chen <805899926@qq.com>
2021-10-14 10:44:29 +0800

    #

--- server/docker/compose/kafka/image/docker-entrypoint.sh
+++ server/docker/compose/kafka/image/docker-entrypoint.sh
@@ -1,35 +1,37 @@
 #!/usr/bin/env bash
 
-KAFKA_CONF_FILE="${KAFKA_HOME}/config/server.properties"
+# KAFKA_CONF_FILE="${KAFKA_HOME}/config/server.properties"
 
-set_config_option() {
-  local key=$1
-  local value=$2
-  local conf_file=$3
+# set_config_option() {
+#   local key=$1
+#   local value=$2
+#   local conf_file=$3
 
-  # escape periods for usage in regular expressions
-  local escaped_key=$(echo ${key} | sed -e "s/\./\\\./g")
+#   # escape periods for usage in regular expressions
+#   local escaped_key=$(echo ${key} | sed -e "s/\./\\\./g")
 
-  echo "configuring => ${escaped_key} in ${conf_file}"
+#   echo "configuring => ${escaped_key} in ${conf_file}"
 
-  # either override an existing entry, or append a new one
-  if grep -E "^${escaped_key}=.*" "${conf_file}" > /dev/null; then
-        sed -i -e "s/${escaped_key}=.*/${key}=${value}/g" "${conf_file}"
-  else
-        echo "${key}= ${value}" >> ${conf_file}
-  fi
-}
+#   # either override an existing entry, or append a new one
+#   if grep -E "^${escaped_key}=.*" "${conf_file}" > /dev/null; then
+#         sed -i -e "s/${escaped_key}=.*/${key}=${value}/g" "${conf_file}"
+#   else
+#         echo "${key}= ${value}" >> ${conf_file}
+#   fi
+# }
 
-for VAR in $(env)
-do
-    env_var=$(echo "$VAR" | cut -d= -f1)
+# for VAR in $(env)
+# do
+#     env_var=$(echo "$VAR" | cut -d= -f1)
 
-    # config server properties
-    if [[ $env_var =~ ^KAFKA_SERVER ]]; then
-        kafka_name=$(echo "$env_var" | cut -d_ -f3- | tr '[:upper:]' '[:lower:]' | tr _ .)
-        set_config_option "$kafka_name" "${!env_var}" "${KAFKA_CONF_FILE}"
-    fi
+#     # config server properties
+#     if [[ $env_var =~ ^KAFKA_SERVER ]]; then
+#         kafka_name=$(echo "$env_var" | cut -d_ -f3- | tr '[:upper:]' '[:lower:]' | tr _ .)
+#         set_config_option "$kafka_name" "${!env_var}" "${KAFKA_CONF_FILE}"
+#     fi
 
-done
+# done
 
-exec "$KAFKA_HOME/bin/kafka-server-start.sh" "$KAFKA_HOME/config/server.properties"
+# exec "$KAFKA_HOME/bin/kafka-server-start.sh" "$KAFKA_HOME/config/server.properties"
+
+exec "$@"
