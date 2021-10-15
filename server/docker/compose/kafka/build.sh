ad536ffbcc27af9e15461633d4e3942d27b56dd2
zhaoyu.chen <805899926@qq.com>
2021-10-15 20:05:30 +0800

    #

--- server/docker/compose/kafka/build.sh
+++ server/docker/compose/kafka/build.sh
@@ -12,4 +12,8 @@ docker login ${GLOBAL_REGISTRY_URL} --username ${GLOBAL_REGISTRY_USERNAME} --pas
 docker build --tag ${GLOBAL_REGISTRY_URL}/${GLOBAL_REGISTRY_DIR}/${dir_name} --file ${dir}/Dockerfile ${dir}/
 docker push ${GLOBAL_REGISTRY_URL}/${GLOBAL_REGISTRY_DIR}/${dir_name}
 
+docker build --tag ${GLOBAL_REGISTRY_URL}/${GLOBAL_REGISTRY_DIR}/${dir_name}-web --file ${dir}/web/Dockerfile ${dir}/web/
+docker push ${GLOBAL_REGISTRY_URL}/${GLOBAL_REGISTRY_DIR}/${dir_name}-web
+
+
 
