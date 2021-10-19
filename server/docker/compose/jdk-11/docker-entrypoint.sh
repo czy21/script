09e543cd06b6517ed2f8e69986593351645e0691
zhaoyu.chen <805899926@qq.com>
2021-10-19 17:09:22 +0800

    #

--- server/docker/compose/jdk-11/docker-entrypoint.sh
+++ server/docker/compose/jdk-11/docker-entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-
-#RUN wget https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.15.0/jmx_prometheus_javaagent-0.15.0.jar
-
-java -jar ${JAR} --spring.config.location=file:${CONFIG_DIR}
+java ${JAVA_OPTS} -jar ${JAR} --spring.config.location=file:${CONFIG_DIR}
