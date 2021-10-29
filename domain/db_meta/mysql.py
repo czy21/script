fd3ded7cc7c50cebed050f788d47f168378d56c2
Bruce CHEN <805899926@qq.com>
2021-10-29 13:52:08 +0800

    #

--- domain/db_meta/mysql.py
+++ domain/db_meta/mysql.py
@@ -4,9 +4,9 @@ self = {
     "header": "SELECT 'executing: ${{{file_path}}}' AS file;",
     "footer": "SELECT 'executed: ${{{file_path}}}' AS file;",
     "substitution": {
-        "TrackedColumns": "\tcreate_time TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6),\n"
-                          "\tcreate_user varchar(36) DEFAULT NULL,\n"
-                          "\tupdate_time TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),\n"
-                          "\tupdate_user varchar(36) DEFAULT NULL"
+        "TrackedColumns": "\tcreated_date  TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6),\n"
+                          "\tcreated_user  varchar(36) DEFAULT NULL,\n"
+                          "\tmodified_date TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),\n"
+                          "\tmodified_user varchar(36) DEFAULT NULL"
     }
 }
