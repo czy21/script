f6389fb5b04d15bed081fe7b61067177c3203a83
Bruce CHEN <805899926@qq.com>
2021-11-01 18:53:22 +0800

    #

--- domain/db_meta/pgsql.py
+++ domain/db_meta/pgsql.py
@@ -4,9 +4,9 @@ self = {
     "header": "SELECT 'executing: ${{{file_path}}}' AS file;",
     "footer": "SELECT 'executed: ${{{file_path}}}' AS file;",
     "substitution": {
-        "TrackedColumns": "\tcreated_date  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,\n"
+        "TrackedColumns": "\tcreated_date  timestamp(6) DEFAULT CURRENT_TIMESTAMP,\n"
                           "\tcreated_user  varchar(36) DEFAULT NULL,\n"
-                          "\tmodified_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,\n"
+                          "\tmodified_date timestamp(6) DEFAULT CURRENT_TIMESTAMP,\n"
                           "\tmodified_user varchar(36) DEFAULT NULL"
     }
 }
