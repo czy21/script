a41cbe12a73a6df99619d18cf77521d33a9697b0
Bruce Chen <805899926@qq.com>
2021-11-04 00:01:13 +0800

    # add pgsql back to gz

--- domain/source/pgsql.py
+++ domain/source/pgsql.py
@@ -9,6 +9,7 @@ from script.utility import db as db_util, collection as list_util, basic as basi
 logger = log.Logger(__name__)
 
 pgsql_cmd = "psql"
+pg_dump_cmd = "pg_dump"
 
 
 def __get_function_name():
@@ -30,10 +31,13 @@ def recreate() -> None:
 
 
 def get_basic_param(host, port, user, password, db_name) -> str:
-    url = "postgresql://{}:{}@{}:{}".format(user, password, host, port)
+    param = ["--host=" + host,
+             "--port=" + port,
+             "--username=" + user
+             ]
     if db_name:
-        url = url + "/" + db_name
-    return "\"" + url + "\""
+        param.append("--dbname=" + db_name)
+    return list_util.arr_param_to_str(param)
 
 
 def get_main_db_param_dict() -> str:
@@ -45,10 +49,12 @@ def get_main_db_param_dict() -> str:
 
 
 def execute() -> None:
-    extra_param_dict = [
+    command = list_util.arr_param_to_str(
+        "PGPASSWORD=" + default_common.param_main_db_pgsql_pass,
+        pgsql_cmd,
+        get_main_db_param_dict(),
         "< " + default_path.output_db_all_in_one_pgsql
-    ]
-    command = list_util.arr_param_to_str(pgsql_cmd, get_main_db_param_dict(), extra_param_dict)
+    )
     logger.info(basic_util.action_formatter(__get_function_name(), command))
     basic_util.execute(command, db_util.print_ql_msg)
 
@@ -56,15 +62,28 @@ def execute() -> None:
 def get_recreate_command(host, port, user, password, db_name) -> str:
     cmd = [
         "echo",
+        "\"",
         [
             "drop database if exists {0} WITH (FORCE);".format(db_name),
-            "CREATE DATABASE {} WITH OWNER = postgres ENCODING = 'UTF8' CONNECTION LIMIT = -1;".format(db_name)
+            "CREATE DATABASE {} WITH OWNER = postgres ENCODING = \'UTF8\' CONNECTION LIMIT = -1;".format(db_name)
         ],
+        "\"",
         "|",
+        "PGPASSWORD=" + default_common.param_main_db_pgsql_pass,
         pgsql_cmd,
         get_basic_param(host, port, user, password, None)
     ]
     return list_util.arr_param_to_str(cmd)
 
+
 def backup_gz() -> None:
-    basic_util.execute("PGPASSWORD=***REMOVED*** pg_dump --dbname=erp_local --host=192.168.2.25 --port=5432 --username=postgres --column-inserts --file duml.sql")
+    cmd = list_util.arr_param_to_str([
+        "PGPASSWORD=" + default_common.param_main_db_pgsql_pass,
+        pg_dump_cmd,
+        get_main_db_param_dict(),
+        "--column-inserts",
+        "| gzip > ",
+        default_path.output_db_bak_gz_pgsql
+    ])
+    logger.info(basic_util.action_formatter(__get_function_name(), cmd))
+    basic_util.execute(cmd)
