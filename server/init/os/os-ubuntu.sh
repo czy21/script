be70029e587e94b0021794fa597e28048b3dad32
Bruce CHEN <805899926@qq.com>
2021-11-04 16:38:34 +0800

    #

--- server/init/os/os-ubuntu.sh
+++ server/init/os/os-ubuntu.sh
@@ -2,8 +2,15 @@
 set -e
 
 apt -y update
-apt -y install wget vim git network-manager nfs-common python3-pip mysql-client
+apt -y install wget vim git network-manager nfs-common python3-pip
+
+# postgres repo
+# sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+# wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+# install optional
 # apt -y install mysql-client postgresql-client
+
 sed -i -r "s/^\s*%sudo.*/%sudo   ALL=(ALL:ALL) NOPASSWD:ALL/;" /etc/sudoers
 
 sed -i -r "s/^\s*UseDNS\s+\w+/#\0/; s/^\s*PermitRootLogin\s+\w+/#\0/; s/^\s*PasswordAuthentication\s+\w+/#\0/; s/^\s*ClientAliveInterval\s+\w+/#\0/" /etc/ssh/sshd_config
