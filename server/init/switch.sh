55beaec55670a886c94da8410a3198be75b1c61b
Bruce Chen <805899926@qq.com>
2021-11-06 19:54:06 +0800

    # improve ansible init

--- server/init/switch.sh
+++ server/init/switch.sh
@@ -1,39 +1,20 @@
 #!/bin/bash
-# bash switch.sh -h user@host -t [target]
-# -t os | docker | hosts | reboot | k8s-master | k8s-nodes | cp-certs | machine | prune
-set -e
 
-while getopts ":h:t:" opt
+# bash switch.sh -h user@host -i [inventory_name] -t [tags]
+# -i site | general
+# -t [tags within inventory_file]
+#    site: os | docker | hosts | k8s-master | k8s-node
+#    general: prune | k8s-reset
+
+while getopts ":h" opt
 do
 	case $opt in
-  h)
-    source ../../utility/share.sh
-    host=$2
-    sh_file=$0
-    shift 2
-    upload_exec_sh $@
-    break
-    ;;
-  t)
-    script_path=$(dirname "$0")
-    private_key_file=${script_path}/___temp/private-key
-    if [ -f "${private_key_file}" ]; then
-      chmod 600 ${private_key_file}
-    fi
-    shift 1
-    ansible_cmd="ansible-playbook \
-    --inventory ${script_path}/ansible_hosts ${script_path}/$1.yml \
-    --step \
-    --verbose"
-    if [ $1 == 'os' ]; then
-      bash -c "${ansible_cmd} --ask-pass"
-    else
-      bash -c "${ansible_cmd} --private-key ${private_key_file}"
-    fi
-		;;
-		?)
-		echo -e "\033[31m$1 un_know input param \033[0m"
-		break
-		;;
+		h)
+      source ../../utility/share.sh
+      host=$2
+      shift 2
+      upload_exec_py $@
+      break
+			;;
 	esac
 done
--- utility/share.sh
+++ utility/share.sh
@@ -6,11 +6,13 @@ function upload_exec_py() {
   name_path=`basename ${pwd_path}`
   utility_dir=$(cd ${pwd_path}/../../utility; pwd)
   ssh $host 'rm -rf $HOME/'${name_path}';'
+
   scp -rqC ${pwd_path} $host:
   scp -rqC ${pwd_path}/../requirements.txt ${utility_dir}/share.py $host:${name_path}
 
   ssh $host 'sudo pip3 install -r $HOME/'${name_path}/'requirements.txt && python3 -B $HOME/'${name_path}/'exec.py '$@';'
-  # ssh $host 'rm -rf $HOME/'${name_path}';'
+
+  ssh $host 'rm -rf $HOME/'${name_path}';'
 }
 
 function upload_exec_sh() {
@@ -19,4 +21,5 @@ function upload_exec_sh() {
   ssh $host 'rm -rf $HOME/'${name_path}';'
   scp -rqC ${pwd_path} $host:
   ssh $host 'sh $HOME/'${name_path}/${sh_file}' '$@';'
+  ssh $host 'rm -rf $HOME/'${name_path}';'
 }
