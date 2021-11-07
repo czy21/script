983f04fbe43f083ffc6e5291401b8ec6fe8290da
Bruce Chen <805899926@qq.com>
2021-11-08 02:06:03 +0800

    # 2021-11-8 finished k8s redeploy all

--- groovy/share/example/config/dev.env.groovy
+++ groovy/share/example/config/dev.env.groovy
@@ -1,14 +1,14 @@
 env.param_kube_credential = 'kube-config-dev'
 env.param_kube_server = 'https://192.168.2.21:6443'
 
-env.param_helm_repo = 'http://nexus.cluster2.com/repository/helm/'
+env.param_helm_repo = 'http://nexus.cluster.com/repository/helm/'
 env.param_helm_java_chart_name = "java-template"
 env.param_helm_java_chart_version = "0.1.0"
 
 env.param_registry_repo= 'registry.cluster.com'
 env.param_registry_dir= 'library'
-env.param_registry_username = '*'
-env.param_registry_password = '*'
+env.param_registry_username = 'admin'
+env.param_registry_password = '***REMOVED***'
 
 env.param_helm_web_chart_name="web-template"
 env.param_helm_web_chart_version = "0.1.0"
