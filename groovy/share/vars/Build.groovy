9c87f16e31eec89d4849c79e18443a144fe40718
Bruce Chen <805899926@qq.com>
2021-11-14 17:01:58 +0800

    #

--- groovy/share/vars/Build.groovy
+++ groovy/share/vars/Build.groovy
@@ -4,7 +4,8 @@ def call(Map map) {
     pipeline{
         agent any
         environment {
-            param_gradle_init_file = "/var/jenkins_home/tools/gradle/init.d/init.gradle"
+            param_gradle_user_home = "/var/jenkins_home/tools/gradle"
+            param_gradle_init_file = "${param_gradle_user_home}/init.d/init.gradle"
             param_project_root    = "${WORKSPACE}/${map.param_project_root}"
             param_project_name    = "${map.param_project_name}"
             param_project_module  = "${map.param_project_module}"
