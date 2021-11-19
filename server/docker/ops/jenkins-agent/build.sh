0768ca07b75b456ed077769cfa3f96b1d5f61fbc
Bruce Chen <805899926@qq.com>
2021-11-19 22:06:31 +0800

    #

--- server/docker/ops/jenkins-agent/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker build --tag ${param_registry_url}/${param_registry_dir}/${param_role_name} --file ${param_role_path}/Dockerfile ${param_role_path}/
-docker push ${param_registry_url}/${param_registry_dir}/${param_role_name}
-
-
