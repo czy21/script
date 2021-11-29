468bbb115d2c345985f06140a6d69d05b2260de1
Bruce CHEN <805899926@qq.com>
2021-11-29 18:02:31 +0800

    #

--- groovy/share/vars/Build.groovy
+++ groovy/share/vars/Build.groovy
@@ -8,7 +8,6 @@ def call(Map map) {
             param_gradle_init_file = "${param_gradle_user_home}/init.gradle"
             param_project_root    = "${WORKSPACE}/${map.param_project_root}"
             param_project_name    = "${map.param_project_name}"
-            param_project_module  = "${map.param_project_module}"
             param_git_repository_url = "${map.param_git_repository_url}"
             param_git_credential_id  = "${map.param_git_credential_id}"
             param_global_env_file_id = "${map.param_global_env_file_id}"
