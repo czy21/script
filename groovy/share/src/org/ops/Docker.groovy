e357931c2b30e7b5c3948ef8d44e17eb3976e405
Bruce CHEN <805899926@qq.com>
2021-11-29 17:10:20 +0800

    #

--- groovy/share/src/org/ops/Docker.groovy
+++ groovy/share/src/org/ops/Docker.groovy
@@ -29,8 +29,8 @@ def prepare(Map map){
     }
     env.param_release_version = params.param_branch
     env.param_image_name=[env.param_registry_repo,env.param_registry_dir,map.param_project_module].findAll{it->it!=null}.join("/")
-    env.param_docker_file = [map.param_project_root,map.param_project_module,"Dockerfile"].findAll{it->it!=null}.join("/")
-    env.param_docker_file_context = [map.param_project_root,map.param_project_module].findAll{it->it!=null}.join("/")
+    env.param_docker_file = [env.param_project_root,map.param_project_module,"Dockerfile"].findAll{it->it!=null}.join("/")
+    env.param_docker_file_context = [env.param_project_root,map.param_project_module].findAll{it->it!=null}.join("/")
 }
 
 return this
