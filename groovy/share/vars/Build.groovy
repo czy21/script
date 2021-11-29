a668f1c9b90abcc5b8b14a35cfd2bd11cf6cb299
Bruce Chen <805899926@qq.com>
2021-11-29 20:30:24 +0800

    #

--- groovy/share/vars/Build.groovy
+++ groovy/share/vars/Build.groovy
@@ -7,12 +7,13 @@ def call(Map map) {
             param_gradle_user_home   = "/var/jenkins_home/tools/gradle"
             param_gradle_init_file   = ["${param_gradle_user_home}","init.gradle"].join("/")
             param_project_root       = ["${WORKSPACE}","${map.param_project_root}"].join("/")
-            param_project_name       = "${map.param_project_name}"
-            param_project_module     = "${map.param_project_module}"
-            param_git_repository_url = "${map.param_git_repository_url}"
-            param_git_credential_id  = "${map.param_git_credential_id}"
-            param_global_env_file_id = "${map.param_global_env_file_id}"
-            param_code_type          = "${map.param_code_type}"
+
+            param_project_name       = Optional.ofNullable("${map.param_project_name}").orElse("")
+            param_project_module     = Optional.ofNullable("${map.param_project_module}").orElse("")
+            param_git_repository_url = Optional.ofNullable("${map.param_git_repository_url}").orElse("")
+            param_git_credential_id  = Optional.ofNullable("${map.param_git_credential_id}").orElse("")
+            param_global_env_file_id = Optional.ofNullable("${map.param_global_env_file_id}").orElse("")
+            param_code_type          = Optional.ofNullable("${map.param_code_type}").orElse("")
         }
         parameters {
           gitParameter branchFilter: 'origin/(.*)', name: 'param_branch', type: 'PT_BRANCH',defaultValue: 'master',useRepository: "${map.param_git_repository_url}"
@@ -33,8 +34,9 @@ def call(Map map) {
                 steps{
                     script{
                         def d = new org.ops.Docker()
-                        d.prepare(map)
-                        d.build()
+                        println env.param_project_module
+                        //d.prepare(map)
+                        //d.build()
                     }
                 }
             }
