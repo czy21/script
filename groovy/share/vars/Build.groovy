a74a422e89217a93c2152db5cfb6a8e80714de29
Bruce CHEN <805899926@qq.com>
2021-12-02 12:12:47 +0800

    #

--- groovy/share/vars/Build.groovy
+++ groovy/share/vars/Build.groovy
@@ -6,7 +6,7 @@ def call(Map map) {
         environment {
             param_gradle_user_home   = "/var/jenkins_home/tools/gradle"
             param_gradle_init_file   = "${param_gradle_user_home}/init.gradle"
-            param_npm_cache          = "/var/jenkins_home/tools/npm-cache"
+            param_yarn_cache          = "/var/jenkins_home/tools/yarn-cache"
             param_project_root       = "${WORKSPACE}/${map.param_project_root}"
             param_project_name       = "${map.param_project_name}"
             param_project_module     = "${map.param_project_module}"
