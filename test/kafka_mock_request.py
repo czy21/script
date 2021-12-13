02eb2f29bcc69c5df59184cf41603a137ac7904c
Bruce CHEN <805899926@qq.com>
2021-12-13 15:55:19 +0800

    #

--- test/kafka_mock_request.py
+++ test/kafka_mock_request.py
@@ -8,43 +8,10 @@ import requests
 
 def publish(max):
     for i in range(0, max):
-        r = requests.post(url="http://localhost:8000/api/data-center/match/fromInstitutionMatch",
-                          data=json.dumps({
-                              "inspectSaleDTO": {
-                                  "id": "e2cfe615-bc64-11eb-aac7-00163e1e0729",
-                                  "fromInstitutionName": "全部紫阳县红椿大药房" + str(i + 1),
-                                  "fromInstitutionRinseStatus": "FAIL",
-                                  "createTime": "2020-12-31 11:13:19",
-                                  "updateTime": "2021-05-20 21:01:46",
-                                  "businessType": "SD",
-                                  "periodName": "2021财年-05",
-                                  "periodId": "8ac2749979651454017968aa75480008",
-                              },
-                              "institutionDTO": {
-                                  "score": 0.648,
-                                  "address": {
-                                      "0": "金盏乡黎各庄村128号"
-                                  },
-                                  "province": "北京市",
-                                  "city": "北京市",
-                                  "county": "朝阳区",
-                                  "name": "北京京勇安康大药房有限公司",
-                                  "standardCode": "",
-                                  "alias": "",
-                                  "uniqueId": "",
-                                  "category": "",
-                                  "state": "Active",
-                                  "code": "JXS782"
-                              },
-                              "tabName": "enterprise",
-                              "dataTypeDesc": "MONTH"
-                          }),
-                          headers={
-                              'Content-Type': 'application/json',
-                              'RS-Header-TenantId': '08dea516d0244bdaaa3a723bbe9cbd57'
-                          })
+        r = requests.get(url="https://callcenter-api.meditrusthealth.cn:8999/api/getCalllogData?key=5aekiETS5845215sdfasED&starttime=2021-10-06 17:08:23&endtime=2021-12-06 17:08:23&page_index={}&page_size=2000".format(i))
+        print(r.__sizeof__())
         print(i + 1, "次")
 
 
 if __name__ == '__main__':
-    publish(200000)
+    publish(100)
