5b9b7abfa1d8e888a8c03efab6462ce24eae9ff6
Bruce Chen <805899926@qq.com>
2021-12-26 13:55:22 +0800

    #

--- server/docker/ops/atlassian/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker build --tag ${param_registry_url}/${param_registry_dir}/confluence --file ${param_role_path}/Dockerfile-Confluence ${param_role_path}/ --no-cache --force-rm
-docker build --tag ${param_registry_url}/${param_registry_dir}/jira --file ${param_role_path}/Dockerfile-Jira ${param_role_path}/ --no-cache --force-rm
-
-docker push ${param_registry_url}/${param_registry_dir}/confluence
-docker push ${param_registry_url}/${param_registry_dir}/jira
