d692eb3b7fd672cea77880ca500267e0192d6bc4
Bruce Chen <805899926@qq.com>
2022-01-02 23:15:43 +0800

    #

--- groovy/share/src/org/ops/K8s.groovy
+++ groovy/share/src/org/ops/K8s.groovy
@@ -10,7 +10,7 @@ def build(){
 
 def apply(){
     withKubeConfig([credentialsId: env.param_kube_credential, serverUrl: env.param_kube_server]) {
-        sh 'kubectl delete -f deploy.yaml --ignore-not-found=true && kubectl apply -f deploy.yaml'
+        sh 'kubectl patch -f deploy.yaml'
     }
 }
 
