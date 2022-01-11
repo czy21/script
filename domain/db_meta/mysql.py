0aca7546db30cba1a6b05d845a84a92155f9c828
Bruce CHEN <805899926@qq.com>
2022-01-11 14:35:04 +0800

    #

--- domain/db_meta/mysql.py
+++ domain/db_meta/mysql.py
@@ -4,9 +4,10 @@ self = {
     "header": "SELECT 'executing: {{ file_path }}' AS file;",
     "footer": "SELECT 'executed: {{ file_path }}' AS file;",
     "substitution": {
-        "TrackedColumns": "\tcreated_date  TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6),\n"
-                          "\tcreated_user  varchar(36) DEFAULT NULL,\n"
-                          "\tmodified_date TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),\n"
-                          "\tmodified_user varchar(36) DEFAULT NULL"
+        "TrackedColumns": "\tcreate_time  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',\n"
+                          "\tcreate_user  varchar(64)  NOT NULL DEFAULT '' COMMENT '创建人',\n"
+                          "\tupdate_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',\n"
+                          "\tupdate_user varchar(64)  NOT NULL DEFAULT '' COMMENT '更新人',\n"
+                          "\tdeleted bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除'"
     }
 }
--- server/omv/README.md
+++ server/omv/README.md
@@ -2,3 +2,9 @@
 ```shell
 sudo dpkg-reconfigure dash # select no sh => bash
 ```
+# manual cp and compress
+```bash
+storage_dir=/srv/dev-disk-by-id-scsi-1ATA_ST4000NM000A-2HZ100_WS213MF7-part1/storage;\
+module_name=docker-data;\
+tar -czvf ${storage_dir}/backup/${module_name}-$(date +%Y%m%d-%H%M).tar.gz -C ${storage_dir} ${module_name}
+```
