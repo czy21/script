d804e02ab7f9529ef6131cd5cac2d1b7f68d2a90
Bruce CHEN <805899926@qq.com>
2022-01-15 19:01:00 +0800

    #

--- server/docker/exec.py
+++ server/docker/exec.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 import argparse
+import json
 import sys
 
 import jinja2
 import share
 from pathlib import Path
+
+import yaml
 from dotenv import dotenv_values
 
 
@@ -26,84 +29,87 @@ def invoke(role_title: str, role_path: Path, **kwargs):
     role_docker_file = role_path.joinpath("Dockerfile")
     role_init_sh = role_path.joinpath("init.sh")
     role_build_sh = role_path.joinpath("build.sh")
-
-    env_dict = {
-        **dotenv_values(env_file),
-        **{
-            "param_role_name": role_name,
-            "param_role_path": role_path.as_posix()
-        }
-    }
-    target_app_path = Path(env_dict["param_docker_data"]).joinpath(role_name)
-
-    for t in filter(lambda f: f.is_file(), role_path.rglob("*")):
-        with open(t, "r", encoding="utf-8", newline="\n") as r_file:
-            content = jinja2.Template(r_file.read()).render(**env_dict)
-            with open(t, "w", encoding="utf-8") as t_file:
-                t_file.write(content)
-
-    def docker_compose_cmd(a):
-        return 'sudo docker-compose --file {0} {1}'.format(role_compose_file.as_posix(), a)
-    if args.i:
-        if role_conf_path.exists():
-            share.execute_cmd(share.arr_param_to_str(
-                [
-                    share.role_print(role_title, "copy conf"),
-                    'sudo mkdir -p {0}'.format(target_app_path),
-                    'sudo cp -rv {0} {1}'.format(role_conf_path.as_posix(), target_app_path.as_posix())
-                ], separator=" && "
-            ))
-        if role_init_sh.exists():
-            share.execute_cmd(share.arr_param_to_str(
-                [
-                    share.role_print(role_title, "init", role_init_sh.as_posix()),
-                    "source {}".format(role_init_sh.as_posix())
-                ], separator=" && "
-            ))
-        if role_compose_file.exists():
-            share.execute_cmd(share.arr_param_to_str(
-                [
-                    share.role_print(role_title, "deploy", role_compose_file.as_posix()),
-                    docker_compose_cmd("config"),
-                    docker_compose_cmd("up --detach --build")
-                ], separator=" && "
-            ))
-    if args.d:
-        share.execute_cmd(share.arr_param_to_str(
-            [
-                share.role_print(role_title, "down", role_compose_file.as_posix()),
-                docker_compose_cmd("down")
-            ], separator=" && "
-        ))
-    share.execute_cmd("echo \n")
-
-    if args.b:
-        registry_url = env_dict['param_registry_url']
-        registry_dir = env_dict['param_registry_dir']
-        registry_username = env_dict['param_registry_username']
-        registry_password = env_dict['param_registry_password']
-        build_cmd = [
-            share.role_print(role_title, "build", role_build_sh.as_posix()),
-            'sudo docker login {0} --username {1} --password {2}'.format(registry_url, registry_username, registry_password)
-        ]
-
-        if role_docker_file.exists():
-            docker_tag = "/".join([str(p).strip("/") for p in [registry_url, registry_dir, role_name]])
-            build_cmd.append([
-                "docker build --tag {0} --file {1} {2}".format(docker_tag, role_docker_file.as_posix(), role_path.as_posix()),
-                "docker push {0}".format(docker_tag)
-            ])
-        if role_build_sh.exists():
-            build_cmd.append(
-                ["source {0}".format(t) for t in [role_env_file.as_posix(), role_build_sh.as_posix()]]
-            )
-        if role_docker_file.exists() or role_build_sh.exists():
-            share.execute_cmd(share.arr_param_to_str(build_cmd, separator=" && "))
-    share.execute_cmd("echo \n")
+    # with open(env_file, "r", encoding="utf-8", newline="\n") as o_file:
+    #     properties = yaml.unsafe_load(o_file.read())
+    # print(properties)
+    # env_dict = {
+    #     **dotenv_values(env_file),
+    #     **{
+    #         "param_role_name": role_name,
+    #         "param_role_path": role_path.as_posix()
+    #     }
+    # }
+    print(dotenv_values(env_file))
+    # target_app_path = Path(env_dict["param_docker_data"]).joinpath(role_name)
+    #
+    # for t in filter(lambda f: f.is_file(), role_path.rglob("*")):
+    #     with open(t, "r", encoding="utf-8", newline="\n") as r_file:
+    #         content = jinja2.Template(r_file.read()).render(**env_dict)
+    #         with open(t, "w", encoding="utf-8") as t_file:
+    #             t_file.write(content)
+    #
+    # def docker_compose_cmd(a):
+    #     return 'sudo docker-compose --file {0} {1}'.format(role_compose_file.as_posix(), a)
+    # if args.i:
+    #     if role_conf_path.exists():
+    #         share.execute_cmd(share.arr_param_to_str(
+    #             [
+    #                 share.role_print(role_title, "copy conf"),
+    #                 'sudo mkdir -p {0}'.format(target_app_path),
+    #                 'sudo cp -rv {0} {1}'.format(role_conf_path.as_posix(), target_app_path.as_posix())
+    #             ], separator=" && "
+    #         ))
+    #     if role_init_sh.exists():
+    #         share.execute_cmd(share.arr_param_to_str(
+    #             [
+    #                 share.role_print(role_title, "init", role_init_sh.as_posix()),
+    #                 "source {}".format(role_init_sh.as_posix())
+    #             ], separator=" && "
+    #         ))
+    #     if role_compose_file.exists():
+    #         share.execute_cmd(share.arr_param_to_str(
+    #             [
+    #                 share.role_print(role_title, "deploy", role_compose_file.as_posix()),
+    #                 docker_compose_cmd("config"),
+    #                 docker_compose_cmd("up --detach --build")
+    #             ], separator=" && "
+    #         ))
+    # if args.d:
+    #     share.execute_cmd(share.arr_param_to_str(
+    #         [
+    #             share.role_print(role_title, "down", role_compose_file.as_posix()),
+    #             docker_compose_cmd("down")
+    #         ], separator=" && "
+    #     ))
+    # share.execute_cmd("echo \n")
+    #
+    # if args.b:
+    #     registry_url = env_dict['param_registry_url']
+    #     registry_dir = env_dict['param_registry_dir']
+    #     registry_username = env_dict['param_registry_username']
+    #     registry_password = env_dict['param_registry_password']
+    #     build_cmd = [
+    #         share.role_print(role_title, "build", role_build_sh.as_posix()),
+    #         'sudo docker login {0} --username {1} --password {2}'.format(registry_url, registry_username, registry_password)
+    #     ]
+    #
+    #     if role_docker_file.exists():
+    #         docker_tag = "/".join([str(p).strip("/") for p in [registry_url, registry_dir, role_name]])
+    #         build_cmd.append([
+    #             "docker build --tag {0} --file {1} {2}".format(docker_tag, role_docker_file.as_posix(), role_path.as_posix()),
+    #             "docker push {0}".format(docker_tag)
+    #         ])
+    #     if role_build_sh.exists():
+    #         build_cmd.append(
+    #             ["source {0}".format(t) for t in [role_env_file.as_posix(), role_build_sh.as_posix()]]
+    #         )
+    #     if role_docker_file.exists() or role_build_sh.exists():
+    #         share.execute_cmd(share.arr_param_to_str(build_cmd, separator=" && "))
+    # share.execute_cmd("echo \n")
 
 
 if __name__ == '__main__':
-    env_file = Path(__file__).parent.joinpath(".env").as_posix()
+    env_file = Path(__file__).parent.joinpath("env.yml").as_posix()
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', action="store_true")
     parser.add_argument('-d', action="store_true")
