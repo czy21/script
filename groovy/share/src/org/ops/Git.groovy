85f5f7b2b4996423c920c7b2f89db257c2fe3fae
Bruce CHEN <805899926@qq.com>
2022-01-17 14:56:54 +0800

    #

--- groovy/share/src/org/ops/Git.groovy
+++ groovy/share/src/org/ops/Git.groovy
@@ -5,7 +5,7 @@ def checkout(){
     if (env.PARAM_BRANCH == null){ env.PARAM_BRANCH = 'master' }
     checkout([$class: 'GitSCM', branches: [[name: "${param_branch}"]],
             extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]],
-            userRemoteConfigs: [[credentialsId: env.param_git_credential_id, url: env.param_git_repository_url]]
+            userRemoteConfigs: [[credentialsId: "${param_git_credential_id}", url: "${param_git_repository_url}"]]
     ])
 }
 
