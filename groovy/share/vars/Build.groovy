0c6b36eaf6a41a98654f6bf162a03e4bf7025bc4
Bruce CHEN <805899926@qq.com>
2022-01-17 15:05:41 +0800

    #

--- groovy/share/vars/Build.groovy
+++ groovy/share/vars/Build.groovy
@@ -1,22 +1,22 @@
 #!/usr/bin/env groovy
 
-def call(Map map) {
+def call() {
     pipeline{
         agent any
         environment {
             param_gradle_user_home   = "/var/jenkins_home/tools/gradle"
             param_gradle_init_file   = "${param_gradle_user_home}/init.gradle"
             param_yarn_cache          = "/var/jenkins_home/tools/yarn-cache"
-            param_project_root       = "${WORKSPACE}/${map.param_project_root}"
-            param_project_name       = "${map.param_project_name}"
-            param_project_module     = "${map.param_project_module}"
-            param_git_repository_url = "${map.param_git_repository_url}"
-            param_git_credential_id  = "${map.param_git_credential_id}"
-            param_global_env_file_id = "${map.param_global_env_file_id}"
-            param_code_type          = "${map.param_code_type}"
+            param_project_root       = "${WORKSPACE}/${env.param_project_root}"
+            param_project_name       = "${env.param_project_name}"
+            param_project_module     = "${env.param_project_module}"
+            param_git_repository_url = "${env.param_git_repository_url}"
+            param_git_credential_id  = "${env.param_git_credential_id}"
+            param_global_env_file_id = "${env.param_global_env_file_id}"
+            param_code_type          = "${env.param_code_type}"
         }
         parameters {
-          gitParameter branchFilter: 'origin/(.*)', name: 'param_branch', type: 'PT_BRANCH',defaultValue: 'master',useRepository: "${map.param_git_repository_url}"
+          gitParameter branchFilter: 'origin/(.*)', name: 'param_branch', type: 'PT_BRANCH',defaultValue: 'master',useRepository: "${env.param_git_repository_url}"
         }
         stages {
             stage('clone'){
@@ -27,18 +27,18 @@ def call(Map map) {
                     }
                 }
             }
-            stage('build'){
-                when{
-                    expression { param_branch != 'master' }
-                }
-                steps{
-                    script{
-                        def d = new org.ops.Docker()
-                        d.prepare(map)
-                        d.build()
-                    }
-                }
-            }
+//             stage('build'){
+//                 when{
+//                     expression { param_branch != 'master' }
+//                 }
+//                 steps{
+//                     script{
+//                         def d = new org.ops.Docker()
+//                         d.prepare(map)
+//                         d.build()
+//                     }
+//                 }
+//             }
         }
     }
 }
