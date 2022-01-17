e7225703f69ecd6553a08d50d66126f76c528267
Bruce CHEN <805899926@qq.com>
2022-01-17 15:30:13 +0800

    #

--- groovy/share/src/org/ops/K8s.groovy
+++ groovy/share/src/org/ops/K8s.groovy
@@ -2,9 +2,8 @@
 package org.ops
 
 def build(){
-    sh 'env | grep \'^param_\' > env.conf'
-    sh 'cat env.conf'
-    sh 'cat env.conf | sed \'s/^param_//g\' | awk -F"=" \'{print $1\"=\"\"\\"\"$2\"\\"\"}\' | paste -d "," -s | xargs helm template ${param_release_name} ${param_release_chart_name} --version ${param_release_chart_version} --namespace ${param_release_namespace} --repo ${param_helm_repo} --set  2>&1 | tee deploy.yaml'
+    sh 'env | grep \'^param_\' > env.conf && cat env.conf'
+    sh "cat env.conf | sed \'s/^param_//g\' | awk -F"=" \'{print $1\"=\"\"\\"\"$2\"\\"\"}\' | paste -d "," -s | xargs helm template ${env.param_release_name} ${env.param_release_chart_name} --version ${env.param_release_chart_version} --namespace ${env.param_release_namespace} --repo ${env.param_helm_repo} --set  2>&1 | tee deploy.yaml"
 }
 
 
@@ -15,8 +14,8 @@ def apply(){
 }
 
 
-def prepare(Map map){
-    configFileProvider([configFile(fileId: "${param_global_env_file_id}", targetLocation: 'global_env.groovy', variable: 'ENV_CONFIG')]) {
+def prepare(){
+    configFileProvider([configFile(fileId: "${env.param_global_env_file_id}", targetLocation: 'global_env.groovy', variable: 'ENV_CONFIG')]) {
         load "global_env.groovy";
     }
 
@@ -34,7 +33,6 @@ def prepare(Map map){
         env.param_release_chart_version=env.param_helm_python_chart_version
         break;
      case "web":
-        env.param_backend_url="${map.param_backend_url}"
         env.param_release_chart_name= env.param_helm_web_chart_name
         env.param_release_chart_version=env.param_helm_web_chart_version
         break;
