a281a529760572c61e7c52c04a11ed1e2bf89596
Bruce CHEN <805899926@qq.com>
2022-01-17 15:10:07 +0800

    #

--- groovy/share/src/org/ops/Docker.groovy
+++ groovy/share/src/org/ops/Docker.groovy
@@ -5,7 +5,7 @@ def build(){
     switch(env.param_code_type) {
      case "java":
         gradle_cmd = ["clean","build"].collect{t->[env.param_project_module,t].findAll{ c -> ![null, "null", ""].contains(c) }.join(":")}.join(" ")
-        sh "chmod +x ${param_project_root}/gradlew && ${param_project_root}/gradlew --gradle-user-home ${param_gradle_user_home} --init-script ${param_gradle_init_file} --build-file ${param_project_root}/build.gradle ${gradle_cmd} -x test --parallel"
+        sh "chmod +x ${param_project_root}/gradlew && ${env.param_project_root}/gradlew --gradle-user-home ${env.param_gradle_user_home} --init-script ${env.param_gradle_init_file} --build-file ${env.param_project_root}/build.gradle ${gradle_cmd} -x test --parallel"
         break;
      case "go":
         break;
@@ -20,9 +20,9 @@ def build(){
         println [env.param_code_type,"not config"].join(" ");
         return;
     }
-    sh 'docker login ${param_registry_repo} --username ${param_registry_username} --password ${param_registry_password}'
-    sh 'docker build --tag ${param_image_name}:${param_release_version} --file ${param_docker_file} ${param_project_context} --no-cache --force-rm'
-    sh 'docker push ${param_image_name}:${param_release_version}'
+    sh 'docker login ${env.param_registry_repo} --username ${env.param_registry_username} --password ${env.param_registry_password}'
+    sh 'docker build --tag ${env.param_image_name}:${env.param_release_version} --file ${env.param_docker_file} ${env.param_project_context} --no-cache --force-rm'
+    sh 'docker push ${env.param_image_name}:${env.param_release_version}'
 }
 
 
