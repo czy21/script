970b80df2e88f24c5794c24baaaf6ea3be2926b2
Bruce CHEN <805899926@qq.com>
2022-01-26 13:15:23 +0800

    #

--- domain/db_meta/mysql.py
+++ domain/db_meta/mysql.py
@@ -5,9 +5,9 @@ self = {
     "footer": "SELECT 'executed: {{ file_path }}' AS file;",
     "substitution": {
         "TrackedColumns": "\tcreate_time  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',\n"
-                          "\tcreate_user  varchar(64)  NULL COMMENT '创建人',\n"
-                          "\tupdate_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',\n"
-                          "\tupdate_user varchar(64)   NULL  COMMENT '更新人',\n"
+                          "\tcreate_user  varchar(36)  NULL COMMENT '创建人',\n"
+                          "\tupdate_time  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',\n"
+                          "\tupdate_user  varchar(36)  NULL  COMMENT '更新人',\n"
                           "\tdeleted bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除'"
     }
 }
