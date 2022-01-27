ca1fef93894a76823597bef5d64eede1e7c69685
Bruce Chen <805899926@qq.com>
2022-01-27 22:19:23 +0800

    #

--- server/docker/install.sh
+++ server/docker/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# bash install.sh -h user@host -i -c
+# bash install.sh -h user@host -i
 # bash install.sh -i -c
 # -i exec init.sh and start compose.yaml
 # -b exec build.sh
 # -c exec post.sh
+# -p param_cluster_env <env>
 
 while getopts ":h" opt
 do
