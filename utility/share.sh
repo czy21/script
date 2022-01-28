bd929a66919bc1176ffadb1376bb4a333ebd085a
Bruce CHEN <805899926@qq.com>
2022-01-28 16:14:25 +0800

    #

--- utility/share.sh
+++ utility/share.sh
@@ -11,20 +11,20 @@ function upload_exec_py() {
   scp -rqC ${pwd_path} $host:
   scp -rqC ${pwd_path}/../requirements.txt ${pwd_path}/../.env ${utility_dir}/share.py $host:${name_path}
 
-  local args=""
-  local exec_cmd=()
+  local args
+  local exec_cmd
   for i in "$@" ; do
     if [ "-r" == $i ]; then
-        exec_cmd+=('sudo pip3 install --requirement $HOME/'${name_path}/'requirements.txt;')
+        exec_cmd+='sudo pip3 install --requirement $HOME/'${name_path}/'requirements.txt;'
         shift 1
         continue
     fi
     args+=" $i"
   done
-  exec_cmd+=('python3 -B $HOME/'${name_path}/'exec.py '${args}';')
-  exec_cmd+=(${prune_cmd})
-  echo -e '\033[32m command: \033[0m'${exec_cmd[@]}
-  ssh $host ${exec_cmd[@]}
+  exec_cmd+='python3 -B $HOME/'${name_path}/'exec.py '${args}';'
+  exec_cmd+=${prune_cmd}
+  echo -e '\033[32m command: \033[0m'${exec_cmd}
+  ssh $host ${exec_cmd}
 }
 
 function upload_exec_sh() {
