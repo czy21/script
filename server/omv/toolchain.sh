4197e07f5a52642c30c424326758325f51bb211c
Bruce Chen <805899926@qq.com>
2022-02-08 22:14:52 +0800

    #

--- server/omv/toolchain.sh
+++ server/omv/toolchain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # bash toolchain.sh -f backup -p [path] -d [dir name]
-# bash toolchain.sh -f prune  -t [path] -m [file prefix] -n [month]
+# bash toolchain.sh -f prune  -t [path] -n [day]
 
 function backup() {
 	local p_dir;
@@ -18,29 +18,23 @@ function backup() {
 }
 
 function prune() {
-	local target_path;
-	local file_prefix;
+	local target_regex;
 	local minus_number;
-	while getopts "t:m:n:" opt;do
+	while getopts "t:n:" opt;do
 		case $opt in
-			t) target_path=$OPTARG;;
-			m) file_prefix=$OPTARG;;
+			t) target_regex=$OPTARG;;
 			n) minus_number=$OPTARG;;
 			*) exit 1 ;;
 		esac
 	done;
-	minus_month=$(date -d "-${minus_number} month" +%Y%m)
-	if [ -z "${target_path}" ]; then
+	minus_date=$(date -d "-${minus_number} day" +%Y%m%d)
+	if [ -z "${target_regex}" ]; then
 		echo "option requires an argument -- t";
 		exit 1;
 	fi
-	if [ -z "${file_prefix}" ]; then
-		echo "option requires an argument -- m";
-		exit 1;
-	fi
-	echo -e "${p_dir}/${d_dir}\033[32m prune started \033[0m"
-	find ${target_path} -name $file_prefix$minus_month -exec rm -rf{}\;
-	echo -e "${p_dir}/${d_dir}\033[32m prune finished \033[0m"
+	# echo -e "${target_regex}\033[32m prune started \033[0m"
+	find ${target_regex}-* -exec sh -c 'f={};file_date=$(basename ${f} | cut -d"-" -f3);if [ $(date +%s -d ${file_date}) -ge '$(date +%s -d ${minus_date})' ]; then echo $f;fi;' \;
+	# echo -e "${target_regex}\033[32m prune finished \033[0m"
 }
 
 while getopts "f:" opt;do
