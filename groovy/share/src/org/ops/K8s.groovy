605ccacdb839fa6f6a55b3cd2ef3139acb386119
Bruce CHEN <805899926@qq.com>
2022-03-02 16:11:27 +0800

    #

--- groovy/share/src/org/ops/K8s.groovy
+++ groovy/share/src/org/ops/K8s.groovy
@@ -3,7 +3,6 @@ package org.ops
 
 def apply() {
     sh 'env | grep \'^param_\' | sed -r \'s/=/: /\' | sed \'s/^param_//g\' > values.yaml'
-    sh 'sleep 99999'
     withKubeConfig([credentialsId: env.param_kube_credential, serverUrl: env.param_kube_server]) {
         sh "helm upgrade --install ${env.param_release_name} ${env.param_release_chart_name} --version ${env.param_release_chart_version} --namespace ${env.param_release_namespace} --repo ${env.param_helm_repo} --values values.yaml --output yaml"
     }
