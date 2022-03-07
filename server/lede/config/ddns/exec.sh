2df734df699481d896124a99866809df0ebe6464
Bruce CHEN <805899926@qq.com>
2022-03-07 15:31:26 +0800

    #

---
+++ server/lede/config/ddns/exec.sh
@@ -0,0 +1,10 @@
+# /bin/bash
+
+function prune() {
+  # prune domain
+  for i in $(seq $(uci show {{ param_role_name }} | grep '^{{ param_role_name }}\(.*\)=service' | wc -l) -1 1);do uci del {{ param_role_name }}.@service[$(($i-1))]; done
+}
+
+function backup() {
+  echo backup
+}
