238cc964b8fd4f2ab252064544768928cdbc23eb
Bruce CHEN <805899926@qq.com>
2022-03-07 15:04:32 +0800

    #

--- server/lede/config/dhcp/exec.sh
+++ server/lede/config/dhcp/exec.sh
@@ -4,7 +4,3 @@ function prune() {
   # prune domain
   for i in $(uci show dhcp | grep '^dhcp.@domain\(.*\)=domain' | sed "s/=domain//; s/[^[:digit:]]//g" | sort -rn); do uci del dhcp.@domain[${i}];done
 }
-
-function backup() {
-  echo backup
-}
