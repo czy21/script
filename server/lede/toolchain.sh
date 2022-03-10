b2626b9bfff9e9e1975d5f2ab79353c168c38738
Bruce CHEN <805899926@qq.com>
2022-03-10 18:54:48 +0800

    #

--- server/lede/toolchain.sh
+++ server/lede/toolchain.sh
@@ -9,9 +9,9 @@ do
 		h)
       source ../../utility/share.sh
       host=$2
-      shift 2
-      upload_exec_py $@
-      break
+      shift 2;
+      upload_exec_py $@;
+      break;
 			;;
 	esac
 done
