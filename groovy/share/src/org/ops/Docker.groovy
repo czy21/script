afbecdb40d576fa0479627bd52cdb52d8da25857
Bruce CHEN <805899926@qq.com>
2022-03-25 11:51:30 +0800

    #

--- groovy/share/src/org/ops/Docker.groovy
+++ groovy/share/src/org/ops/Docker.groovy
@@ -15,7 +15,7 @@ def build() {
             env.NODEJS_HOME = "${tool 'node-v16.13.2'}"
             env.PATH = "${NODEJS_HOME}/bin:${PATH}"
             yarn_cmd = "yarn --cwd ${env.param_project_context} --registry ${env.param_npm_repo} --cache-folder ${env.param_yarn_cache}"
-            sh "${yarn_cmd} install --no-lockfile --update-checksums && ${yarn_cmd} --ignore-engines build"
+            sh "${yarn_cmd} install --update-checksums && ${yarn_cmd} --ignore-engines build"
             break;
         default:
             println[env.param_code_type, "not config"].join(" ");
