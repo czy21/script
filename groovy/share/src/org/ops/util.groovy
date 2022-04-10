85e790ff76936b0bcffd776e28394c1af6c571b9
Bruce CHEN <805899926@qq.com>
2022-04-10 18:29:04 +0800

    #

---
+++ groovy/share/src/org/ops/util.groovy
@@ -0,0 +1,6 @@
+@Grab('org.apache.commons:commons-lang3:3.12.0')
+import org.apache.commons.lang3.StringUtils
+
+def isEmptyWithNullString() {
+  return StringUtils.isEmpty(c) || c.equals("null")
+}
