a102ffe86c2b5cd56caf09ed74abedbf721398c9
Bruce Chen <805899926@qq.com>
2022-04-10 18:49:15 +0800

    #

--- groovy/share/src/org/ops/Util.groovy
+++ groovy/share/src/org/ops/Util.groovy
@@ -3,7 +3,7 @@ package org.ops
 @Grab('org.apache.commons:commons-lang3:3.12.0')
 import org.apache.commons.lang3.StringUtils
 
-def isEmptyWithNullString(String str) {
+static def isEmptyWithNullString(String str) {
   return StringUtils.isEmpty(str) || str.equals("null")
 }
 
