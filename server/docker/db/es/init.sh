72ac9ce793e395d4e038590542bc013e41884124
Bruce Chen <805899926@qq.com>
2022-04-14 21:48:40 +0800

    # add es and kibana for docker

--- server/docker/db/es/init.sh
+++ server/docker/db/es/init.sh
@@ -2,6 +2,8 @@
 set -e
 
 sudo sysctl -w vm.max_map_count=262144
-
 data_dir={{ param_docker_data }}/{{ param_role_name }}/data/
-sudo mkdir -p ${data_dir} && sudo chmod 777 -R ${data_dir}
+for ((i=1;i<{{ param_db_minio_cluster_replicas }};i++));do
+ data_node_dir=${data_dir}/${i}
+ sudo mkdir -p ${data_node_dir} && sudo chmod 777 -R ${data_node_dir}
+done
