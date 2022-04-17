b3cbee236566883b20aff3a52c5051386911bda5
Bruce CHEN <805899926@qq.com>
2022-04-17 22:35:51 +0800

    #

--- server/docker/app/flink/init.sh
+++ server/docker/app/flink/init.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-jar_dir={{ param_docker_data }}/{{ param_role_name }}/data/jar/
-completed_job_dir={{ param_docker_data }}/{{ param_role_name }}/data/completed-jobs/
-log_dir={{ param_docker_data }}/{{ param_role_name }}/log/
-mkdir -p ${jar_dir} && chmod -R 777 ${jar_dir}
-mkdir -p ${completed_job_dir} && chmod -R 777 ${completed_job_dir}
-mkdir -p ${log_dir} && chmod -R 777 ${log_dir}
+anonymous_dirs=(
+{{ param_docker_data }}/{{ param_role_name }}/data/jar/
+{{ param_docker_data }}/{{ param_role_name }}/data/completed-jobs/
+{{ param_docker_data }}/{{ param_role_name }}/log/jmr/
+{{ param_docker_data }}/{{ param_role_name }}/log/his/
+)
+
+for (( i=1;i<={{ param_bd_flink_cluster_tmr_replicas }};i++)); do
+anonymous_dirs+=" {{ param_docker_data }}/{{ param_role_name }}/log/tmr/${i}"
+done;
+
+for t in ${anonymous_dirs[@]}; do
+  sudo mkdir -p ${t} && sudo chmod -R 777 ${t}
+done
