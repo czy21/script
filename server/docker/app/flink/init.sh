63c8342acdff085165f3af577f7e45bae7164160
Bruce CHEN <805899926@qq.com>
2022-04-17 22:07:34 +0800

    #

--- server/docker/app/flink/init.sh
+++ server/docker/app/flink/init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-set -e
-
-
+jar_dir={{ param_docker_data }}/{{ param_role_name }}/data/jar/
+completed_job_dir={{ param_docker_data }}/{{ param_role_name }}/data/completed-jobs/
+log_dir={{ param_docker_data }}/{{ param_role_name }}/log/
+mkdir -p ${jar_dir} && chmod -R 777 ${jar_dir}
+mkdir -p ${completed_job_dir} && chmod -R 777 ${completed_job_dir}
+mkdir -p ${log_dir} && chmod -R 777 ${log_dir}
