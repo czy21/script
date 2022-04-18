52d59e3b3543099b9b39af867385a3553f87595d
Bruce Chen <805899926@qq.com>
2022-04-18 18:50:29 +0800

    #

--- server/docker/app/flink/init.sh
+++ server/docker/app/flink/init.sh
@@ -1,16 +1,1 @@
 #!/bin/bash
-
-anonymous_dirs=(
-{{ param_docker_data }}/{{ param_role_name }}/data/jar/
-{{ param_docker_data }}/{{ param_role_name }}/data/completed-jobs/
-{{ param_docker_data }}/{{ param_role_name }}/log/jmr/
-{{ param_docker_data }}/{{ param_role_name }}/log/his/
-)
-
-for (( i=1;i<={{ param_bd_flink_cluster_tmr_replicas }};i++)); do
-anonymous_dirs+=" {{ param_docker_data }}/{{ param_role_name }}/log/tmr/${i}"
-done;
-
-for t in ${anonymous_dirs[@]}; do
-  sudo mkdir -p ${t} && sudo chmod -R 777 ${t}
-done
