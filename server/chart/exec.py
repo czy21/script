38eb10708603f3caa319e4d42e2ff9e4f171d51c
Bruce CHEN <805899926@qq.com>
2022-04-20 14:25:14 +0800

    #

--- server/chart/exec.py
+++ server/chart/exec.py
@@ -4,6 +4,7 @@ import pathlib
 
 import jinja2
 import share
+import yaml
 
 
 def invoke(role_title: str, role_path: pathlib.Path, **kwargs):
@@ -12,16 +13,20 @@ def invoke(role_title: str, role_path: pathlib.Path, **kwargs):
 
     role_name = role_path.name
     role_values_file = role_path.joinpath("values.yaml")
+    role_values_override_file = role_path.joinpath("values.override.yaml")
 
     temp_all_in_one_path = role_path.joinpath("___temp/deploy.yaml")
     temp_all_in_one_path.parent.mkdir(parents=True, exist_ok=True)
     temp_all_in_one_path.touch()
 
     if role_values_file.exists():
-        with open(role_values_file, "r", encoding="utf-8", newline="\n") as r_file:
-            content = jinja2.Template(r_file.read()).render(**env_dict)
+        with open(role_values_file, "r", encoding="utf-8", newline="\n") as vf:
+            content = jinja2.Template(vf.read()).render(**env_dict)
             with open(role_values_file, "w", encoding="utf-8") as t_file:
                 t_file.write(content)
+
+    with open(role_values_override_file, "w", encoding="utf-8") as ovf:
+        yaml.dump(env_dict, ovf)
 
     def helm_action_cmd():
         _action = args.a
@@ -32,7 +37,7 @@ def invoke(role_title: str, role_path: pathlib.Path, **kwargs):
             _action = "upgrade --install"
         helm_cmd = [
             "helm {0} {1} {2}".format(_action, role_name, role_path.as_posix()),
-            "--set {0}".format(",".join(["=".join([k, "\"" + v + "\""]) for (k, v) in env_dict.items()]))
+            "--values {0}".format(role_values_override_file)
         ]
         if not args.ignore_namespace:
             helm_cmd.append("--namespace {0}".format(args.n))
@@ -40,7 +45,7 @@ def invoke(role_title: str, role_path: pathlib.Path, **kwargs):
             helm_cmd.append("--create-namespace")
         if _action == "template":
             helm_cmd.append("> {0}".format(temp_all_in_one_path))
-        return "&&".join([
+        return " && ".join([
             'helm dep up {0}'.format(role_path.as_posix()),
             share.flat_to_str(helm_cmd)
         ])
