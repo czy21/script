7afc71050b5316fa7fa8929f866da20cc51d6b45
Bruce CHEN <805899926@qq.com>
2022-04-20 10:13:54 +0800

    #

--- server/docker/db/es/init.sh
+++ server/docker/db/es/init.sh
@@ -3,7 +3,7 @@ set -e
 
 sudo sysctl -w vm.max_map_count=262144
 data_dir={{ param_docker_data }}/{{ param_role_name }}/data/
-for ((i=1;i<{{ param_db_minio_cluster_replicas }};i++));do
+for ((i=1;i<{{ param_db_es_cluster_replicas }};i++));do
  data_node_dir=${data_dir}/${i}
  sudo mkdir -p ${data_node_dir} && sudo chmod 777 -R ${data_node_dir}
 done
