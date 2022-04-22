6a8515d51b92621e4722be9f16a6f1ab7f13a8fa
Bruce CHEN <805899926@qq.com>
2022-04-22 17:08:17 +0800

    #

--- domain/db_meta/mysql.py
+++ domain/db_meta/mysql.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
-
+createTimeColumn = "create_time  TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间'"
+createUserColumn = "create_user  varchar(36)  NULL COMMENT '创建人'"
+updateTimeColumn = "update_time  TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新时间'"
+updateUserColumn = "update_user  varchar(36)  NULL COMMENT '更新人'"
+deletedColumn = "deleted bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除'"
 self = {
     "header": "SELECT 'executing: {{ file_path }}' AS file;",
     "footer": "SELECT 'executed: {{ file_path }}' AS file;",
     "substitution": {
-        "TrackedColumns": "\tcreate_time  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',\n"
-                          "\tcreate_user  varchar(36)  NULL COMMENT '创建人',\n"
-                          "\tupdate_time  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',\n"
-                          "\tupdate_user  varchar(36)  NULL  COMMENT '更新人',\n"
-                          "\tdeleted bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除'"
+        "CreateTimeColumn": "\t{0}".format(createTimeColumn),
+        "CreateUserColumn": "\t{0}".format(createUserColumn),
+        "UpdateTimeColumn": "\t{0}".format(updateTimeColumn),
+        "UpdateUserColumn": "\t{0}".format(updateUserColumn),
+        "DeletedColumn": "\t{0}".format(deletedColumn),
+        "TrackColumn": ",\n".join(["\t{0}".format(t) for t in [createTimeColumn, createUserColumn, updateTimeColumn, updateUserColumn]]),
     }
 }
