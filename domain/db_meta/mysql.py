4e481232f06150d4abf7fcccf635e2bf1b4e3b17
Bruce CHEN <805899926@qq.com>
2022-04-23 21:25:16 +0800

    #

--- domain/db_meta/mysql.py
+++ domain/db_meta/mysql.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-createTimeColumn = "create_time  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间'"
-createUserColumn = "create_user  varchar(36)  NULL COMMENT '创建人'"
-updateTimeColumn = "update_time  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'"
-updateUserColumn = "update_user  varchar(36)  NULL COMMENT '更新人'"
+createTimeColumn = "create_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间'"
+createUserColumn = "create_user varchar(36)  NULL COMMENT '创建人'"
+updateTimeColumn = "update_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'"
+updateUserColumn = "update_user varchar(36)  NULL COMMENT '更新人'"
 deletedColumn = "deleted bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除'"
 self = {
     "header": "SELECT 'executing: {{ file_path }}' AS file;",
@@ -13,6 +13,6 @@ self = {
         "UpdateTimeColumn": "\t{0}".format(updateTimeColumn),
         "UpdateUserColumn": "\t{0}".format(updateUserColumn),
         "DeletedColumn": "\t{0}".format(deletedColumn),
-        "TrackColumn": ",\n".join(["\t{0}".format(t) for t in [createTimeColumn, createUserColumn, updateTimeColumn, updateUserColumn]]),
+        "TrackColumn": ",\n".join(["\t{0}".format(t) for t in [createTimeColumn, createUserColumn, updateTimeColumn, updateUserColumn, deletedColumn]]),
     }
 }
