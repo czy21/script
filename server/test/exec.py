cea85b541a4a8b5f870d4cbab20cafe54d9af903
Bruce CHEN <805899926@qq.com>
2022-05-03 11:05:24 +0800

    #

---
+++ server/test/exec.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import pathlib
+import stat
+
+import share
+
+if __name__ == '__main__':
+    private_key = pathlib.Path(__file__).parent.joinpath("___temp/private-key").as_posix()
+    os.chmod(private_key, stat.S_IRUSR)
+    ansible_hosts = pathlib.Path(__file__).parent.joinpath("ansible-hosts").as_posix()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', required=True)
+    parser.add_argument('-k', action="store_true")
+    args = parser.parse_args()
+    ansible_file = pathlib.Path(__file__).parent.joinpath(args.i + ".yml").as_posix()
+    ansible_cmd = share.flat_to_str([
+        "ANSIBLE_STDOUT_CALLBACK=yaml",
+        "ANSIBLE_SUDO_PASS=0",
+        "ANSIBLE_HOST_KEY_CHECKING=0",
+        "ANSIBLE_FORCE_COLOR=1",
+        "ansible-playbook",
+        "--inventory", ansible_hosts, ansible_file,
+        "--ask-pass" if args.k else ["--private-key", private_key],
+        "--flush-cache",
+        "-v"
+    ])
+    print(ansible_cmd)
+    share.execute_cmd(ansible_cmd)
---
+++ server/test/toolchain.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# bash toolchain.sh -h user@host -i [inventory_name] -t [tags]
+# -i site | general
+# -t [tags within inventory_file]
+#    site: os | docker | hosts | k8s-master | k8s-node
+#    general: prune | k8s-reset
+
+while getopts "h:" opt
+do
+	case $opt in
+		h)
+      source ../../utility/share.sh
+      host=$2
+      shift 2
+      upload_exec_py $@
+      break
+			;;
+	esac
+done
