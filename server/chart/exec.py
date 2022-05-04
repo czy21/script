b6b9f4a7d298a5a8b51e8b4200acad4335da5475
Bruce CHEN <805899926@qq.com>
2022-05-04 16:55:50 +0800

    #

--- server/chart/exec.py
+++ server/chart/exec.py
@@ -28,28 +28,6 @@ def invoke(role_title: str, role_path: pathlib.Path, **kwargs):
     with open(role_values_override_file, "w", encoding="utf-8") as ovf:
         yaml.dump(env_dict, ovf)
 
-    def helm_action_cmd():
-        _action = args.a
-        _extension = ""
-        if _action == "delete":
-            return "helm delete {0} {1}".format(role_name, "" if args.ignore_namespace else "--namespace {0}".format(args.n))
-        if _action == "install":
-            _action = "upgrade --install"
-        helm_cmd = [
-            "helm {0} {1} {2}".format(_action, role_name, role_path.as_posix()),
-            "--values {0}".format(role_values_override_file)
-        ]
-        if not args.ignore_namespace:
-            helm_cmd.append("--namespace {0}".format(args.n))
-        if args.create_namespace:
-            helm_cmd.append("--create-namespace")
-        if _action == "template":
-            helm_cmd.append("> {0}".format(temp_all_in_one_path))
-        return " && ".join([
-            'helm dep up {0}'.format(role_path.as_posix()),
-            share.flat_to_str(helm_cmd)
-        ])
-
     _cmds = [
         share.role_print(role_title, args.a)
     ]
@@ -59,13 +37,28 @@ def invoke(role_title: str, role_path: pathlib.Path, **kwargs):
         helm_repo_url = env_dict["param_helm_repo_url"]
         helm_username = env_dict["param_helm_username"]
         helm_password = env_dict["param_helm_password"]
-        _cmds.append(share.flat_to_str([
-            "helm plugin list | if [ -z \"$(grep -w nexus-push)\" ];then helm plugin install --version master https://github.com/sonatype-nexus-community/helm-nexus-push.git;fi",
-            "helm repo   list | if [ -z \"$(grep -w {0})\" ];then helm repo add {0} {1};fi".format(helm_repo_name, helm_repo_url),
-            "helm package {0} --destination {0} | sed 's/Successfully packaged chart and saved it to: //g' | xargs helm nexus-push {1}  --username {2} --password {3}".format(role_path, helm_repo_name, helm_username, helm_password)
-        ], delimiter=" && "))
+        _cmds.append("helm plugin list | if [ -z \"$(grep -w nexus-push)\" ];then helm plugin install --version master https://github.com/sonatype-nexus-community/helm-nexus-push.git;fi")
+        _cmds.append("helm repo   list | if [ -z \"$(grep -w {0})\" ];then helm repo add {0} {1};fi".format(helm_repo_name, helm_repo_url))
+        _cmds.append("helm package {0} --destination {0} | sed 's/Successfully packaged chart and saved it to: //g' | xargs helm nexus-push {1}  --username {2} --password {3}".format(role_path, helm_repo_name, helm_username, helm_password))
+
     else:
-        _cmds.append(helm_action_cmd())
+        _action = args.a
+        _extension = ""
+        if _action == "delete":
+            return "helm delete {0} {1}".format(role_name, "" if args.ignore_namespace else "--namespace {0}".format(args.n))
+        if _action == "install":
+            _action = "upgrade --install"
+        helm_cmd = [
+            "helm {0} {1} {2} --values {3}".format(_action, role_name, role_path.as_posix(), role_values_override_file)
+        ]
+        if not args.ignore_namespace:
+            helm_cmd.append("--namespace {0}".format(args.n))
+        if args.create_namespace:
+            helm_cmd.append("--create-namespace")
+        if _action == "template":
+            helm_cmd.append("> {0}".format(temp_all_in_one_path))
+        _cmds.append('helm dep up {0}'.format(role_path.as_posix()))
+        _cmds.append(share.flat_to_str(helm_cmd))
     _cmd_str = share.flat_to_str(_cmds, delimiter=" && ")
     share.execute_cmd(_cmd_str)
 
