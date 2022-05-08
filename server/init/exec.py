1ce9b086e3105cb6e5fb7c69ef1be0f5de92b596
Bruce CHEN <805899926@qq.com>
2022-05-08 11:09:27 +0800

    #

--- server/init/exec.py
+++ server/init/exec.py
@@ -5,8 +5,14 @@ import pathlib
 import stat
 
 import share
+import yaml
 
 if __name__ == '__main__':
+    yaml.add_constructor('!join', lambda loader, node: "".join(loader.construct_sequence(node, deep=True)))
+    with open(pathlib.Path(__file__).parent.joinpath("env.yaml").as_posix(), mode="r", encoding="utf-8") as ef:
+        with open(pathlib.Path(__file__).parent.joinpath("vars/env.yml"), mode="w", encoding="utf-8") as vf:
+            yaml.dump(yaml.full_load(ef), vf)
+
     private_key = pathlib.Path(__file__).parent.joinpath("___temp/private-key").as_posix()
     os.chmod(private_key, stat.S_IRUSR)
     ansible_hosts = pathlib.Path(__file__).parent.joinpath("ansible-hosts").as_posix()
@@ -16,6 +22,7 @@ if __name__ == '__main__':
     parser.add_argument('-k', action="store_true")
     args = parser.parse_args()
     ansible_file = pathlib.Path(__file__).parent.joinpath(args.i + ".yml").as_posix()
+
     ansible_cmd = share.flat_to_str([
         "ANSIBLE_SUDO_PASS=0",
         "ANSIBLE_HOST_KEY_CHECKING=0",
