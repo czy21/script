a88110ad3a212409106350ea15931cb76fdbc99d
Bruce CHEN <805899926@qq.com>
2022-05-08 22:28:35 +0800

    #

--- server/init/roles/docker/templates/centos.sh
+++ server/init/roles/docker/templates/centos.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -e
 
-# docker
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo sed -i.bak -e "s,^baseurl=https://download.docker.com/linux,baseurl=http://{{ param_mirror_yum }},g" /etc/yum.repos.d/docker-ce.repo
-sudo yum -y install docker-ce docker-ce-cli containerd.io
+sudo yum -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 sudo systemctl enable docker
-
-# docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
