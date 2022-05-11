a61f5cb22cd8c6588c97043486904fd5787ddc4b
Bruce CHEN <805899926@qq.com>
2022-05-11 17:44:49 +0800

    #

--- server/ssl/exec.py
+++ server/ssl/exec.py
@@ -11,22 +11,14 @@ import yaml
 
 
 def invoke(role_title: str, role_path: pathlib.Path, **kwargs):
-    args = kwargs["args"]
     root_path = kwargs["root_path"]
-    env_dict: dict = kwargs["env_dict"]
     role_name = role_path.name
-
-    for t in filter(lambda f: f.is_file() and share.exclude_match("({0})".format("|".join(args.excludes)), f.as_posix()), role_path.rglob("*")):
-        with open(t, "r", encoding="utf-8", newline="\n") as sf:
-            content = jinja2.Template(sf.read()).render(**env_dict)
-            with open(t, "w", encoding="utf-8") as tf:
-                tf.write(content)
 
     _cmds = []
     role_build_sh = role_path.joinpath("build.sh")
     if role_build_sh.exists():
         _cmds.append("bash {0}".format(role_build_sh.as_posix()))
-        root_role_tmp_path = pathlib.Path(root_path).joinpath(role_name)
+        root_role_tmp_path = pathlib.Path(root_path).joinpath("___temp").joinpath(role_name)
         _cmds.append("mkdir -p {0}".format(root_role_tmp_path))
         _cmds.append("cp -r {0}/* {1}/".format(role_path.joinpath("___temp"), root_role_tmp_path))
     _cmd_str = share.flat_to_str([_cmds, "echo \n"], delimiter=" && ")
