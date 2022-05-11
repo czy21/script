a8fbd5253cfdc5d022c1b04bd3a5ad5920352a42
Bruce CHEN <805899926@qq.com>
2022-05-11 10:55:26 +0800

    #

--- utility/share.py
+++ utility/share.py
@@ -118,7 +118,6 @@ def execute(ctx, func, **kwargs):
         for t in filter(lambda f: f.is_file(), role_path.rglob("*")):
             _exclude_bools = [exclude_match(r, t.as_posix()) for r in global_jinja2ignore_rules]
             if all(_exclude_bools):
-                print(t)
                 with open(t, "r", encoding="utf-8", newline="\n") as sf:
                     content = jinja2.Template(sf.read()).render(**role_dict)
                     with open(t, "w", encoding="utf-8") as tf:
