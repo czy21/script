5fc269b07062d806fadeb0a34589a812a1c10f02
Bruce CHEN <805899926@qq.com>
2022-05-12 15:50:49 +0800

    #

--- server/docker/exec.py
+++ server/docker/exec.py
@@ -12,17 +12,18 @@ def invoke(role_title: str, role_path: pathlib.Path, **kwargs):
     role_node_path = role_path.joinpath("node")
     role_node_selected = share.get_dir_dict(role_node_path, select_tip="node num(example:1)") if role_node_path.exists() else None
     role_node_target_path: pathlib.Path = role_node_selected.get(next(iter(role_node_selected))) if role_node_selected else None
+    role_node_deploy_file = role_node_target_path.joinpath("deploy.yml")
     if role_node_path.exists():
         if role_node_target_path:
             share.execute_cmd(share.flat_to_str([
                 share.role_print(role_title, "copy node"),
-                "cp -rv {0}/* {1}".format(role_node_target_path.as_posix(), role_path.as_posix())
+                "find {0} -maxdepth 1 ! -path {0} ! -name deploy.yml -exec cp -rv -t {1}/".format(role_node_target_path.as_posix(), role_path.as_posix()) + " {} \\;"
             ], delimiter=" && "))
         else:
             return
     role_name = role_path.name
     role_conf_path = role_path.joinpath("conf")
-    role_compose_file = role_path.joinpath("deploy.yml")
+    role_deploy_file = role_path.joinpath("deploy.yml")
     role_docker_file = role_path.joinpath("Dockerfile")
     role_init_sh = role_path.joinpath("init.sh")
     role_build_sh = role_path.joinpath("build.sh")
@@ -30,7 +31,13 @@ def invoke(role_title: str, role_path: pathlib.Path, **kwargs):
     target_app_path = pathlib.Path(env_dict["param_docker_data"]).joinpath(role_name)
 
     def docker_compose_cmd(option):
-        return 'sudo docker-compose --project-name {0} --file {1} --file {2} {3}'.format(role_name, kwargs["base_deploy_file"], role_compose_file.as_posix(), option)
+        role_deploy_files = [
+            kwargs["base_deploy_file"],
+            role_deploy_file.as_posix()
+        ]
+        if role_node_target_path.exists():
+            role_deploy_files.append(role_node_deploy_file)
+        return 'sudo docker-compose --project-name {0} {1} {2}'.format(role_name, " ".join(["--file {0}".format(t) for t in role_deploy_files]), option)
 
     _cmds = []
 
@@ -42,14 +49,14 @@ def invoke(role_title: str, role_path: pathlib.Path, **kwargs):
         if role_init_sh.exists():
             _cmds.append(share.role_print(role_title, "init", role_init_sh.as_posix()))
             _cmds.append("source {}".format(role_init_sh.as_posix()))
-        if role_compose_file.exists():
-            _cmds.append(share.role_print(role_title, "deploy", role_compose_file.as_posix()))
+        if role_deploy_file.exists():
+            _cmds.append(share.role_print(role_title, "deploy", role_deploy_file.as_posix()))
             if args.debug:
                 _cmds.append(docker_compose_cmd("config"))
             _cmds.append(docker_compose_cmd(share.flat_to_str("up --detach --build --remove-orphans")))
     if args.d:
-        if role_compose_file.exists():
-            _cmds.append(share.role_print(role_title, "down", role_compose_file.as_posix()))
+        if role_deploy_file.exists():
+            _cmds.append(share.role_print(role_title, "down", role_deploy_file.as_posix()))
             _cmds.append(docker_compose_cmd("down --remove-orphans"))
 
     if args.b:
