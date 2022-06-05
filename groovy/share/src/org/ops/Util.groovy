329d402adf622d145d54529d91b3553298b6d4ac
Bruce CHEN <805899926@qq.com>
2022-06-05 19:59:49 +0800

    #

--- groovy/share/src/org/ops/Util.groovy
+++ groovy/share/src/org/ops/Util.groovy
@@ -1,11 +1,13 @@
 #!/usr/bin/env groovy
 package org.ops
+import java.nio.file.Path
 
 static def isEmpty(String str) {
     return str == null || str.length() == 0 || "null".equals(str)
 }
 
 static def isNotEmpty(String str) {
+    println(Path.of("a","b/","c",""))
     return !isEmpty(str)
 }
 
