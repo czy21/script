998e3d2919b8847b3b8829af2581ad71d96eccbe
Bruce CHEN <805899926@qq.com>
2022-06-05 17:52:02 +0800

    #

--- groovy/share/src/org/ops/Docker.groovy
+++ groovy/share/src/org/ops/Docker.groovy
@@ -7,18 +7,15 @@ def build() {
     configFileProvider([configFile(fileId: "${env.param_global_env_file_id}", targetLocation: 'global_env.groovy', variable: 'ENV_CONFIG')]) {
         load "global_env.groovy";
     }
-    // project
     env.param_project_context = [env.param_project_root, env.param_project_module].findAll { t -> Util.isNotEmpty(t as String) }.join("/")
-    // release
+    env.param_release_version = params.param_branch
     env.param_release_name = [
-        "${env.param_registry_repo}/${env.param_registry_dir}",
-        Util.isEmpty(env.param_release_name as String)
-        ? [env.param_project_name, env.param_project_module].findAll { t -> Util.isNotEmpty(t as String) }.join("-")
-        : env.param_release_name
+            "${env.param_registry_repo}/${env.param_registry_dir}",
+            Util.isEmpty(env.param_release_name as String)
+            ? [env.param_project_name, env.param_project_module].findAll { t -> Util.isNotEmpty(t as String) }.join("-") 
+            : env.param_release_name
     ].join("/")
-    env.param_release_version = Util.isNotEmpty(env.param_release_version as String) ? env.param_release_version : params.param_branch
-    // docker
-    env.param_docker_context = Util.isNotEmpty(env.param_docker_context as String) ? env.param_docker_context : env.param_project_context
+    env.param_docker_context = Util.isNotEmpty(env.param_project_module as String) ? env.param_project_context : env.param_project_root
     env.param_docker_file = "${env.param_docker_context}/Dockerfile"
 
     // build
@@ -43,7 +40,7 @@ def build() {
     }
     // docker push
     sh "docker login ${env.param_registry_repo} --username ${env.param_registry_username} --password ${env.param_registry_password}"
-    sh "docker build --tag ${env.param_release_name}:${env.param_release_version} --file ${env.param_docker_file} ${env.param_docker_context}"
+    sh "docker build --tag ${env.param_release_name}:${env.param_release_version} --file ${env.param_docker_file} ${env.param_project_context}"
     sh "docker push ${env.param_release_name}:${env.param_release_version}"
 }
 
