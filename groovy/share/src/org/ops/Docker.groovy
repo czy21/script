6f29bd207bcbf624ffb71dd7bd237d64fe5748c0
Bruce CHEN <805899926@qq.com>
2022-06-22 18:36:31 +0800

    #

--- groovy/share/src/org/ops/Docker.groovy
+++ groovy/share/src/org/ops/Docker.groovy
@@ -66,7 +66,7 @@ def build() {
     sh "${build_cmd}"
     sh "docker build --tag ${env.param_release_name}:${env.param_release_version} --file ${env.param_docker_file} ${env.param_docker_context}"
     configFileProvider([configFile(fileId: "docker-config", targetLocation: '.jenkins-build/docker/config.json')]) {
-        sh "docker --config .jenkins-build/docker/config.json push ${env.param_release_name}:${env.param_release_version}"
+        sh "docker --config .jenkins-build/docker/ push ${env.param_release_name}:${env.param_release_version}"
     }
 }
 
