e46546c2db2f66ecfeea5676461f3b589f343e85
Bruce CHEN <805899926@qq.com>
2022-06-22 20:59:05 +0800

    #

--- jenkins/src/org/ops/Docker.groovy
+++ jenkins/src/org/ops/Docker.groovy
@@ -20,12 +20,12 @@ def build() {
                              : Util.ofPath(env.param_project_root,env.param_docker_context)
     env.param_docker_file = Util.ofPath(env.param_docker_context,"Dockerfile")
 
-    Map<String, Runnable> tools = new HashMap<>()
-    tools["java"] = {
+    tools = [
+      java: {
         env.JAVA_HOME = "${tool 'jdk-17'}"
         env.PATH = "${JAVA_HOME}/bin:${PATH}"
-    }
-    tools["go"] = {
+      },
+      go: {
         env.GO_HOME = "${tool 'go-v1.18.2'}"
         env.GOPROXY = env.param_go_proxy
         env.GOCACHE = env.param_go_cache
@@ -33,16 +33,16 @@ def build() {
         env.GOMODCACHE = env.param_go_mod_cache
         env.CGO_ENABLED = "0"
         env.PATH = "${GO_HOME}/bin:${PATH}"
-    }
-    tools["web"] = {
+      },
+      web: {
         env.NODEJS_HOME = "${tool 'node-v16.14.0'}"
         env.PATH = "${NODEJS_HOME}/bin:${PATH}"
-    }
-
+      }
+    ]
     build_cmd = ""
     switch (env.param_code_type) {
         case "java":
-            tools.get(env.param_code_type).run()
+            tools.get(env.param_code_type).call()
             build_cmd = Util.format(
                 "chmod +x {0}/gradlew && {0}/gradlew --gradle-user-home {1} --init-script {2} --build-file {0}/build.gradle {3} -x test --refresh-dependencies",
                 env.param_project_root,
@@ -52,7 +52,7 @@ def build() {
             )
             break;
         case "go":
-            tools.get(env.param_code_type).run()
+            tools.get(env.param_code_type).call()
             build_cmd = Util.format(
                "cd {0};go build -o build main.go;",
                env.param_project_context
@@ -61,7 +61,7 @@ def build() {
         case "python":
             break;
         case "web":
-            tools.get(env.param_code_type).run()
+            tools.get(env.param_code_type).call()
             yarn_cmd = Util.format(
                 "yarn --cwd {0} --registry {1} --cache-folder {2}",
                 env.param_project_context,
@@ -72,7 +72,7 @@ def build() {
             break;
         case "shell":
             if (Util.isNotEmpty(env.param_tools)) {
-             env.param_tools.split(",").each{ t -> tools.get(t).run() }
+             env.param_tools.split(",").each{ t -> tools.get(t).call() }
             }
             build_cmd = Util.format("chmod +x {0};{0}",Util.ofPath(env.param_project_root,env.param_project_shell_file))
         default:
