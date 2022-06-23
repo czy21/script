4f99b1c204979fdb85294b2342279b9c6a053bdd
Bruce CHEN <805899926@qq.com>
2022-06-23 16:14:33 +0800

    #

--- jenkins/src/org/ops/K8s.groovy
+++ jenkins/src/org/ops/K8s.groovy
@@ -24,7 +24,7 @@ def apply() {
       }
     ]
     release.get(env.param_code_type).call()
-    Common.writeParamToYaml()
+    Common.writeParamToYaml(this)
     withKubeConfig([credentialsId: env.param_kube_credential, serverUrl: env.param_kube_server]) {
         helm_cmd = StringUtils.format(
             "helm upgrade --install {0} {1} --version {2} --namespace {3} --repo {4} --values .jenkins/param.yaml --output yaml",
