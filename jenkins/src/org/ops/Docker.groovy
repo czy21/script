e1326ac6842e32b993ab46f0c105fbeb9d291a36
Bruce CHEN <805899926@qq.com>
2022-06-23 15:46:22 +0800

    #

--- jenkins/src/org/ops/Docker.groovy
+++ jenkins/src/org/ops/Docker.groovy
@@ -76,8 +76,7 @@ def build() {
     ]
     build_cmd = cmd.get(env.param_code_type).call()
     def param = readProperties text: sh(script: 'env | grep \'^param_\'', returnStdout: true).trim()
-    param = CollectionUtils.sortMapByKey(param)
-    writeYaml file: '.jenkins/param.yaml', data: param, charset: 'UTF-8', overwrite: true
+    writeYaml file: '.jenkins/param.yaml', data: CollectionUtils.sortMapByKey(param), charset: 'UTF-8', overwrite: true
      sh "${build_cmd}"
      sh "docker build --tag ${env.param_release_name}:${env.param_release_version} --file ${env.param_docker_file} ${env.param_docker_context}"
      configFileProvider([configFile(fileId: "docker-config", targetLocation: '.jenkins/docker/config.json')]) {
