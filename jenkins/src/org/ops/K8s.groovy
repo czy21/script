498148cdd35c230867877fe225862140be94543e
Bruce CHEN <805899926@qq.com>
2022-06-23 11:18:25 +0800

    #

--- jenkins/src/org/ops/K8s.groovy
+++ jenkins/src/org/ops/K8s.groovy
@@ -2,28 +2,25 @@
 package org.ops
 
 def apply() {
-
-    switch (env.param_code_type) {
-        case "java":
-            env.param_release_chart_name = env.param_helm_java_chart_name
-            env.param_release_chart_version = env.param_helm_java_chart_version
-            break;
-        case "go":
-            env.param_release_chart_name = env.param_helm_go_chart_name
-            env.param_release_chart_version = env.param_helm_go_chart_version
-            break;
-        case "python":
-            env.param_release_chart_name = env.param_helm_python_chart_name
-            env.param_release_chart_version = env.param_helm_python_chart_version
-            break;
-        case "web":
-            env.param_release_chart_name = env.param_helm_web_chart_name
-            env.param_release_chart_version = env.param_helm_web_chart_version
-            break;
-        default:
-            println(env.param_code_type + " not config" as String);
-            return;
-    }
+    release = [
+      java: {
+        env.param_release_chart_name = env.param_helm_java_chart_name
+        env.param_release_chart_version = env.param_helm_java_chart_version
+      },
+      go: {
+        env.param_release_chart_name = env.param_helm_go_chart_name
+        env.param_release_chart_version = env.param_helm_go_chart_version
+      },
+      py: {
+        env.param_release_chart_name = env.param_helm_python_chart_name
+        env.param_release_chart_version = env.param_helm_python_chart_version
+      },
+      web: {
+        env.param_release_chart_name = env.param_helm_web_chart_name
+        env.param_release_chart_version = env.param_helm_web_chart_version
+      }
+    ]
+    release.get(env.param_code_type).call()
 
     // k8s apply
     sh 'env | grep \'^param_\' | sed \'s/=/: /\' | sed \'s/^param_//\' > values.yaml'
