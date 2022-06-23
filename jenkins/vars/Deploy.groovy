f870bdff8c5e951f8dc37868515a591d4ed226e0
Bruce CHEN <805899926@qq.com>
2022-06-23 11:11:30 +0800

    #

--- jenkins/vars/Deploy.groovy
+++ jenkins/vars/Deploy.groovy
@@ -21,6 +21,14 @@ def call() {
             stage('deploy') {
                 steps {
                     script {
+                        configFileProvider([configFile(fileId: "${env.param_global_env_file_id}", targetLocation: '.jenkins/default_param.groovy')]) {
+                            param = load ".jenkins/default_param.groovy"
+                            param.each{ k,v->
+                              if (env.getProperty(k) == null) {
+                                env.setProperty(k,v)
+                              }
+                            }
+                        }
                         new org.ops.K8s().apply()
                     }
                 }
