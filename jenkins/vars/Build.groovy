e473b3fd0ce669a2dc46889f0af033c68a7d3bea
Bruce CHEN <805899926@qq.com>
2022-06-23 16:47:03 +0800

    #

--- jenkins/vars/Build.groovy
+++ jenkins/vars/Build.groovy
@@ -2,23 +2,24 @@
 import org.ops.Docker
 import org.ops.util.PathUtils
 
+
 def call() {
-
-    env.param_gradle_user_home = "/var/jenkins_home/tools/gradle"
-    env.param_gradle_init_file = "/conf/init.gradle"
-    env.param_go_mod_cache = "/var/jenkins_home/tools/go/pkg/mod"
-    env.param_go_cache = "/var/jenkins_home/tools/go/cache/go-build"
-    env.param_yarn_cache = "/var/jenkins_home/tools/yarn/cache"
-    env.param_project_root = PathUtils.ofPath("${env.WORKSPACE}", "${env.param_project_root}")
-    env.param_project_name = "${env.param_project_name}"
-    env.param_project_module = "${env.param_project_module}"
-    env.param_git_repository_url = "${env.param_git_repository_url}"
-    env.param_git_credential_id = "${env.param_git_credential_id}"
-    env.param_global_env_file_id = "${env.param_global_env_file_id}"
-    env.param_code_type = "${env.param_code_type}"
-
     pipeline {
         agent any
+        environment {
+            param_gradle_user_home = "/var/jenkins_home/tools/gradle"
+            param_gradle_init_file = "/conf/init.gradle"
+            param_go_mod_cache = "/var/jenkins_home/tools/go/pkg/mod"
+            param_go_cache = "/var/jenkins_home/tools/go/cache/go-build"
+            param_yarn_cache = "/var/jenkins_home/tools/yarn/cache"
+            param_project_root = PathUtils.ofPath("${env.WORKSPACE}", "${env.param_project_root}")
+            param_project_name = "${env.param_project_name}"
+            param_project_module = "${env.param_project_module}"
+            param_git_repository_url = "${env.param_git_repository_url}"
+            param_git_credential_id = "${env.param_git_credential_id}"
+            param_global_env_file_id = "${env.param_global_env_file_id}"
+            param_code_type = "${env.param_code_type}"
+        }
         parameters {
             gitParameter branchFilter: 'origin/(.*)', name: 'param_branch', type: 'PT_BRANCH', defaultValue: 'master', useRepository: "${env.param_git_repository_url}"
         }
