d01309293b10776696f425df6b120d7dee551883
Bruce CHEN <805899926@qq.com>
2022-06-23 16:30:29 +0800

    #

--- jenkins/src/org/ops/Docker.groovy
+++ jenkins/src/org/ops/Docker.groovy
@@ -5,81 +5,81 @@ package org.ops
 import org.ops.util.PathUtils
 import org.ops.util.StringUtils
 
-def build() {
-    env.param_project_context = PathUtils.ofPath(env.param_project_root, env.param_project_module)
-    env.param_release_version = params.param_branch
-    env.param_release_name = PathUtils.ofPath(
-            env.param_registry_repo,
-            env.param_registry_dir,
-            StringUtils.isEmpty(env.param_release_name)
-                    ? StringUtils.join("-", env.param_project_name, env.param_project_module)
-                    : env.param_release_name
+static def build(script) {
+    script.env.param_project_context = PathUtils.ofPath(script.env.param_project_root, script.env.param_project_module)
+    script.env.param_release_version = script.params.param_branch
+    script.env.param_release_name = PathUtils.ofPath(
+            script.env.param_registry_repo,
+            script.env.param_registry_dir,
+            StringUtils.isEmpty(script.env.param_release_name)
+                    ? StringUtils.join("-", script.env.param_project_name, script.env.param_project_module)
+                    : script.env.param_release_name
     )
-    env.param_docker_context = env.param_docker_context == null
-            ? env.param_project_context
-            : PathUtils.ofPath(env.param_project_root, env.param_docker_context)
-    env.param_docker_file = PathUtils.ofPath(env.param_docker_context, "Dockerfile")
+    script.env.param_docker_context = script.env.param_docker_context == null
+            ? script.env.param_project_context
+            : PathUtils.ofPath(script.env.param_project_root, script.env.param_docker_context)
+    script.env.param_docker_file = PathUtils.ofPath(script.env.param_docker_context, "Dockerfile")
 
-    tool = [
+    def tool = [
             java: {
-                env.JAVA_HOME = "${tool 'jdk-17'}"
-                env.PATH = "${JAVA_HOME}/bin:${PATH}"
+                script.env.JAVA_HOME = "${script.tool 'jdk-17'}"
+                script.env.PATH = "${script.env.JAVA_HOME}/bin:${script.PATH}"
             },
             go  : {
-                env.GO_HOME = "${tool 'go-v1.18.2'}"
-                env.GOPROXY = env.param_go_proxy
-                env.GOCACHE = env.param_go_cache
-                env.GOSUMDB = "off"
-                env.GOMODCACHE = env.param_go_mod_cache
-                env.CGO_ENABLED = "0"
-                env.PATH = "${GO_HOME}/bin:${PATH}"
+                script.env.GO_HOME = "${script.tool 'go-v1.18.2'}"
+                script.env.GOPROXY = script.env.param_go_proxy
+                script.env.GOCACHE = script.env.param_go_cache
+                script.env.GOSUMDB = "off"
+                script.env.GOMODCACHE = script.env.param_go_mod_cache
+                script.env.CGO_ENABLED = "0"
+                script.env.PATH = "${script.env.GO_HOME}/bin:${script.PATH}"
             },
             web : {
-                env.NODEJS_HOME = "${tool 'node-v16.14.0'}"
-                env.PATH = "${NODEJS_HOME}/bin:${PATH}"
+                script.env.NODEJS_HOME = "${script.tool 'node-v16.14.0'}"
+                script.env.PATH = "${script.env.NODEJS_HOME}/bin:${script.PATH}"
             }
     ]
-    cmd = [
+    def cmd = [
             java : {
                 tool.get("java").call()
                 return StringUtils.format(
                         "chmod +x {0}/gradlew && {0}/gradlew --gradle-user-home {1} --init-script {2} --build-file {0}/build.gradle {3} -x test --refresh-dependencies",
-                        env.param_project_root,
-                        env.param_gradle_user_home,
-                        env.param_gradle_init_file,
-                        ["clean", "build"].collect { t -> StringUtils.join(":", env.param_project_module, t) }.join(" ")
+                        script.env.param_project_root,
+                        script.env.param_gradle_user_home,
+                        script.env.param_gradle_init_file,
+                        ["clean", "build"].collect { t -> StringUtils.join(":", script.env.param_project_module, t) }.join(" ")
                 )
             },
             go   : {
                 tool.get("go").call()
                 return StringUtils.format(
                         "cd {0};go build -o build main.go;",
-                        env.param_project_context
+                        script.env.param_project_context
                 )
             },
             web  : {
                 tool.get("web").call()
-                yarn_cmd = StringUtils.format(
+                def yarn_cmd = StringUtils.format(
                         "yarn --cwd {0} --registry {1} --cache-folder {2}",
-                        env.param_project_context,
-                        env.param_npm_repo,
-                        env.param_yarn_cache
+                        script.env.param_project_context,
+                        script.env.param_npm_repo,
+                        script.env.param_yarn_cache
                 )
                 return StringUtils.format("{0} install --no-lockfile --update-checksums && {0} --ignore-engines build", yarn_cmd)
             },
             shell: {
-                if (StringUtils.isNotEmpty(env.param_tools)) {
-                    env.param_tools.split(",").each { t -> tool.get(t).call() }
+                if (StringUtils.isNotEmpty(script.env.param_tools)) {
+                    script.env.param_tools.split { "," }.each { t -> tool.get(t).call() }
                 }
-                return StringUtils.format("chmod +x {0};{0}", PathUtils.ofPath(env.param_project_root, env.param_project_shell_file))
+                return StringUtils.format("chmod +x {0};{0}", PathUtils.ofPath(script.env.param_project_root, script.env.param_project_shell_file))
             }
     ]
-    build_cmd = cmd.get(env.param_code_type).call()
+    def build_cmd = cmd.get(script.env.param_code_type).call()
     Common.writeParamToYaml(this)
-    sh "${build_cmd}"
-    sh "docker build --tag ${env.param_release_name}:${env.param_release_version} --file ${env.param_docker_file} ${env.param_docker_context}"
-    configFileProvider([configFile(fileId: "docker-config", targetLocation: '.jenkins/docker/config.json')]) {
-        sh "docker --config .jenkins/docker/ push ${env.param_release_name}:${env.param_release_version}"
+    script.sh "${build_cmd}"
+    script.sh "docker build --tag ${script.env.param_release_name}:${script.env.param_release_version} --file ${script.env.param_docker_file} ${script.env.param_docker_context}"
+    script.configFileProvider([script.configFile(fileId: "docker-config", targetLocation: '.jenkins/docker/config.json')]) {
+        script.sh "docker --config .jenkins/docker/ push ${script.env.param_release_name}:${script.env.param_release_version}"
     }
 }
 
