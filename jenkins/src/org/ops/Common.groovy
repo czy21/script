5dc705a86a90d442eac4a96926017a9be8cbc1d6
Bruce CHEN <805899926@qq.com>
2022-06-23 16:12:47 +0800

    #

---
+++ jenkins/src/org/ops/Common.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+package org.ops
+
+import org.ops.util.CollectionUtils
+
+static def writeParamToYaml() {
+    def param = readProperties text: sh(script: 'env | grep \'^param_\'', returnStdout: true).trim()
+    writeYaml file: '.jenkins/param.yaml', data: CollectionUtils.sortMapByKey(param as Map<String, Object>), charset: 'UTF-8', overwrite: true
+}
+
+return this
