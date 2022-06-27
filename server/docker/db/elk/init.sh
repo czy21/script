d9aa59c7b9af771e4dd826e24a21acd2a228ee14
Bruce CHEN <805899926@qq.com>
2022-06-27 18:53:14 +0800

    #

--- server/docker/db/elk/init.sh
+++ server/docker/db/elk/init.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -e
 
-
 sudo sysctl -w vm.max_map_count=262144
-data_dir={{ param_role_target_path }}/data/
-for ((i=1;i<={{ param_db_es_cluster_replicas }};i++));do
- data_node_dir=${data_dir}/${i}
- sudo mkdir -p ${data_node_dir} && sudo chmod 777 -R ${data_node_dir}
-done
