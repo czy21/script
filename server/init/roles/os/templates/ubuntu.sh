5ca45be2d8082f2698545639edf3bb5750040cae
Bruce CHEN <805899926@qq.com>
2022-06-28 14:36:38 +0800

    #

--- server/init/roles/os/templates/ubuntu.sh
+++ server/init/roles/os/templates/ubuntu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-sed -i.bak "s,\(ca.archive\|archive\|security\).ubuntu.com,{{ param_mirror_apt }},g" /etc/apt/sources.list
+sudo sed -i.bak "s,\(ca.archive\|archive\|security\).ubuntu.com,{{ param_mirror_apt }},g" /etc/apt/sources.list
 
-apt -y update
-apt -y install wget vim git network-manager nfs-common make gcc python3-pip
+sudo apt -y update
+sudo NEEDRESTART_SUSPEND=1 apt -y install wget vim git network-manager nfs-common make gcc python3-pip
 
 # postgres repo
 #sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
@@ -13,21 +13,26 @@ apt -y install wget vim git network-manager nfs-common make gcc python3-pip
 # install optional
 #apt -y install mysql-client postgresql-client
 
-sed -i -r "s/^\s*%sudo.*/%sudo   ALL=(ALL:ALL) NOPASSWD:ALL/;" /etc/sudoers
+sudo sed -i 's|^%sudo.*|%sudo   ALL=(ALL:ALL) NOPASSWD:ALL|g' /etc/sudoers
 
-sed -i -r "s/^\s*UseDNS\s+\w+/#\0/; s/^\s*PermitRootLogin\s+\w+/#\0/; s/^\s*PasswordAuthentication\s+\w+/#\0/; s/^\s*ClientAliveInterval\s+\w+/#\0/" /etc/ssh/sshd_config
-echo "
+sudo sed -i -r \
+-e 's/^\s*UseDNS\s+\w+/#\0/' \
+-e 's/^\s*PermitRootLogin\s+\w+/#\0/' \
+-e 's/^\s*PasswordAuthentication\s+\w+/#\0/' \
+-e 's/^\s*ClientAliveInterval\s+\w+/#\0/' /etc/ssh/sshd_config
+
+
+sudo -u root bash -c 'echo "
 UseDNS no
 PermitRootLogin yes
 PasswordAuthentication no
 ClientAliveInterval 30
-" >>/etc/ssh/sshd_config
+" >> /etc/ssh/sshd_config'
 
 public_key="set -e;cd;mkdir -p .ssh;chmod 700 .ssh;echo {{ param_ssh_public_key }} > .ssh/authorized_keys;chmod 644 .ssh/authorized_keys"
-sudo -u root bash -c "${public_key}"
 sudo -u {{ param_user_ops }} bash -c "${public_key}"
 
-ufw disable
+sudo ufw disable
 
-swapoff -a
-sed -i -r "s/^\s*/swap.img\s+\w+/#\0/" /etc/fstab
+sudo swapoff -a
+sudo sed -i -r "s/^\s*/swap.img\s+\w+/#\0/" /etc/fstab
