da0b5dccbb8ec1707cefb545b664a4726834642c
Bruce CHEN <805899926@qq.com>
2022-06-29 18:50:49 +0800

    #

--- utility/log.py
+++ utility/log.py
@@ -1,9 +1,6 @@
 # coding:utf-8
 import logging
-import os
 import pathlib
-from pathlib import Path
-from time import sleep
 
 import colorlog
 
@@ -15,45 +12,14 @@ log_colors_config = {
     'CRITICAL': 'red',
 }
 
-log_sleep = 0.1
 
-
-class Logger:
-
-    def __init__(self, name=None):
-        self.logger = logging.getLogger(name if name else __name__)
-        self.logger.setLevel(logging.INFO)
-        ch = colorlog.StreamHandler()
-        ch.setFormatter(colorlog.ColoredFormatter(
-            '%(white)s%(asctime)s %(log_color)s%(levelname)s %(purple)s%(thread)d %(white)s[ %(threadName)s ] %(cyan)s%(name)s %(white)s- %(message)s',
-            log_colors=log_colors_config))
-        self.logger.addHandler(ch)
-
-    def set_log_file(self, file: pathlib.Path):
+def init_logger(name=None, file: pathlib.Path = None):
+    logger = logging.getLogger(name)
+    ch = colorlog.StreamHandler()
+    color_format = '%(white)s%(asctime)s %(log_color)s%(levelname)s %(purple)s%(thread)d %(white)s[ %(threadName)s ] %(cyan)s%(name)s %(white)s- %(message)s'
+    ch.setFormatter(colorlog.ColoredFormatter(color_format, log_colors=log_colors_config))
+    logger.addHandler(ch)
+    if file:
         fh = logging.FileHandler(filename=file.absolute().as_posix(), encoding='utf-8')
         fh.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(thread)d [ %(threadName)s ] %(name)s - %(message)s'))
-        self.logger.addHandler(fh)
-
-    def debug(self, message, name=None):
-        if self.logger.level == logging.DEBUG:
-            if name:
-                self.logger.name = name
-            self.logger.debug(message)
-
-    def info(self, message, name=None):
-        if self.logger.level == logging.INFO:
-            if name:
-                self.logger.name = name
-            self.logger.info(message)
-
-    def warning(self, message, name=None):
-        if self.logger.level == logging.WARNING:
-            if name:
-                self.logger.name = name
-            self.logger.warning(message)
-
-    def error(self, message, name=None):
-        if self.logger.level == logging.ERROR:
-            if name:
-                self.logger.name = name
-            self.logger.error(message)
+        logger.addHandler(fh)
--- utility/regex.py
+++ utility/regex.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+import json
+import logging
 import re
+
+logger = logging.getLogger()
 
 
 def is_match(pattern: str, name: str):
@@ -8,3 +12,9 @@ def is_match(pattern: str, name: str):
 
 def not_match(pattern: str, text: str):
     return not is_match(pattern, text)
+
+
+def match_rules(rules: list, text: str, name: str = ""):
+    _rules = [{"rule": r, "isMatch": is_match(r, text)} for r in rules]
+    logger.debug("{0} rules: {1} => {2}".format(name, text, json.dumps(_rules)))
+    return _rules
