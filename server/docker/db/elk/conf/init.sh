31f3f8d06b8890c5edb9b97471e635cf5c1d9563
Bruce CHEN <805899926@qq.com>
2022-06-30 20:54:52 +0800

    #

--- server/docker/db/elk/conf/init.sh
+++ server/docker/db/elk/conf/init.sh
@@ -5,15 +5,15 @@ for ((i=1;i<={{ param_db_es_cluster_replicas }};i++));do
  mkdir -p ${data_node_dir} && chown -R 1000:0 ${data_node_dir}
 done
 
-cert_dir=/usr/share/elasticsearch/config/cert
+cert_path={{ param_db_es_target_cert_path }}
 
 args="--silent --pem"
-if [ -f ${cert_dir}/ca/ca.crt ] && [ -f ${cert_dir}/ca/ca.key ]; then
-  args+=" --ca-cert ${cert_dir}/ca/ca.crt --ca-key ${cert_dir}/ca/ca.key"
+if [ -f ${cert_path}/ca/ca.crt ] && [ -f ${cert_path}/ca/ca.key ]; then
+  args+=" --ca-cert ${cert_path}/ca/ca.crt --ca-key ${cert_path}/ca/ca.key"
 else
   args+=" --keep-ca-key"
 fi;
 echo -e ${args}
-rm -rf ${cert_dir}/bundle.zip
-bin/elasticsearch-certutil cert ${args} --in config/instance.yml -out ${cert_dir}/bundle.zip && unzip -n ${cert_dir}/bundle.zip -d ${cert_dir}/
-chown -R 1000:0 ${cert_dir}
+rm -rf ${cert_path}/bundle.zip
+bin/elasticsearch-certutil cert ${args} --in config/instance.yml -out ${cert_path}/bundle.zip && unzip -n ${cert_path}/bundle.zip -d ${cert_path}/
+chown -R 1000:0 ${cert_path}
