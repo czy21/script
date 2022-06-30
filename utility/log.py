f293add3dae0ef56633f68906737376e00016df1
Bruce CHEN <805899926@qq.com>
2022-06-30 11:12:25 +0800

    #

--- utility/log.py
+++ utility/log.py
@@ -20,6 +20,9 @@ def init_logger(name=None, file: pathlib.Path = None):
     ch.setFormatter(colorlog.ColoredFormatter(color_format, log_colors=log_colors_config))
     logger.addHandler(ch)
     if file:
+        if not file.exists():
+            file.parent.mkdir(exist_ok=True, parents=True)
+            file.touch(exist_ok=True)
         fh = logging.FileHandler(filename=file.absolute().as_posix(), encoding='utf-8')
         fh.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(thread)d [ %(threadName)s ] %(name)s - %(message)s'))
         logger.addHandler(fh)
