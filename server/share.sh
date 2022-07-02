d385db4fe227551fa82479419f058f922c15c0d6
Bruce Chen <805899926@qq.com>
2022-07-02 15:22:10 +0800

    #

--- server/share.sh
+++ server/share.sh
@@ -4,6 +4,9 @@
 # -r install requirement.txt
 
 function upload_exec_py() {
+  local PYTHON_HOME="\$HOME/.python3"
+  local PYTHON_EXEC="${PYTHON_HOME}/bin/python3"
+  local is_debug=false
   local book_source=$(pwd)
   local book_target=$(basename ${book_source})
   local book_target_temp_path=${book_target}/___temp
@@ -16,11 +19,8 @@ function upload_exec_py() {
   ${scp_cmd} ${book_source} $host:
   ${scp_cmd} ${book_source}/../requirements.txt ${book_source}/../env.yaml ${book_source}/../share.py ${utility} $host:${book_target}
 
-  local args
-  local exec_cmd=()
-  local is_debug=false
-  local PYTHON_HOME="\$HOME/.python3-opsor"
-  local PYTHON_EXEC="${PYTHON_HOME}/bin/python3"
+  local args=""
+  local exec_cmd=""
   exec_cmd+="if [ ! -d ${PYTHON_HOME} ];then python3 -m venv ${PYTHON_HOME};fi && "
   for ((i=1;i<="$#";i++));do
     item=${!i}
@@ -36,10 +36,8 @@ function upload_exec_py() {
   done
   exec_cmd+="${PYTHON_EXEC} -B \$HOME/${book_target}/main.py ${args}"
   ${ssh_cmd} ${exec_cmd}
-  if ${ssh_cmd} "[ -d ${book_target_temp_path} ]"; then
-    ${scp_cmd} $host:${book_target_temp_path}/ ${book_source}/
-  fi
-  if [ ${is_debug} = false ]; then
+  ${scp_cmd} $host:${book_target_temp_path}/ ${book_source}/
+  if [ ${is_debug} == false ]; then
     ${ssh_cmd} ${del_cmd}
   fi
 }
