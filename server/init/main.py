de5829fb0383f07c1e852cd0fdcc387a05c3e834
Bruce CHEN <805899926@qq.com>
2022-07-03 16:04:03 +0800

    #

--- server/init/main.py
+++ server/init/main.py
@@ -7,13 +7,11 @@ import stat
 import share
 import yaml
 
-from utility import collection as collection_util, file as file_util
+from utility import collection as collection_util, file as file_util, yaml as yaml_util
 
 if __name__ == '__main__':
-    yaml.add_constructor('!join', file_util.yaml_tag_join)
-    yaml.add_constructor('!decode', file_util.yaml_tag_decode)
-    env_dict = file_util.read_file(pathlib.Path(__file__).parent.joinpath("env.yaml"), lambda f: yaml.full_load(f))
-    file_util.write_file(pathlib.Path(__file__).parent.joinpath("vars/env.yml"), lambda f: f.write(yaml.dump(env_dict)))
+    env_dict = yaml_util.load(file_util.read_text(pathlib.Path(__file__).parent.joinpath("env.yaml")))
+    file_util.write_text(pathlib.Path(__file__).parent.joinpath("vars/env.yml"), yaml.dump(env_dict))
 
     with open(pathlib.Path(__file__).parent.joinpath("env.yaml").as_posix(), mode="r", encoding="utf-8") as ef:
         with open(pathlib.Path(__file__).parent.joinpath("vars/env.yml"), mode="w", encoding="utf-8") as vf:
@@ -23,11 +21,13 @@ if __name__ == '__main__':
     os.chmod(private_key, stat.S_IRUSR)
     ansible_hosts = pathlib.Path(__file__).parent.joinpath("ansible-hosts").as_posix()
     parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--param', nargs="+", default=[])
     parser.add_argument('-i', required=True)
     parser.add_argument('-t', required=True)
     parser.add_argument('-k', action="store_true")
     parser.add_argument('-u', '--user', type=str, default=env_dict["param_user_ops"])
     args = parser.parse_args()
+    print(args)
     ansible_file = pathlib.Path(__file__).parent.joinpath(args.i + ".yml").as_posix()
 
     ansible_cmd = [
@@ -42,6 +42,10 @@ if __name__ == '__main__':
         "--step",
         # "--verbose"
     ]
+    if args.param:
+        param_extra_iter = iter(args.param)
+        _param = " ".join(["{0}={1}".format(k, v) for k, v in dict(zip(param_extra_iter, param_extra_iter)).items()])
+        ansible_cmd.append("-e \"{0}\"".format(_param))
     if args.user:
         ansible_cmd.append("--user {0}".format(args.user))
     share.run_cmd(collection_util.flat_to_str(ansible_cmd))
