dcc6487130921812333f01f395a9091b88a1aaad
Bruce Chen <805899926@qq.com>
2022-07-04 23:00:50 +0800

    #

--- server/router/ddns/build.sh
+++ server/router/ddns/build.sh
@@ -5,16 +5,15 @@ function backup() {
   mkdir -p ${bak_path}
 
   cat /dev/null > ${bak_path}/{{ param_script_uci_file_name }}
-
+  local pattern="\(ALI_DNS_1\|ALI_DNS_2\)"
   uci show {{ param_role_name }} \
-  | grep '@domain' \
+  | grep "^{{ param_role_name }}.${pattern}" \
   | sed \
-  -e 's|\[.*\]|[-1]|g' \
-  -e 's|.*=domain|add {{ param_role_name }} domain|g' >> ${bak_path}/{{ param_script_uci_file_name }}
+  -e 's|^{{ param_role_name }}|set \0|g' >> ${bak_path}/{{ param_script_uci_file_name }}
 }
 
 function install() {
-	uci show {{ param_role_name }} | grep '^{{ param_role_name }}.@domain\(.*\)=domain' | sed -e 's|^{{ param_role_name }}.@domain|delete \0|g' -e 's|=domain||g' -e '1!G;h;$!d' | uci batch
+	uci show {{ param_role_name }} | grep '.service_name' | sed -e 's|.service_name.*$||g' -e 's|^ddns|delete \0|g' | uci batch
   sed -e '$i commit {{ param_role_name }}' {{ param_role_path }}/{{ param_script_uci_file_name }} | uci batch
 }
 
