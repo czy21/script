f39829dcca79f6900fb13f8003a30e598d4dcf22
Bruce CHEN <805899926@qq.com>
2022-07-04 12:49:29 +0800

    #

--- server/docker/main.py
+++ server/docker/main.py
@@ -81,7 +81,7 @@ def invoke(role_title: str, role_path: pathlib.Path, role_env: dict, args: argpa
             docker_image_tag = "/".join([str(p).strip("/") for p in [registry_url, registry_dir, role_name]])
             _cmds.append("docker build --tag {0} --file {1} {2}".format(docker_image_tag, role_docker_file.as_posix(), role_path.as_posix()))
             _cmds.append("docker push {0}".format(docker_image_tag))
-    _cmd_str = collection_util.flat_to_str([_cmds, "echo \n"], delimiter=" && ")
+    _cmd_str = collection_util.flat_to_str(_cmds, delimiter=" && ")
     share.run_cmd(_cmd_str)
 
 
