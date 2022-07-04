cafe91ce2e050dc7ac148af9535679cb7bed15d7
Bruce CHEN <805899926@qq.com>
2022-07-04 19:22:39 +0800

    #

--- server/router/dhcp/build.sh
+++ server/router/dhcp/build.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 echo $@
+function backup() {
+  echo "backup"
+}
+
+function install() {
+	echo "install"
+}
+
+while getopts $@ opt;do
+	case $opt in
+		f)
+		  func_name=$2;
+		  ${func_name} $@;
+		  break;
+		;;
+	esac
+done
