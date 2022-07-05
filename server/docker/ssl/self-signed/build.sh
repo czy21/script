950e3ce1f5dbaec8974da7419b139f9cdb81b2d1
Bruce CHEN <805899926@qq.com>
2022-07-05 16:10:13 +0800

    #

--- server/docker/ssl/self-signed/build.sh
+++ server/docker/ssl/self-signed/build.sh
@@ -14,6 +14,6 @@ conf_file={{ param_role_path }}/conf
 openssl_cnf=${conf_file}/openssl.cnf
 openssl_ext=${conf_file}/openssl.ext
 
-openssl genrsa -out ${tmp_generate}/server.key 4096 && openssl req -new -key ${tmp_generate}/server.key -out ${tmp_generate}/server.csr -config ${openssl_cnf} -nodes -subj "/C=CN/ST=SH/L=SH/O=czy/OU=czy/CN=server.cluster.com"
+openssl genrsa -out ${tmp_generate}/server.key 4096 && openssl req -new -key ${tmp_generate}/server.key -out ${tmp_generate}/server.csr -config ${openssl_cnf} -nodes -subj "/C=CN/ST=SH/L=SH/O=Home/OU=IT/CN=server.cluster.com"
 
 openssl ca -in ${tmp_generate}/server.csr -out ${tmp_generate}/server.crt -cert ${tmp_ca}/ca.crt -keyfile ${tmp_ca}/ca.key -config ${openssl_cnf} -extfile ${openssl_ext}
