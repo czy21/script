bc8171e5414e69ca357bbbe391ee532e76fa7211
Bruce CHEN <805899926@qq.com>
2022-07-31 14:37:49 +0800

    #

--- utility/path.py
+++ utility/path.py
@@ -26,4 +26,4 @@ def join_path(first: str, *more) -> str:
     p = pathlib.Path(first)
     for m in more:
         p = p.joinpath(m)
-    return p.absolute().resolve().as_posix()
+    return os.path.abspath(p.as_posix())
