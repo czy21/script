d45a2d1a58b5e59e427bb775ce5157a8769f613d
Bruce CHEN <805899926@qq.com>
2022-08-12 13:53:27 +0800

    #

--- server/init/roles/k8s/templates/containerd-centos.sh
+++ server/init/roles/k8s/templates/containerd-centos.sh
@@ -9,10 +9,6 @@ sudo sed -i.bak -e "s,^baseurl=https://download.docker.com,baseurl=http://{{ par
 sudo yum -y install containerd.io
 sudo systemctl daemon-reload && sudo systemctl restart containerd && systemctl enable containerd
 
-sudo wget -c https://github.com/kubernetes-sigs/cri-tools/releases/download/v{{ param_containerd_version }}/crictl-v{{ param_containerd_version }}-linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local/bin
-sudo chown root:root /usr/local/bin/crictl
-sudo chmod +x /usr/local/bin/crictl
-sudo ln -s /usr/local/bin/crictl /usr/bin/crictl
 sudo echo "
 runtime-endpoint: unix:///run/containerd/containerd.sock
 image-endpoint: unix:///run/containerd/containerd.sock
