0cd4bae143b92ca46b796cba63749b6723507197
Bruce CHEN <805899926@qq.com>
2022-08-12 14:58:07 +0800

    #

--- server/init/roles/k8s/templates/containerd-centos.sh
+++ server/init/roles/k8s/templates/containerd-centos.sh
@@ -6,7 +6,7 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 
 sudo sed -i.bak -e "s,^baseurl=https://download.docker.com,baseurl=http://{{ param_mirror_apt }}/docker-ce,g" /etc/yum.repos.d/docker-ce.repo
 
-sudo yum -y install containerd.io
+sudo yum -y install containerd.io-{{ param_containerd_version }}
 sudo systemctl daemon-reload && sudo systemctl restart containerd && systemctl enable containerd
 
 sudo echo "
