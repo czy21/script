e21d07bc4b2ce47c02e905684d440f3f1e2b2424
Bruce CHEN <805899926@qq.com>
2022-08-13 23:53:13 +0800

    #

---
+++ server/init/roles/k8s/templates/k8s-install-centos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+sudo cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+sudo yum install -y kubelet-{{ param_k8s_version }} kubeadm-{{ param_k8s_version }} kubectl-{{ param_k8s_version }} --disableexcludes=kubernetes
+sudo systemctl enable --now kubelet
