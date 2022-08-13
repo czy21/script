9280933666235435cd191a1a291dc611b5950e9c
Bruce CHEN <805899926@qq.com>
2022-08-14 00:16:26 +0800

    #

--- server/init/roles/docker/templates/centos.sh
+++ server/init/roles/docker/templates/centos.sh
@@ -16,4 +16,4 @@ sudo systemctl enable docker
 # docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/{{ param_compose_version }}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
