62380c98d8d461ca8ce2e7101d3f6fa075ecbe8b
Bruce CHEN <805899926@qq.com>
2022-08-14 22:09:17 +0800

    #

--- server/init/roles/docker/templates/centos.sh
+++ server/init/roles/docker/templates/centos.sh
@@ -6,7 +6,7 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 # override docker repo
-sudo sed -i.bak -e "s,^baseurl=https://download.docker.com,baseurl=http://{{ param_mirror_apt }}/docker-ce,g" /etc/yum.repos.d/docker-ce.repo
+sudo sed -i.bak -e "s,^baseurl=https://download.docker.com,baseurl=http://{{ param_mirror_raw }}/docker-ce,g" /etc/yum.repos.d/docker-ce.repo
 
 sudo yum -y install docker-ce docker-ce-cli containerd.io
 sudo systemctl daemon-reload
