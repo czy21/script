b9a76739e3afcb90cf268bfd0c3ce6b4c5699218
Bruce CHEN <805899926@qq.com>
2022-08-14 12:27:26 +0800

    #

--- server/init/roles/k8s/templates/crio-install-centos.sh
+++ server/init/roles/k8s/templates/crio-install-centos.sh
@@ -4,4 +4,5 @@ set -e
 sudo curl -L -o /etc/yum.repos.d/cri-o-{{ param_k8s_version }}.repo \
 https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/1.24:/{{ param_k8s_version }}/CentOS_8_Stream/devel:kubic:libcontainers:stable:cri-o:1.24:{{ param_k8s_version }}.repo
 sudo yum -y install cri-o
+sudo sed -i.bak -e 's|registry.k8s.io|{{ param_registry_proxy_url }}|g' -e 's|^# \(pause_image = \)|\1|g' /etc/crio/crio.conf
 sudo systemctl daemon-reload && sudo systemctl restart crio && systemctl enable crio
