f157ea67c3fbdc1b726154e4e450b846675e64f6
Bruce CHEN <805899926@qq.com>
2022-08-14 23:23:41 +0800

    #

--- server/init/roles/k8s/templates/k8s-install-centos.sh
+++ server/init/roles/k8s/templates/k8s-install-centos.sh
@@ -11,5 +11,9 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 exclude=kubelet kubeadm kubectl
 EOF
+sudo sed -i \
+-e "s|https://packages.cloud.google.com|http://{{ param_mirror_raw }}/kubernetes|g" \
+-e "s|^gpgcheck=1|gpgcheck=0|g" \
+-e "s|^repo_gpgcheck=1|repo_gpgcheck=0|g" /etc/yum.repos.d/kubernetes.repo
 sudo yum install -y kubelet-{{ param_k8s_version }} kubeadm-{{ param_k8s_version }} kubectl-{{ param_k8s_version }} --disableexcludes=kubernetes
 sudo systemctl enable --now kubelet
