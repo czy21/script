398f82631cd020c87eba75eea6257c989be82fde
Bruce CHEN <805899926@qq.com>
2022-10-08 10:57:20 +0800

    #

--- server/docker/db/rocketmq/conf/setup.sh
+++ server/docker/db/rocketmq/conf/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 for ((i=1;i<={{ param_mq_rocket_cluster_replicas }};i++));do
- node_conf_dir=/data/conf/${i}
- broker_conf=${node_conf_dir}/broker.conf
- mkdir -p ${node_conf_dir}
+ broker_data_dir=/data/${i}
+ broker_conf=${broker_data_dir}/broker.conf
+ mkdir -p ${broker_data_dir}
 cat << EOF > ${broker_conf}
 brokerClusterName = {{ param_mq_rocket_cluster_name }}
 brokerName = rocketmq-broker-${i}
