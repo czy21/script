7425a6454d410dc52a00a74357565c03a69afb4c
Bruce CHEN <805899926@qq.com>
2022-10-11 16:08:45 +0800

    # cleanup

--- test/encrypt_test.py
+++
@@ -1,5 +0,0 @@
-import bcrypt
-
-if __name__ == '__main__':
-    hashed = bcrypt.hashpw(b"hhhhh", bcrypt.gensalt(rounds=12))
-    print(hashed)
--- test/wordTest.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-import numpy as np
-
-f = np.load(Path('c:/Users/zhaoyu.chen/Desktop/developer/mx-project/weights.npz').as_posix())
-print("a")
