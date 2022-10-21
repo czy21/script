9de4bbc397dd9b3a2df2705752dccc24c7223ff5
Bruce CHEN <805899926@qq.com>
2022-10-21 22:11:10 +0800

    #

--- domain/db_meta/pgsql.py
+++ domain/db_meta/pgsql.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
+createTimeColumn = "create_time  timestamp(6) DEFAULT CURRENT_TIMESTAMP"
+createUserColumn = "create_user  varchar(36)  DEFAULT NULL"
+updateTimeColumn = "update_time  timestamp(6) DEFAULT CURRENT_TIMESTAMP"
+updateUserColumn = "update_user  varchar(36)  DEFAULT NULL"
+deletedColumn = "deleted bool NOT NULL DEFAULT 'n'"
+
 self = {
     "header": "SELECT 'executing: {{ file_path }}' AS file;",
     "footer": "SELECT 'executed: {{ file_path }}' AS file;",
     "substitution": {
-        "TrackColumn": "\tcreate_time  timestamp(6) DEFAULT CURRENT_TIMESTAMP,\n"
-                          "\tcreate_user  varchar(36)  DEFAULT NULL,\n"
-                          "\tupdate_time  timestamp(6) DEFAULT CURRENT_TIMESTAMP,\n"
-                          "\tupdate_user  varchar(36)  DEFAULT NULL,\n"
-                          "\tdeleted bool NOT NULL DEFAULT 'n'"
+        "CreateTimeColumn": "\t{0}".format(createTimeColumn),
+        "CreateUserColumn": "\t{0}".format(createUserColumn),
+        "UpdateTimeColumn": "\t{0}".format(updateTimeColumn),
+        "UpdateUserColumn": "\t{0}".format(updateUserColumn),
+        "DeletedColumn": "\t{0}".format(deletedColumn),
+        "TrackColumn": ",\n".join(["\t{0}".format(t) for t in [createTimeColumn, createUserColumn, updateTimeColumn, updateUserColumn, deletedColumn]]),
     }
 }
