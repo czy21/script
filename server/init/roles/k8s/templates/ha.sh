694a50456417d7d1ba6a3870e13050e8897ca6ad
Bruce CHEN <805899926@qq.com>
2022-11-19 22:16:51 +0800

    #

---
+++ server/init/roles/k8s/templates/ha.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+HOSTS=({{ param_hosts | join(' ', attribute='ip') }})
+NAMES=({{ param_hosts | join(' ', attribute='name') }})
+
+for i in "${!HOSTS[@]}"; do
+  HOST=${HOSTS[$i]}
+  NAME=${NAMES[$i]}
+  STATE="BACKUP"
+  if [ ${HOST} == ${HOSTS[0]} ]; then
+    STATE="MASTER"
+  fi
+
+  sudo cat << EOF > /etc/keepalived/keepalived.conf
+! /etc/keepalived/keepalived.conf
+! Configuration File for keepalived
+global_defs {
+    router_id LVS_DEVEL
+}
+vrrp_script check_apiserver {
+  script "/etc/keepalived/check_apiserver.sh"
+  interval 3
+  weight -2
+  fall 10
+  rise 2
+}
+
+vrrp_instance VI_1 {
+    state ${STATE}
+    interface ${INTERFACE}
+    virtual_router_id ${ROUTER_ID}
+    priority ${PRIORITY}
+    authentication {
+        auth_type PASS
+        auth_pass ${AUTH_PASS}
+    }
+    virtual_ipaddress {
+        ${APISERVER_VIP}
+    }
+    track_script {
+        check_apiserver
+    }
+}
+EOF
+done
