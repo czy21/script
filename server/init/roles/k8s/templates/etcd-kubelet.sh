9389d91b4f9811772c3bc54a871201b4bd321f6b
Bruce CHEN <805899926@qq.com>
2022-11-19 19:46:26 +0800

    #

--- server/init/roles/k8s/templates/etcd-kubelet.sh
+++ server/init/roles/k8s/templates/etcd-kubelet.sh
@@ -13,57 +13,53 @@ CLUSTER="{{ initial_nodes | join(',') }}"
 DIR=${HOME}/{{ param_remote_role_path }}/{{ param_k8s_etcd_cluster_name }}
 PKI=${DIR}/pki
 mkdir -p ${PKI}
-sudo kubeadm init phase certs etcd-ca --kubernetes-version {{ param_k8s_version }}
+kubeadm init phase certs etcd-ca --kubernetes-version {{ param_k8s_version }} --cert-dir ${PKI}
 
 for i in "${!HOSTS[@]}"; do
   HOST=${HOSTS[$i]}
   NAME=${NAMES[$i]}
   HOST_DIR=${DIR}/${HOST}
-  KUBEADM_CFG=${HOST_DIR}/kubeadm-config.yaml
+  KUBE_CFG=${HOST_DIR}/kubeadm-config.yaml
   mkdir -p ${HOST_DIR}
-  cat << EOF > ${KUBEADM_CFG}
+  cat << EOF > ${KUBE_CFG}
 ---
 apiVersion: "kubeadm.k8s.io/v1beta3"
 kind: InitConfiguration
 nodeRegistration:
-    name: ${NAME}
-    criSocket: "{{ param_cri_socket }}"
+  name: ${NAME}
+  criSocket: "{{ param_cri_socket }}"
 localAPIEndpoint:
-    advertiseAddress: ${HOST}
+  advertiseAddress: ${HOST}
 ---
 apiVersion: "kubeadm.k8s.io/v1beta3"
 kind: ClusterConfiguration
 kubernetesVersion: "v{{ param_k8s_version }}"
 imageRepository: "{{ param_registry_proxy_url }}"
+certificatesDir: "${PKI}"
 etcd:
-    local:
-        serverCertSANs:
-        - "${HOST}"
-        peerCertSANs:
-        - "${HOST}"
-        extraArgs:
-            initial-cluster: {{ initial_nodes | join(',') }}
-            initial-cluster-state: new
-            name: ${NAME}
-            listen-peer-urls: https://${HOST}:2380
-            listen-client-urls: https://${HOST}:2379
-            advertise-client-urls: https://${HOST}:2379
-            initial-advertise-peer-urls: https://${HOST}:2380
+  local:
+    serverCertSANs:
+      - "${HOST}"
+    peerCertSANs:
+      - "${HOST}"
+    extraArgs:
+      initial-cluster: {{ initial_nodes | join(',') }}
+      initial-cluster-state: new
+      name: ${NAME}
+      listen-peer-urls: https://${HOST}:2380
+      listen-client-urls: https://${HOST}:2379
+      advertise-client-urls: https://${HOST}:2379
+      initial-advertise-peer-urls: https://${HOST}:2380
 EOF
-   sudo kubeadm init phase certs etcd-server --config=${KUBEADM_CFG}
-   sudo kubeadm init phase certs etcd-peer --config=${KUBEADM_CFG}
-   sudo kubeadm init phase certs etcd-healthcheck-client --config=${KUBEADM_CFG}
-   sudo kubeadm init phase certs apiserver-etcd-client --config=${KUBEADM_CFG}
-   sudo cp -R /etc/kubernetes/pki ${HOST_DIR}
-   sudo find /etc/kubernetes/pki -not -name ca.crt -not -name ca.key -type f -delete
-   if [ $i == 0 ]; then
-     sudo cp -R ${HOST_DIR}/pki ${DIR}
-   else
-     sudo find ${HOST_DIR}/pki -name ca.key -type f -delete
+   kubeadm init phase certs etcd-server --config=${KUBE_CFG}
+   kubeadm init phase certs etcd-peer --config=${KUBE_CFG}
+   kubeadm init phase certs etcd-healthcheck-client --config=${KUBE_CFG}
+   kubeadm init phase certs apiserver-etcd-client --config=${KUBE_CFG}
+   find ${DIR} -name kubeadm-config.yaml -type f -exec sed -i '/certificatesDir/d' {} \;
+   cp -R ${PKI} ${HOST_DIR}
+   find ${PKI} -not -name ca.crt -not -name ca.key -type f -delete
+   if [ ${HOST} != ${HOSTS[0]} ]; then
+     find ${HOST_DIR}/pki -name ca.key -type f -delete
    fi
-   uid=$(id -u)
-   sudo chown -R ${uid}:${uid} ${DIR}
 done
-
-sh -c 'cd {{ param_remote_role_path }} && tar -pzcvf {{ param_k8s_etcd_cluster_name }}.tar.gz {{ param_k8s_etcd_cluster_name }}'
-rm -rf ${DIR}
+cp -R ${DIR}/${HOSTS[0]}/pki ${DIR}
