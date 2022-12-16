66a530cdb7ca618f752898f08b75c87d09e9852c
Bruce CHEN <805899926@qq.com>
2022-12-16 10:34:13 +0800

    #

--- server/share.py
+++ server/share.py
@@ -58,7 +58,7 @@ def split_kv_str(kv_str) -> (str, str):
 def get_match_dirs(rules, items):
     _dirs = []
     for p in items:
-        _rules = regex_util.match_rules(rules, p.as_posix(), ".jinia2ignore {0}".format(dfs_dir.__name__))
+        _rules = regex_util.match_rules(rules, p.as_posix(), ".jinja2ignore {0}".format(dfs_dir.__name__))
         if not any(_rules.values()):
             _dirs.append(p)
     return _dirs
@@ -162,7 +162,7 @@ def loop_namespaces(root_path: pathlib.Path,
                 file_util.write_text(role_env_output_file, yaml.dump(role_env))
             # write jinja2 template
             for t in filter(lambda f: f.is_file(), role_output_path.rglob("*")):
-                _rules = regex_util.match_rules([*jinja2ignore_rules, role_output_path.joinpath("env.yaml").as_posix()], t.as_posix(), ".jinia2ignore {0}".format(loop_namespaces.__name__))
+                _rules = regex_util.match_rules([*jinja2ignore_rules, role_output_path.joinpath("env.yaml").as_posix()], t.as_posix(), ".jinja2ignore {0}".format(loop_namespaces.__name__))
                 if not any(_rules.values()):
                     file_util.write_text(t, template_util.Template(file_util.read_text(t)).render(**role_env))
 
