8918cc10e15b02d658d80df4b03191ed40117bcc
Bruce CHEN <805899926@qq.com>
2022-12-22 23:10:01 +0800

    #

--- jenkins/src/org/ops/Docker.groovy
+++ jenkins/src/org/ops/Docker.groovy
@@ -8,21 +8,21 @@ import org.ops.util.StringUtils
 def build() {
     env.param_project_context = PathUtils.ofPath(env.param_project_root, env.param_project_module)
     env.param_release_version = StringUtils.isNotEmpty(env.param_release_version)
-                              ? env.param_release_version
-                              : params.param_branch
+            ? env.param_release_version
+            : params.param_branch
     env.param_release_name = PathUtils.ofPath(
             env.param_registry_repo,
             env.param_registry_dir,
-            StringUtils.isEmpty(env.param_release_name)
-                    ? StringUtils.join("-", env.param_project_name, env.param_project_module)
-                    : env.param_release_name
+            StringUtils.isNotEmpty(env.param_release_name)
+                    ? env.param_release_name
+                    : StringUtils.join("-", env.param_project_name, env.param_project_module)
     )
-    env.param_docker_context = StringUtils.isNull(env.param_docker_context)
-                            ? env.param_project_context
-                            : PathUtils.ofPath(env.param_project_root, env.param_docker_context)
+    env.param_docker_context = StringUtils.isNotNull(env.param_docker_context)
+            ? PathUtils.ofPath(env.param_project_root, env.param_docker_context)
+            : env.param_project_context
     env.param_docker_file = PathUtils.ofPath(env.param_docker_context, "Dockerfile")
 
-    def tool = [
+    def toolMap = [
             java: {
                 env.JAVA_HOME = "${tool 'jdk-17'}"
                 env.PATH = "${JAVA_HOME}/bin:${PATH}"
@@ -41,9 +41,9 @@ def build() {
                 env.PATH = "${NODEJS_HOME}/bin:${PATH}"
             }
     ]
-    def cmd = [
+    def cmdMap = [
             java : {
-                tool.get("java").call()
+                toolMap.get("java").call()
                 return StringUtils.format(
                         "chmod +x {0}/gradlew && {0}/gradlew --no-daemon --gradle-user-home {1} --init-script {2} --build-file {0}/build.gradle {3} -x test --refresh-dependencies",
                         env.param_project_root,
@@ -53,14 +53,14 @@ def build() {
                 )
             },
             go   : {
-                tool.get("go").call()
+                toolMap.get("go").call()
                 return StringUtils.format(
                         "cd {0};go build -o build main.go;",
                         env.param_project_context
                 )
             },
             web  : {
-                tool.get("web").call()
+                toolMap.get("web").call()
                 yarn_cmd = StringUtils.format(
                         "yarn --cwd {0} --registry {1} --cache-folder {2}",
                         env.param_project_context,
@@ -71,12 +71,12 @@ def build() {
             },
             shell: {
                 if (StringUtils.isNotEmpty(env.param_tools)) {
-                    env.param_tools.split(",").each { tool.get(it).call() }
+                    env.param_tools.split(",").each { toolMap.get(it).call() }
                 }
                 return StringUtils.format("chmod +x {0};{0}", PathUtils.ofPath(env.param_project_root, env.param_project_shell_file))
             }
     ]
-    build_cmd = cmd.get(env.param_code_type).call()
+    build_cmd = cmdMap.get(env.param_code_type).call()
     new Common().writeParamToYaml()
     sh "${build_cmd}"
     configFileProvider([configFile(fileId: "docker-config", targetLocation: '.jenkins/docker/config.json')]) {
--- jenkins/src/org/ops/Kubernetes.groovy
+++ jenkins/src/org/ops/Kubernetes.groovy
@@ -5,34 +5,34 @@ package org.ops
 import org.ops.util.StringUtils
 
 def deploy() {
-    def release = [
-      java: {
-        env.param_release_chart_name = env.param_helm_java_chart_name
-        env.param_release_chart_version = env.param_helm_java_chart_version
-      },
-      go: {
-        env.param_release_chart_name = env.param_helm_go_chart_name
-        env.param_release_chart_version = env.param_helm_go_chart_version
-      },
-      py: {
-        env.param_release_chart_name = env.param_helm_python_chart_name
-        env.param_release_chart_version = env.param_helm_python_chart_version
-      },
-      web: {
-        env.param_release_chart_name = env.param_helm_web_chart_name
-        env.param_release_chart_version = env.param_helm_web_chart_version
-      }
+    def chartMap = [
+            java: {
+                env.param_release_chart_name = env.param_helm_java_chart_name
+                env.param_release_chart_version = env.param_helm_java_chart_version
+            },
+            go  : {
+                env.param_release_chart_name = env.param_helm_go_chart_name
+                env.param_release_chart_version = env.param_helm_go_chart_version
+            },
+            py  : {
+                env.param_release_chart_name = env.param_helm_python_chart_name
+                env.param_release_chart_version = env.param_helm_python_chart_version
+            },
+            web : {
+                env.param_release_chart_name = env.param_helm_web_chart_name
+                env.param_release_chart_version = env.param_helm_web_chart_version
+            }
     ]
-    release.get(env.param_code_type).call()
+    chartMap.get(env.param_code_type).call()
     new Common().writeParamToYaml()
     withKubeConfig([credentialsId: env.param_kube_credential, serverUrl: env.param_kube_server]) {
         helm_cmd = StringUtils.format(
-            "helm upgrade --install {0} {1} --version {2} --namespace {3} --repo {4} --values .jenkins/param.yaml --output yaml",
-            env.param_release_name,
-            env.param_release_chart_name,
-            env.param_release_chart_version,
-            env.param_release_namespace,
-            env.param_helm_repo
+                "helm upgrade --install {0} {1} --version {2} --namespace {3} --repo {4} --values .jenkins/param.yaml --output yaml",
+                env.param_release_name,
+                env.param_release_chart_name,
+                env.param_release_chart_version,
+                env.param_release_namespace,
+                env.param_helm_repo
         )
         sh "${helm_cmd}"
     }
