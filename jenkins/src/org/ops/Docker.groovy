b1e2710ddbc5effd37aae060e4d46c8bf3f6b9ae
Bruce CHEN <805899926@qq.com>
2023-02-03 15:15:20 +0800

    #

--- jenkins/src/org/ops/Docker.groovy
+++ jenkins/src/org/ops/Docker.groovy
@@ -79,14 +79,12 @@ def build() {
     build_cmd = cmdMap.get(env.param_code_type).call()
     new Common().writeParamToYaml()
     env.DOCKER_HOME = "${tool 'docker'}"
-    env.PATH = "${DOCKER_HOME}/bin:${PATH}"
     configFileProvider([
         configFile(fileId: "init.gradle", targetLocation: '.jenkins/init.gradle'),
         configFile(fileId: "docker-config", targetLocation: '.jenkins/docker/config.json')
     ]) {
-        docker_build_cmd = "docker build --tag ${env.param_release_name}:${env.param_release_version} --file ${env.param_docker_file} ${env.param_docker_context}"
-        docker_push_cmd  = StringUtils.format("docker --config {0} push ${env.param_release_name}:${env.param_release_version}",
-                                              PathUtils.ofPath("${env.WORKSPACE}", ".jenkins/docker/")
+        docker_build_cmd = "sudo ${DOCKER_HOME}/bin/docker build --tag ${env.param_release_name}:${env.param_release_version} --file ${env.param_docker_file} ${env.param_docker_context}"
+        docker_push_cmd  = StringUtils.format("sudo ${DOCKER_HOME}/bin/docker --config {0} push ${env.param_release_name}:${env.param_release_version}", PathUtils.ofPath("${env.WORKSPACE}", ".jenkins/docker/")
         )
         sh "${build_cmd} && ${docker_build_cmd} && ${docker_push_cmd}"
     }
