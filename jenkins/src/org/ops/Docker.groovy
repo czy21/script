ce6494fbcad7b9b1fb503eca42619d0cd5a09e52
Bruce CHEN <805899926@qq.com>
2023-03-14 21:19:22 +0800

    #

--- jenkins/src/org/ops/Docker.groovy
+++ jenkins/src/org/ops/Docker.groovy
@@ -40,7 +40,7 @@ def build() {
             dotnet: {
                 sdkMap.get("dotnet").call()
                 return StringUtils.format(
-                        "rm -rf {0}/build && dotnet publish --configfile {1} -c Release -p:PublishSingleFile=true --self-contained false {0} -o {0}/build",
+                        "rm -rf {0}/build && dotnet publish --configfile {1} -c Release -p:AssemblyName=api,PublishSingleFile=true --self-contained false {0} -o {0}/build",
                         env.param_project_root,
                         PathUtils.ofPath("${env.WORKSPACE}", ".jenkins/nuget.config")
                 )
