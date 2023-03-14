d6ec15f468e1a391b422fb285b48a93628888f4c
Bruce CHEN <805899926@qq.com>
2023-03-14 13:33:50 +0800

    #

--- jenkins/src/org/ops/Common.groovy
+++ jenkins/src/org/ops/Common.groovy
@@ -8,4 +8,30 @@ def writeParamToYaml() {
     writeYaml file: '.jenkins/param.yaml', data: CollectionUtils.sortMapByKey(param as Map<String, Object>), charset: 'UTF-8', overwrite: true
 }
 
+def getSDKMap() {
+    def sdkMap = [
+            java  : {
+                env.JAVA_HOME = "${tool 'jdk-17'}"
+                env.PATH = "${JAVA_HOME}/bin:${PATH}"
+            },
+            go    : {
+                env.GO_HOME = "${tool 'go-v1.20'}"
+                env.GOPROXY = env.param_go_proxy
+                env.GOSUMDB = "off"
+                env.CGO_ENABLED = "0"
+                env.PATH = "${GO_HOME}/bin:${PATH}"
+            },
+            web   : {
+                env.NODEJS_HOME = "${tool 'node-v18.14.0'}"
+                env.PATH = "${NODEJS_HOME}/bin:${PATH}"
+            },
+            dotnet: {
+                env.DOTNET_HOME = "${tool 'net7.0-linux-64'}"
+                env.DOTNET_SYSTEM_GLOBALIZATION_INVARIANT = 1
+                env.PATH = "${DOTNET_HOME}:${PATH}"
+            }
+    ]
+    return sdkMap;
+}
+
 return this
--- jenkins/src/org/ops/Docker.groovy
+++ jenkins/src/org/ops/Docker.groovy
@@ -6,6 +6,7 @@ import org.ops.util.PathUtils
 import org.ops.util.StringUtils
 
 def build() {
+    def common = new Common()
     env.param_project_context = PathUtils.ofPath(env.param_project_root, env.param_project_module)
     env.param_docker_context = StringUtils.isNotNull(env.param_docker_context) ? PathUtils.ofPath(env.param_project_root, env.param_docker_context) : env.param_project_context
     env.param_docker_file = PathUtils.ofPath(env.param_docker_context, "Dockerfile")
@@ -15,32 +16,10 @@ def build() {
             StringUtils.defaultIfEmpty(env.param_release_name, StringUtils.join("-", env.param_project_name, env.param_project_module))
     )
     env.param_release_version = StringUtils.defaultIfEmpty(env.param_release_version, params.param_branch)
-
-    def toolMap = [
-            java  : {
-                env.JAVA_HOME = "${tool 'jdk-17'}"
-                env.PATH = "${JAVA_HOME}/bin:${PATH}"
-            },
-            go    : {
-                env.GO_HOME = "${tool 'go-v1.20'}"
-                env.GOPROXY = env.param_go_proxy
-                env.GOSUMDB = "off"
-                env.CGO_ENABLED = "0"
-                env.PATH = "${GO_HOME}/bin:${PATH}"
-            },
-            web   : {
-                env.NODEJS_HOME = "${tool 'node-v18.14.0'}"
-                env.PATH = "${NODEJS_HOME}/bin:${PATH}"
-            },
-            dotnet: {
-                env.DOTNET_HOME = "${tool 'net7.0-linux-64'}"
-                env.DOTNET_SYSTEM_GLOBALIZATION_INVARIANT = 1
-                env.PATH = "${DOTNET_HOME}:${PATH}"
-            }
-    ]
+    def sdkMap = common.getSDKMap()
     def cmdMap = [
             java  : {
-                toolMap.get("java").call()
+                sdkMap.get("java").call()
                 return StringUtils.format(
                         "chmod +x {0}/gradlew && {0}/gradlew --init-script {2} --build-file {0}/build.gradle {3} -x test --refresh-dependencies",
                         env.param_project_root,
@@ -50,16 +29,16 @@ def build() {
                 )
             },
             go    : {
-                toolMap.get("go").call()
+                sdkMap.get("go").call()
                 return StringUtils.format("cd {0};go build -o build main.go;", env.param_project_context)
             },
             web   : {
-                toolMap.get("web").call()
+                sdkMap.get("web").call()
                 yarn_cmd = StringUtils.format("yarn --cwd {0} --registry {1}", env.param_project_context, env.param_npm_repo)
                 return StringUtils.format("{0} install --no-lockfile --update-checksums && {0} --ignore-engines build", yarn_cmd)
             },
             dotnet: {
-                toolMap.get("dotnet").call()
+                sdkMap.get("dotnet").call()
                 return StringUtils.format(
                         "rm -rf {0}/build && dotnet publish -c Release -p:AssemblyName=api,PublishSingleFile=true --self-contained false {0} -o {0}/build",
                         env.param_project_root
@@ -67,13 +46,13 @@ def build() {
             },
             shell : {
                 if (StringUtils.isNotEmpty(env.param_tools)) {
-                    env.param_tools.split(",").each { toolMap.get(it).call() }
+                    env.param_tools.split(",").each { sdkMap.get(it).call() }
                 }
                 return StringUtils.format("chmod +x {0};{0}", PathUtils.ofPath(env.param_project_root, env.param_project_shell_file))
             }
     ]
     build_cmd = cmdMap.get(env.param_code_type).call()
-    new Common().writeParamToYaml()
+    common.writeParamToYaml()
     env.DOCKER_HOME = "${tool 'docker'}"
     configFileProvider([
             configFile(fileId: "init.gradle", targetLocation: '.jenkins/init.gradle'),
