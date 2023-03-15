9debcab41d01aa054e7b34b0d77fd31ea3f2c44b
Bruce CHEN <805899926@qq.com>
2023-03-15 12:54:37 +0800

    #

--- jenkins/src/org/ops/Common.groovy
+++ jenkins/src/org/ops/Common.groovy
@@ -8,12 +8,10 @@ def writeParamToYaml() {
     Map<String, Object> param = readProperties text: sh(script: 'env | grep \'^param_\'', returnStdout: true).trim()
     Map<String, Object> param1 = new HashMap<>();
     param.each { it ->
-        {
-            if (StringUtils.isEmpty(it.getValue() as String)) {
-                param1.put(it.getKey(), null)
-            }
-            param1.put(it.getKey(), it.getValue())
+        if (StringUtils.isEmpty(it.getValue() as String)) {
+            param1.put(it.getKey(), null)
         }
+        param1.put(it.getKey(), it.getValue())
     }
     writeYaml file: '.jenkins/param.yaml', data: CollectionUtils.sortMapByKey(param), charset: 'UTF-8', overwrite: true
 }
