c24fd0bd39674216d9ddfbecd6a9ce640cccec4f
Bruce CHEN <805899926@qq.com>
2023-03-15 11:22:51 +0800

    #

--- jenkins/src/org/ops/Docker.groovy
+++ jenkins/src/org/ops/Docker.groovy
@@ -75,8 +75,8 @@ def build() {
     ]
     env.DOCKER_HOME = "${tool 'docker'}"
     configFileProvider([
-            configFile(fileId: "init.gradle",  variable: 'CONFIG_FILE_GRADLE'),
-            configFile(fileId: "nuget.config", variable: 'CONFIG_FILE_NUGET'),
+            configFile(fileId: "gradle.config",  variable: 'CONFIG_FILE_GRADLE'),
+            configFile(fileId: "nuget.config",   variable: 'CONFIG_FILE_NUGET'),
             configFile(fileId: "docker.config", targetLocation: '.jenkins/docker/config.json')
     ]) {
         build_cmd = cmdMap.get(env.param_code_type).call()
