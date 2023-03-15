8acbdb06b8fe065e1df6150f54eca51d024f9481
Bruce CHEN <805899926@qq.com>
2023-03-15 13:29:02 +0800

    #

--- jenkins/src/org/ops/Common.groovy
+++ jenkins/src/org/ops/Common.groovy
@@ -6,9 +6,9 @@ import org.ops.util.StringUtils
 
 def writeParamToYaml() {
     Map<String, Object> param = readProperties text: sh(script: 'env | grep \'^param_\'', returnStdout: true).trim()
-    param.each {
-        it.value = StringUtils.isNotNull(it.getValue() as String) ? it.value : null
-        return it
+    param.each { it ->
+        Object value = StringUtils.isNotNull(it.getValue() as String) ? it.getValue() : null
+        return Map.entry(it.getKey(), value)
     }
     param = CollectionUtils.sortMapByKey(param)
     writeYaml file: '.jenkins/param.yaml', data: param, charset: 'UTF-8', overwrite: true
