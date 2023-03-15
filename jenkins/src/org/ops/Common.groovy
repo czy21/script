53e96c1680026fa6a0df6e087d3370d0a54ae2e5
Bruce CHEN <805899926@qq.com>
2023-03-15 13:25:38 +0800

    #

--- jenkins/src/org/ops/Common.groovy
+++ jenkins/src/org/ops/Common.groovy
@@ -6,11 +6,9 @@ import org.ops.util.StringUtils
 
 def writeParamToYaml() {
     Map<String, Object> param = readProperties text: sh(script: 'env | grep \'^param_\'', returnStdout: true).trim()
-    param.each { k, v ->
-        if (v in String && StringUtils.isNull(v as String)) {
-            param.put(k, null)
-        }
-        return
+    param.each {
+        it.value = StringUtils.isNotNull(it.getValue() as String) ? it.value : null
+        return it
     }
     param = CollectionUtils.sortMapByKey(param)
     writeYaml file: '.jenkins/param.yaml', data: param, charset: 'UTF-8', overwrite: true
--- jenkins/test/org/ops/Main.groovy
+++ jenkins/test/org/ops/Main.groovy
@@ -1,8 +1,18 @@
 static void main(String[] args) {
-    def a = [
-            java: {
-                
-            }
-    ]
-    a.get("java").call()
+//    def a = [
+//            java: {
+//
+//            }
+//    ]
+//    a.get("java").call()
+
+    Map<String, Object> a = new HashMap<>();
+    a.put("a", "111");
+    a.put("b", "null")
+    a.put("c", "null")
+    def b = a.findAll { k, v -> v in String && v == "null" }.each {
+        it.value = null
+        return it
+    }
+    println "aa"
 }
