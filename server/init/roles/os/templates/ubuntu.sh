8f62639a92ff4d904f6027633d85643af7cfe8a5
Bruce CHEN <805899926@qq.com>
2023-03-27 17:54:27 +0800

    #

--- server/init/roles/os/templates/ubuntu.sh
+++ server/init/roles/os/templates/ubuntu.sh
@@ -6,7 +6,7 @@ echo -n "%sudo   ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/99-custom
 NEEDRESTART_SUSPEND=1 apt remove needrestart -y && apt autoclean -y && apt autoremove -y
 
 apt -y update
-apt -y install wget vim git network-manager nfs-common cifs-utils make gcc iputils-ping bash-completion systemd-timesyncd rsync netcat
+apt -y install wget vim git nfs-common cifs-utils make gcc iputils-ping bash-completion systemd-timesyncd rsync netcat
 
 # postgres repo
 #sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
@@ -15,7 +15,7 @@ apt -y install wget vim git network-manager nfs-common cifs-utils make gcc iputi
 # install optional
 #apt -y install mysql-client postgresql-client
 
-public_key="set -e;cd;mkdir -p .ssh;chmod 700 .ssh;echo {{ param_ssh_public_key }} > .ssh/authorized_keys;chmod 644 .ssh/authorized_keys"
+public_key="set -e;cd;mkdir -p .ssh;chmod 700 .ssh;echo {{ param_user_ops_opsor_ssh_public_key }} > .ssh/authorized_keys;chmod 644 .ssh/authorized_keys"
 sudo -u {{ param_user_ops }} bash -c "${public_key}"
 
 swapoff -a
