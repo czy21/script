6f7d43ef47932fb5eab34278147ac03b177fc2af
Bruce CHEN <805899926@qq.com>
2023-05-11 21:23:34 +0800

    #

--- jenkins/src/org/ops/Docker.groovy
+++ jenkins/src/org/ops/Docker.groovy
@@ -10,7 +10,7 @@ def build() {
         docker_image_tag = "${env.param_release_name}:${env.param_release_version}"
         docker_build_cmd = "sudo ${docker_cli} build --tag ${docker_image_tag} --file ${env.param_docker_file} ${env.param_docker_context} --pull"
         docker_push_cmd = "sudo ${docker_cli} --config ${docker_config_dir} push ${docker_image_tag}"
-        sh "${docker_build_cmd} && ${docker_push_cmd}"
+        sh "${docker_build_cmd} && ${docker_push_cmd} && sudo docker-compose ls"
     }
 }
 
