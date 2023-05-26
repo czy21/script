13b4d9272b868535b98b92db265ddc234f41309d
Bruce CHEN <805899926@qq.com>
2023-05-26 17:10:05 +0800

    #

--- server/init/roles/os/templates/centos.sh
+++ server/init/roles/os/templates/centos.sh
@@ -5,10 +5,10 @@ echo -n "%wheel  ALL=(ALL)       ALL" > /etc/sudoers.d/99-custom
 
 yum clean all && yum --refresh makecache -v
 
-yum -y install wget vim nfs-utils bash-completion git jq rsync nc
+yum -y install wget vim nfs-utils bash-completion git jq rsync nc net-tools
 dnf -y install python39
 
-useradd -m {{ param_user_ops }} && usermod -aG wheel {{ param_user_ops }} && passwd -d {{ param_user_ops }}
+useradd -m {{ param_user_ops }} && usermod -aG wheel {{ param_user_ops }} && passwd -d {{ param_user_ops }} && chown {{ param_user_ops }}:{{ param_user_ops }} /home/{{ param_user_ops }}
 
 public_key="set -e;cd;mkdir -p .ssh;chmod 700 .ssh;echo {{ param_user_ops_opsor_ssh_public_key }} > .ssh/authorized_keys;chmod 644 .ssh/authorized_keys"
 sudo -u root bash -c "${public_key}"
