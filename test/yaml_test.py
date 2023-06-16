d61b2a0a0c19b4da891c865b94ac0f7cd8abc04f
Bruce CHEN <805899926@qq.com>
2023-06-16 13:50:39 +0800

    #

--- test/yaml_test.py
+++ test/yaml_test.py
@@ -4,10 +4,9 @@ import typing
 
 import jinja2
 import yaml
-from utility import collection as collection_util
+from utility import yaml as yaml_util
 
 if __name__ == '__main__':
     with open(pathlib.Path(__file__).parent.joinpath("___temp/a.yaml"), encoding="utf-8") as f:
         a = yaml.full_load(f)
-        b = collection_util.dict_render(a, key_filter_func=lambda t: t.__contains__("p5"), val_filter_func=lambda t: t.__contains__("p2"))
-        print(b)
+        yaml_util.process(a)
