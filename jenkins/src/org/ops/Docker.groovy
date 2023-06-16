6786c7b117e0961e5355e9ff79d04f2105cb1291
Bruce CHEN <805899926@qq.com>
2023-06-16 08:59:35 +0800

    # add param_release_image

--- jenkins/src/org/ops/Docker.groovy
+++ jenkins/src/org/ops/Docker.groovy
@@ -7,7 +7,7 @@ def build() {
         env.DOCKER_HOME = "${tool 'docker'}"
         docker_cli = "${DOCKER_HOME}/bin/docker"
         docker_config_dir = PathUtils.ofPath("${env.WORKSPACE}", ".jenkins/docker/")
-        docker_image_tag = "${env.param_release_name}:${env.param_release_version}"
+        docker_image_tag = "${env.param_release_image}:${env.param_release_version}"
         docker_build_cmd = "sudo ${docker_cli} build --tag ${docker_image_tag} --file ${env.param_docker_file} ${env.param_docker_context} --pull"
         docker_push_cmd = "sudo ${docker_cli} --config ${docker_config_dir} push ${docker_image_tag}"
         sh "${docker_build_cmd} && ${docker_push_cmd}"
