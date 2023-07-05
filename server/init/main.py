855512eb585540e646831321880ae4de0cfcfbbe
Bruce CHEN <805899926@qq.com>
2023-07-05 09:46:24 +0800

    #

--- server/init/main.py
+++ server/init/main.py
@@ -17,36 +17,41 @@ from utility import (
 logger = logging.getLogger()
 
 if __name__ == '__main__':
+    pwd = pathlib.Path(__file__).parent
+    build_path = pwd.joinpath("build")
+    build_path.mkdir(exist_ok=True)
+    tmp_path = pwd.joinpath("___temp")
+    tmp_path.mkdir(exist_ok=True)
     log_util.init_logger()
-    private_key = pathlib.Path(__file__).parent.joinpath("___temp/private-key")
+    private_key = tmp_path.joinpath("private-key")
     if not private_key.exists():
         logger.error("ssh private-key not exists")
         sys.exit(0)
     private_key = private_key.as_posix()
-    ansible_hosts = pathlib.Path(__file__).parent.joinpath("ansible-hosts").as_posix()
+    ansible_hosts = pwd.joinpath("ansible-hosts").as_posix()
     parser = argparse.ArgumentParser(formatter_class=share.CustomHelpFormatter, conflict_handler="resolve")
     share.Installer.set_common_argument(parser)
-    parser.add_argument('--env-file', required=False, default="env.yaml", type=str, help="env file xxx.yaml")
+    parser.add_argument('--env-file', required=False, default="env.yaml", type=str, help="env file (default=[env.yaml])")
     parser.add_argument('-f', '--file', required=True, type=str, help="inventory file")
     parser.add_argument('-t', '--tag', required=True, type=str, help="t1,t2")
-    parser.add_argument('-u', '--user', required=False, type=str, help="connect as this user (default=[param_user_ops])")
     parser.add_argument('-k', '--ask-pass', action="store_true", help="ask for connection password")
+    parser.add_argument('-u', '--user', required=False, type=str, help="connect as this user (default=[param_user_ops])")
     parser.add_argument('--dry-run', action="store_true", help="don't make any changes")
     parser.add_argument('--no-step', action="store_true", help="disable one-step-at-a-time")
     args = parser.parse_args()
-    env_file: pathlib.Path = pathlib.Path(__file__).parent.joinpath(args.env_file)
+    env_file: pathlib.Path = pwd.joinpath(args.env_file)
     if not env_file.exists():
         logger.error("env file not exists")
         sys.exit(0)
     env_dict = yaml_util.load(file_util.read_text(env_file))
-    file_util.write_text(pathlib.Path(__file__).parent.joinpath("vars/env.yml"), yaml.dump(env_dict))
+    file_util.write_text(pwd.joinpath("vars/env.yml"), yaml.dump(env_dict))
     if not args.user:
         args.user = env_dict["param_user_ops"]
     if args.debug:
         logger.setLevel(logging.DEBUG)
 
-    ansible_inventory_file = pathlib.Path(__file__).parent.joinpath("{0}.yml".format(args.file)).as_posix()
-    ansible_log_file = pathlib.Path(__file__).parent.joinpath("___temp/ansible.log").as_posix()
+    ansible_inventory_file = pwd.joinpath("{0}.yml".format(args.file)).as_posix()
+    ansible_log_file = pwd.joinpath("build/ansible.log").as_posix()
     _cmds = ["chmod 600 {0}".format(private_key)]
     ansible_playbook_cmd = [
         "ANSIBLE_SUDO_PASS=0",
@@ -74,6 +79,5 @@ if __name__ == '__main__':
         ansible_playbook_cmd.append("-vv")
     if args.dry_run:
         ansible_playbook_cmd.append("--check")
-    _cmds.append("echo -n '' > {0}".format(ansible_log_file))
     _cmds.append(collection_util.flat_to_str(ansible_playbook_cmd))
     share.execute(collection_util.flat_to_str(_cmds, delimiter=" && "), is_return=False, dry_run=args.dry_run)
