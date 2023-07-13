92637808f1908d2ffa1f08bfcd6830bfa0523724
Bruce CHEN <805899926@qq.com>
2023-07-13 18:54:33 +0800

    #

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ logger = logging.getLogger()
 if __name__ == '__main__':
     logger.setLevel(logging.DEBUG)
     cwd = pathlib.Path(__file__).cwd()
-    doc_public = cwd.joinpath("doc/public")
+    doc = cwd.joinpath("doc")
+    doc_public = doc.joinpath("public")
     mkdocs = cwd.joinpath("mkdocs.yaml")
     mkdocs_template = cwd.joinpath("mkdocs_template.yaml")
     docker_deploys = cwd.joinpath("server/docker/build")
     docker_md = doc_public.joinpath("docker")
-    docker_md_template = doc_public.joinpath("docker-template.md")
+    docker_md_template = doc.joinpath("docker-template.md")
     docker_mds = []
     docker_md_ignore = ["ssl/", "test/"]
     docker_md_ignore.extend(["{0}/build/output".format(t) for t in ["script"]])
-    for t in filter(lambda f: f.is_file() and not any(regex_util.match_rules(docker_md_ignore, f.as_posix()).values()), docker_deploys.rglob("**/output/deploy.yml")):
+    for t in filter(lambda f: f.is_file() and not any(regex_util.match_rules(docker_md_ignore, f.as_posix()).values()),
+                    docker_deploys.rglob("**/output/deploy.yml")):
         name = t.parent.parent.parent.stem
         content = file_util.read_text(t)
         md_text = template_util.Template(file_util.read_text(docker_md_template)).render(**{"param_docker_compose_content": content})
