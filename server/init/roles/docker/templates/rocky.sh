cc393b305b67857540d440380add595361d0518c
Bruce CHEN <805899926@qq.com>
2023-07-19 11:30:22 +0800

    #

---
+++ server/init/roles/docker/templates/rocky.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# docker
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# override docker repo available mirror:https://mirrors.tuna.tsinghua.edu.cn
+sudo sed -i.bak -e "s|https://download.docker.com|http://{{ param_mirror_raw }}/docker-ce|g" /etc/yum.repos.d/docker-ce.repo
+
+sudo yum -y install docker-ce docker-ce-cli containerd.io
+sudo systemctl daemon-reload && sudo systemctl restart docker && sudo systemctl enable docker
