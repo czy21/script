53d91a5777cf5204b38de458b2b0dc2f7b9d0d26
Bruce CHEN <805899926@qq.com>
2023-07-19 14:42:03 +0800

    # lock docker version

--- server/init/roles/docker/templates/rocky.sh
+++ server/init/roles/docker/templates/rocky.sh
@@ -8,5 +8,5 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 # override docker repo available mirror:https://mirrors.tuna.tsinghua.edu.cn
 sudo sed -i.bak -e "s|https://download.docker.com|http://{{ param_mirror_raw }}/docker-ce|g" /etc/yum.repos.d/docker-ce.repo
 
-sudo yum -y install docker-ce docker-ce-cli containerd.io
+sudo yum -y install docker-ce-{{ param_docker_version }} docker-ce-cli-{{ param_docker_version }} containerd.io docker-buildx-plugin
 sudo systemctl daemon-reload && sudo systemctl restart docker && sudo systemctl enable docker
