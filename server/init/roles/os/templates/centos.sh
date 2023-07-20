bbd46ff2c3d30ee4e9b5f9c4a65c972fb81bcd82
Bruce CHEN <805899926@qq.com>
2023-07-20 08:32:42 +0800

    #

--- server/init/roles/os/templates/centos.sh
+++ server/init/roles/os/templates/centos.sh
@@ -10,12 +10,12 @@ dnf -y install python39
 
 useradd -m {{ param_user_ops }} && usermod -aG wheel {{ param_user_ops }} && passwd -d {{ param_user_ops }} && chown {{ param_user_ops }}:{{ param_user_ops }} /home/{{ param_user_ops }}
 
-public_key="set -e;cd;mkdir -p .ssh;chmod 700 .ssh;echo {{ param_user_ops_opsor_ssh_public_key }} > .ssh/authorized_keys;chmod 644 .ssh/authorized_keys"
+public_key="set -e;cd;mkdir -p .ssh;chmod 700 .ssh;echo {{ param_user_ops_ssh_public_key }} > .ssh/authorized_keys;chmod 644 .ssh/authorized_keys"
 sudo -u root bash -c "${public_key}"
 sudo -u {{ param_user_ops }} bash -c "${public_key}"
 
 systemctl stop firewalld && systemctl disable firewalld
 sed -i -r "s/SELINUX=enforcing/SELINUX=disabled/" /etc/selinux/config
 
-# fix bug: Missing privilege separation directory: /run/sshd
+# fix: Missing privilege separation directory: /run/sshd
 echo 'd /var/run/sshd 0755 root' > /usr/lib/tmpfiles.d/sshd.conf
