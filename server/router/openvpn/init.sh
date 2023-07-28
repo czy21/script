c8b045fa2bad0eaa2115963a6ca55dd81c9c2c04
Bruce CHEN <805899926@qq.com>
2023-07-28 23:40:23 +0800

    # add homepage

--- server/router/openvpn/init.sh
+++ server/router/openvpn/init.sh
@@ -7,8 +7,10 @@ fi
 
 if [ "backup" == "${param_command}" ];then
   find {{ param_openvpn_etc_path }} -type f -regex '.*\.\(auth\|ovpn\)' -exec sh -c 'f={}; cp $f {{ param_role_temp_path }}/$(basename $f).bak' \;
+  find {{ param_openvpn_etc_path }} -type d -regex '.*_\(ccd\)' -exec sh -c 'd={}; cp -r $d {{ param_role_temp_path }}' \;
 fi
 
 if [ "restore" == "${param_command}" ];then
   find {{ param_role_temp_path }} -type f -regex '.*\.\(auth\|ovpn\).bak' -exec sh -c 'f={}; cp $f {{ param_openvpn_etc_path }}/$(basename $f .bak)' \;
+  find {{ param_role_temp_path }} -type d -regex '.*_\(ccd\)' -exec sh -c 'd={}; cp -r $d {{ param_openvpn_etc_path }}' \;
 fi
