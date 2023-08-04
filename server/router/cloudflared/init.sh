f499cf820746c619c365a78e2d5a51b7833057d4
Bruce CHEN <805899926@qq.com>
2023-08-04 12:24:35 +0800

    # add cloudflare for openwrt

---
+++ server/router/cloudflared/init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+param_command="{{ param_command }}"
+etc_app_path="{{ param_app_path }}"
+if [ "install" == "${param_command}" ];then
+  echo ''
+fi
+
+if [ "backup" == "${param_command}" ];then
+  find ${etc_app_path}/ -maxdepth 1 ! -path ${etc_app_path}/ -exec cp -r {} {{ param_role_bak_path }} \;
+fi
+
+if [ "restore" == "${param_command}" ];then
+  cp -r {{ param_role_bak_path }}/* ${etc_app_path}
+fi
