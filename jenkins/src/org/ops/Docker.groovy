a28ffe46cf56646678e97efa022b89bf0515f6e2
Bruce CHEN <805899926@qq.com>
2023-08-07 22:01:55 +0800

    #

--- jenkins/src/org/ops/Docker.groovy
+++ jenkins/src/org/ops/Docker.groovy
@@ -5,11 +5,11 @@ import org.ops.util.PathUtils
 def build() {
     configFileProvider([configFile(fileId: "docker.config", targetLocation: '.jenkins/docker/config.json')]) {
         env.DOCKER_HOME = "${tool 'docker'}"
-        docker_cli = "${DOCKER_HOME}/bin/docker"
+        env.param_docker_cli = "${DOCKER_HOME}/bin/docker"
         docker_config_dir = PathUtils.ofPath("${env.WORKSPACE}", ".jenkins/docker/")
         docker_image_tag = "${env.param_release_image}:${env.param_release_version}"
-        docker_build_cmd = "sudo ${docker_cli} build --tag ${docker_image_tag} --file ${env.param_docker_file} ${env.param_docker_context} --pull"
-        docker_push_cmd = "sudo ${docker_cli} --config ${docker_config_dir} push ${docker_image_tag}"
+        docker_build_cmd = "sudo ${param_docker_cli} build --tag ${docker_image_tag} --file ${env.param_docker_file} ${env.param_docker_context} --pull"
+        docker_push_cmd = "sudo ${param_docker_cli} --config ${docker_config_dir} push ${docker_image_tag}"
         sh "${docker_build_cmd} && ${docker_push_cmd}"
     }
 }
@@ -25,6 +25,9 @@ def deploy() {
     ]
     if (deployMap.containsKey(env.param_code_type)) {
         deployMap.get(env.param_code_type).call()
+    }
+    if (fileExists("${env.param_docker_compose_file}")) {
+       sh "cp ${env.param_docker_compose_file} .jenkins/docker-compose.yaml"
     }
     docker_host = "tcp://${env.param_docker_deploy_host}:2375"
     param_file = PathUtils.ofPath("${env.WORKSPACE}", ".jenkins/param.yaml")
