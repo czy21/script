7f514b09285c52d94abafac8ca5eab31ac8dfb0d
Bruce CHEN <805899926@qq.com>
2023-08-19 22:49:13 +0800

    # improve toolchain.sh

--- server/chart/toolchain.sh
+++ server/chart/toolchain.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
 # bash toolchain.sh -h user@host install
-# -h show help
+# --help show help
 # -p param_cluster_name [dev | ops]
 #    param_ks_role [host | member]
 #    param_ks_jwt  str
 # --ignore-namespace
 # --create-namespace
 
-while getopts "h:" opt
-do
-	case $opt in
-		h)
-      host=$OPTARG
-			;;
-	esac
-done
-shift "$((OPTIND-1))"
-
-source ../share.sh
-upload_exec_py $@
+../share.sh $@
--- server/router/toolchain.sh
+++ server/router/toolchain.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
 # bash toolchain.sh -h user@host install
-# -h show help
+# --help show help
 
-while getopts "h:" opt
-do
-	case $opt in
-		h)
-      host=$OPTARG
-			;;
-	esac
-done
-shift "$((OPTIND-1))"
-
-source ../share.sh
-upload_exec_py $@
+../share.sh $@
