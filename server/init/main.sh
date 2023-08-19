df1067233036e037b4c7c298b643c6fcf7912a3c
Bruce CHEN <805899926@qq.com>
2023-08-19 22:56:46 +0800

    #

--- server/init/main.sh
+++ server/init/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# bash toolchain.sh -h user@host -f [inventory_file] -t [tags]
+# bash main.sh -h user@host -f [inventory_file] -t [tags]
 # -f deploy.yml | general.yml
 # -t [tags within inventory_file]
 #    deploy: os | docker | k8s | k8s-master | k8s-worker
