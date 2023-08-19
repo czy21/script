acb14cfad29f24a4142da824fd2b55da6b21348d
Bruce CHEN <805899926@qq.com>
2023-08-19 22:49:13 +0800

    # improve toolchain.sh

--- server/init/toolchain.sh
+++ server/init/toolchain.sh
@@ -6,15 +6,4 @@
 #    deploy: os | docker | k8s | k8s-master | k8s-worker
 #    general: prune | machine
 
-while getopts "h:" opt
-do
-	case $opt in
-		h)
-      host=$OPTARG
-			;;
-	esac
-done
-shift "$((OPTIND-1))"
-
-source ../share.sh
-upload_exec_py $@
+../share.sh $@
