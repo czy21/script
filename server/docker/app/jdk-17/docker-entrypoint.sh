c9cf72548a2e31049978f6e2ea864c864f13210e
Bruce Chen <805899926@qq.com>
2023-09-24 10:23:35 +0800

    # add spring.profiles.active

--- server/docker/app/jdk-17/docker-entrypoint.sh
+++ server/docker/app/jdk-17/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec java ${JAVA_OPTS} -jar ${JAR} --spring.config.additional-location=/app/conf/ ${@}
+exec java ${JAVA_OPTS} -jar ${JAR} --spring.config.additional-location=/app/conf/ --spring.profiles.active=${SPRING_PROFILES_ACTIVE} ${@}
