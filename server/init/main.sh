77ab90680b7d7a8dc4b1c44fff479d6d32af1c77
Bruce Chen <805899926@qq.com>
2023-10-19 15:44:32 +0800

    #

--- server/init/main.sh
+++ server/init/main.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# bash main.sh -h user@host -f [inventory_file] -t [tags]
-# -f deploy.yml | general.yml
+# bash main.sh user@host -f [inventory_file] -t [tags]
 # -t [tags within inventory_file]
 #    deploy: os | docker | k8s | k8s-master | k8s-worker
 #    general: prune | machine
