222b87268e244935677d582c5ddd1f35a87675e3
Bruce Chen <805899926@qq.com>
2023-10-23 15:21:18 +0800

    #

--- server/docker/app/homeassistant/init.sh
+++ server/docker/app/homeassistant/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 set -e
-
-sudo cp -r {{ param_docker_data }}/{{ param_role_name }}/conf/ {{ param_docker_data }}/{{ param_role_name }}/data/
+sudo mkdir -p {{ param_docker_data }}/{{ param_role_name }}/data/
+sudo cp -rv {{ param_docker_data }}/{{ param_role_name }}/conf/* {{ param_docker_data }}/{{ param_role_name }}/data/
