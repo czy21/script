fb20247591d155279de7b92ff5142c0113959dbd
Bruce Chen <805899926@qq.com>
2023-10-27 15:10:59 +0800

    # upgrae k8s to 1.26

--- server/init/roles/kubernetes/templates/k8s-centos.sh
+++ server/init/roles/kubernetes/templates/k8s-centos.sh
@@ -4,13 +4,11 @@ set -e
 sudo cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+baseurl=https://pkgs.k8s.io/core:/stable:/v{{ param_k8s_minor_version }}/rpm/
 enabled=1
-gpgcheck=0
-repo_gpgcheck=0
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-exclude=kubelet kubeadm kubectl
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v{{ param_k8s_minor_version }}/rpm/repodata/repomd.xml.key
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
 EOF
-sudo sed -i.bak -e "s|https://packages.cloud.google.com|http://{{ param_mirror_raw }}/kubernetes|g" /etc/yum.repos.d/kubernetes.repo
-sudo yum install -y kubelet-{{ param_k8s_version }} kubeadm-{{ param_k8s_version }} kubectl-{{ param_k8s_version }} --disableexcludes=kubernetes
+sudo yum install -y kubelet-{{ param_k8s_patch_version }} kubeadm-{{ param_k8s_patch_version }} kubectl-{{ param_k8s_patch_version }} --disableexcludes=kubernetes
 sudo systemctl enable kubelet --now
