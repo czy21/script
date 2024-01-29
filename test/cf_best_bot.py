9d86dcab55090a7a7fd54efe47377f9cae5bb715
Bruce CHEN <805899926@qq.com>
2024-01-29 19:42:10 +0800

    #

---
+++ test/cf_best_bot.py
@@ -0,0 +1,27 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+TOKEN = ''
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(update.message.text)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+
+    echo_handler = MessageHandler(filters.ALL, echo)
+    application.add_handler(echo_handler)
+    application.run_polling()
