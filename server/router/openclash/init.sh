5af74bd9a184b88c68c33e18f77080c634a7cdc4
Bruce CHEN <805899926@qq.com>
2024-02-19 10:54:01 +0800

    # add clash backup

---
+++ server/router/openclash/init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+param_command="{{ param_command }}"
+etc_app_path="{{ param_app_path }}"
+if [ "install" == "${param_command}" ];then
+  echo ''
+fi
+
+if [ "backup" == "${param_command}" ];then
+  find ${etc_app_path}/custom -maxdepth 1 ! -path ${etc_app_path}/custom -name "openclash_custom_rules.list" -exec sh -c 'p={{ param_role_bak_path }}/custom;mkdir -p $p;cp -r {} $p' \;
+fi
+
+if [ "restore" == "${param_command}" ];then
+  cp -r {{ param_role_bak_path }}/custom/* ${etc_app_path}/custom
+fi
