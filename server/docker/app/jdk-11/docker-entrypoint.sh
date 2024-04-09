3bca6f305633744fd91ec614225c49144b9af69b
zhaoyu.chen <805899926@qq.com>
2024-04-09 19:38:12 +0800

    更新 server/docker/app/jdk-11/docker-entrypoint.sh

--- server/docker/app/jdk-11/docker-entrypoint.sh
+++ server/docker/app/jdk-11/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-exec java ${JAVA_OPTS} -jar ${JAR} --spring.config.additional-location=/app/conf/ ${@}
+JMX_ARGS=-Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false
+exec java ${JMX_ARGS} ${JAVA_OPTS} -jar ${JAR} --spring.config.additional-location=/app/conf/ ${@}
