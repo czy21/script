171939d6ae5b2adfb5f5f2ae75bb14055277a1a8
Bruce Chen <805899926@qq.com>
2024-04-11 20:09:32 +0800

    # add jmx port

--- server/docker/app/jdk-17/docker-entrypoint.sh
+++ server/docker/app/jdk-17/docker-entrypoint.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-exec java ${JAVA_OPTS} -jar ${JAR} --spring.config.additional-location=/app/conf/ ${@}
+exec java -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false ${JAVA_OPTS} -jar ${JAR} --spring.config.additional-location=/app/conf/ ${@}
