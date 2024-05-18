5aec7f790903a141d08a82a05d245955b62e7b8d
Bruce CHEN <805899926@qq.com>
2024-05-18 20:12:56 +0800

    #

--- utility/basic.py
+++ utility/basic.py
@@ -44,17 +44,9 @@ def execute(
         encoding="utf-8",
         is_input=True,
         is_return=True,
-        stack_index=1,
         shell=os.name == 'nt',
         dry_run=False
 ):
-    stack = inspect.stack()
-    stack_logs = []
-    if len(stack) - 1 >= stack_index:
-        stack_item = stack[stack_index]
-        stack_logs.append("{0} line: {1} func: {2}".format(pathlib.Path(stack_item.filename).as_posix(), str(stack_item.lineno), stack_item.function))
-    stack_logs.append(re.sub('&&\s+', '&&\n', cmd))
-    logger.info("\n".join(stack_logs))
     if is_input:
         input_exec = str(input("Are you sure you want to execute (y/n)?").strip())
         if input_exec != "y":
