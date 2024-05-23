04e7e9475b866c4f46c0ce8f14adefcf26a32ca1
Bruce Chen <805899926@qq.com>
2024-05-23 20:19:33 +0800

    #

--- server/docker/ssl/self-signed/build.sh
+++ server/docker/ssl/self-signed/build.sh
@@ -19,9 +19,9 @@ conf_file={{ param_role_output_path }}/conf
 openssl_cnf=${conf_file}/openssl.cnf
 openssl_ext=${conf_file}/openssl.ext
 
-openssl genrsa -out ${tmp_generate}/{{ param_ssl_generate_domain }}.key 4096
+openssl genrsa -out ${tmp_generate}/{{ param_ssl_generate_domain }}.key 2048
 openssl req -new -key ${tmp_generate}/{{ param_ssl_generate_domain }}.key -out ${tmp_generate}/{{ param_ssl_generate_domain }}.csr -config ${openssl_cnf} -nodes -subj "/C=CN/ST=SH/L=SH/O=Home/OU=IT/CN={{ param_ssl_generate_domain }}"
-openssl ca -batch -in ${tmp_generate}/{{ param_ssl_generate_domain }}.csr -out ${tmp_generate}/{{ param_ssl_generate_domain }}.crt -cert ${tmp_ca}/ca.crt -keyfile ${tmp_ca}/ca.key -config ${openssl_cnf} -extfile ${openssl_ext}
+openssl ca -batch -notext -in ${tmp_generate}/{{ param_ssl_generate_domain }}.csr -out ${tmp_generate}/{{ param_ssl_generate_domain }}.crt -cert ${tmp_ca}/ca.crt -keyfile ${tmp_ca}/ca.key -config ${openssl_cnf} -extfile ${openssl_ext}
 
 openssl base64 -A -in ${tmp_generate}/{{ param_ssl_generate_domain }}.key -out ${tmp_generate}/{{ param_ssl_generate_domain }}.key.base64
 openssl base64 -A -in ${tmp_generate}/{{ param_ssl_generate_domain }}.crt -out ${tmp_generate}/{{ param_ssl_generate_domain }}.crt.base64
