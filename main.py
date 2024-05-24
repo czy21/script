bf312a544e3231efffa5b52b438de3dceede9b96
Bruce Chen <805899926@qq.com>
2024-05-24 15:15:34 +0800

    #

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import filecmp
 import logging
-import pathlib,json
+import pathlib,json,shutil
 from server import share
 from utility import file as file_util, template as template_util
 
 
 def collect_doc(source_name):
     source_dir = current.joinpath("server/{0}".format(source_name))
-    #share.execute("cd {0} && rm -rf build && sh main.sh {1} build --target doc --env-file env-public.yaml --all-namespace".format(source_dir.as_posix(), "local"))
+    share.execute("cd {0} && rm -rf build && sh main.sh {1} build --target doc --env-file env-public.yaml --all-namespace".format(source_dir.as_posix(), "local"))
     source_build_dir = source_dir.joinpath("build")
     target_dir = doc_public.joinpath(source_name)
+    shutil.rmtree(target_dir)
     namespaces = []
     for s in filter(lambda f: f.is_dir(),source_build_dir.iterdir()):
         roles=[]
@@ -17,9 +18,11 @@ def collect_doc(source_name):
             t: pathlib.Path = target_dir.joinpath("{}/{}.md".format(s.name,sd.parent.parent.parent.name))
             role_name=sd.parent.parent.parent.name
             roles.append({"name": role_name,"file": "{}/{}/{}.md".format(source_name,s.name,role_name)})
-            if not t.exists() or not filecmp.cmp(sd, t):
-                file_util.copy(sd, t)
-        namespaces.append({"namespace":s.name,"roles":roles})
+            file_util.copy(sd, t)
+        roles.sort(key=lambda k: k.get('name'))
+        if roles:
+          namespaces.append({"namespace":s.name,"roles":roles})
+    namespaces.sort(key=lambda k: k.get('namespace'))
     return namespaces
 
 logger = logging.getLogger()
@@ -31,12 +34,12 @@ if __name__ == '__main__':
     doc = current.joinpath("doc")
     doc_public = doc.joinpath("public")
     mkdocs_template = doc.joinpath("mkdocs_template.yaml")
-    docker_docs=collect_doc("docker")
-    chart_docs=collect_doc("chart")
     mkdocs_text = template_util.Template(file_util.read_text(mkdocs_template)).render(
         **{
-            "param_docker_namespaces":docker_docs,
-            "param_chart_namespaces": chart_docs
+            "param_doc_nav": {
+              "Docker": collect_doc("docker"),
+              "Chart": collect_doc("chart"),
+           }  
         }
     )
     file_util.write_text(mkdocs, mkdocs_text)
