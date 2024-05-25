375c4c93d52f88221d991b430c8b78899e72dda4
Bruce CHEN <805899926@qq.com>
2024-05-25 16:16:37 +0800

    #

--- jenkins/src/org/ops/util/PathUtils.groovy
+++ jenkins/src/org/ops/util/PathUtils.groovy
@@ -4,11 +4,11 @@ package org.ops.util
 import java.nio.file.Path
 
 static def ofPath(String first, String... more) {
-    return Path.of(first, more.findAll { StringUtils.isNotEmpty(it) } as String[]).toString()
+    return Path.of(first as String, more.findAll { StringUtils.isNotEmpty(it) } as String[]).toString()
 }
 
 static def relativize(String p1, String p2) {
-    return Path.of(p1).relativize(Path.of(p2)).toString()
+    return Path.of(p1 as String).relativize(Path.of(p2 as String)).toString()
 }
 
 return this
