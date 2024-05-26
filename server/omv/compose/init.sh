21544cb590990938c09b402c74dd49fc4c69ae9a
Bruce CHEN <805899926@qq.com>
2024-05-26 21:24:05 +0800

    # improve omv init

--- server/omv/compose/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo mkdir -p /usr/local/lib/docker/cli-plugins/
-sudo curl -L "https://github.com/docker/compose/releases/download/v{{ param_compose_version }}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/lib/docker/cli-plugins/docker-compose
-sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-sudo ln -sf /usr/local/lib/docker/cli-plugins/docker-compose /usr/bin/docker-compose
