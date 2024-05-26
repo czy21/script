48ff9cb85e6f1190d427ed91f8dd77d44172079e
Bruce CHEN <805899926@qq.com>
2024-05-26 22:56:27 +0800

    #

--- server/init/roles/docker/templates/rocky.sh
+++ server/init/roles/docker/templates/rocky.sh
@@ -5,8 +5,9 @@ set -e
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-# override docker repo available mirror:https://mirrors.tuna.tsinghua.edu.cn
-sudo sed -i.bak -e "s|https://download.docker.com|http://{{ param_mirror_raw }}/docker-ce|g" /etc/yum.repos.d/docker-ce.repo
+if [ "{{ param_mirror_use_proxy | lower }}" = true ];then
+  sudo sed -i.bak -e "s|https://download.docker.com|http://{{ param_mirror_raw }}/docker-ce|g" /etc/yum.repos.d/docker-ce.repo
+fi
 
 sudo yum -y install docker-ce-{{ param_docker_version }} docker-ce-cli-{{ param_docker_version }} containerd.io docker-buildx-plugin
 sudo systemctl daemon-reload && sudo systemctl restart docker && sudo systemctl enable docker
