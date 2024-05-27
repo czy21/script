53d0d878eb8405907616ecb19ac0a3dd256241f1
Bruce Chen <805899926@qq.com>
2024-05-27 13:35:07 +0800

    #

--- jenkins/src/org/ops/Docker.groovy
+++ jenkins/src/org/ops/Docker.groovy
@@ -36,7 +36,7 @@ def deploy() {
     docker_host = "tcp://${env.param_docker_deploy_host}:2375"
     param_file = PathUtils.ofPath("${env.WORKSPACE}", ".jenkins/param.yaml")
     docker_compose_file = PathUtils.ofPath("${env.WORKSPACE}", ".jenkins/docker-compose.yaml")
-    docker_deploy_cmd = "DOCKER_HOST=${docker_host} docker-compose ls"
+    docker_deploy_cmd = "DOCKER_HOST=${docker_host} docker-compose --project-name ${env.param_release_name} --file ${docker_compose_file} --env-file ${param_file} up --detach --remove-orphans"
     sh "${docker_deploy_cmd}"
 }
 
