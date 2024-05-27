f0a2184b6b18520a6c0797b79e1757b65a0616a3
Bruce Chen <805899926@qq.com>
2024-05-27 15:03:29 +0800

    #

--- jenkins/src/org/ops/Docker.groovy
+++ jenkins/src/org/ops/Docker.groovy
@@ -40,7 +40,7 @@ def deploy() {
         docker_compose_file = PathUtils.ofPath("${env.WORKSPACE}", ".jenkins/docker-compose.yaml")
         cmd="mkdir -p \$HOME/.ssh;echo 'Host *\n  StrictHostKeyChecking no' > \$HOME/.ssh/config;"
         cmd+="eval `ssh-agent`;chmod 600 ${DOCKER_SSH_PRIVATE_KEY};ssh-add ${DOCKER_SSH_PRIVATE_KEY};"
-        cmd+="DOCKER_HOST=ssh://opsor@${env.param_docker_deploy_host} docker-compose --project-name ${env.param_release_name} --file ${docker_compose_file} --env-file ${param_file} up --detach --remove-orphans || error=true"
+        cmd+="DOCKER_HOST=ssh://opsor@${env.param_docker_deploy_host} docker-compose --project-name ${env.param_release_name} --file ${docker_compose_file} --env-file ${param_file} up --detach --remove-orphans || error=true;"
         cmd+="ssh-agent -k;"
         cmd+="[ $error ] && exit -1"
         sh "${cmd}"
