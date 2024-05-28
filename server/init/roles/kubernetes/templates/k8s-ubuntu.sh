ceae616025e235e7832ab15611f781ff7fda18b6
Bruce CHEN <805899926@qq.com>
2024-05-28 23:19:37 +0800

    # fix k8s init

--- server/init/roles/kubernetes/templates/k8s-ubuntu.sh
+++ server/init/roles/kubernetes/templates/k8s-ubuntu.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v{{ param_k8s_minor_version }}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v{{ param_k8s_minor_version }}/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v{{ param_k8s_minor_version }}/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install -y kubelet={{ param_k8s_patch_version }}-00 kubeadm={{ param_k8s_patch_version }}-00 kubectl={{ param_k8s_patch_version }}-00
