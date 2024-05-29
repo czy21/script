a755f2cc29ec08a1a70b4bac2f13a0a9878560a4
Bruce CHEN <805899926@qq.com>
2024-05-29 15:45:44 +0800

    #

--- server/init/roles/kubernetes/templates/k8s-ubuntu.sh
+++ server/init/roles/kubernetes/templates/k8s-ubuntu.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
 
-# install k8s
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v{{ param_k8s_minor_version }}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v{{ param_k8s_minor_version }}/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
 
+PROJECT_PATH=prerelease:/main
+curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/$PROJECT_PATH/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/$PROJECT_PATH/deb/ /" | sudo tee /etc/apt/sources.list.d/cri-o.list
+
 sudo apt-get update
-sudo apt-get install -y kubelet={{ param_k8s_patch_version }}-00 kubeadm={{ param_k8s_patch_version }}-00 kubectl={{ param_k8s_patch_version }}-00
-sudo apt-mark hold kubelet kubeadm kubectl
+sudo apt-get install -y cri-o kubelet={{ param_k8s_patch_version }}-00 kubeadm={{ param_k8s_patch_version }}-00 kubectl={{ param_k8s_patch_version }}-00
+sudo systemctl enable crio kubelet --now
