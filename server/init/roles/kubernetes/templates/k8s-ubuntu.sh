e758dfe06b1bb8c38f5c48ad834aa68de2ea7c73
Bruce CHEN <805899926@qq.com>
2024-05-29 18:06:58 +0800

    # improve

--- server/init/roles/kubernetes/templates/k8s-ubuntu.sh
+++ server/init/roles/kubernetes/templates/k8s-ubuntu.sh
@@ -4,13 +4,14 @@ set -e
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
+# k8s
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v{{ param_k8s_minor_version }}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v{{ param_k8s_minor_version }}/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
 
-PROJECT_PATH=prerelease:/main
-curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/$PROJECT_PATH/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/$PROJECT_PATH/deb/ /" | sudo tee /etc/apt/sources.list.d/cri-o.list
+# cri-o
+curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/stable:/v{{ param_k8s_minor_version }}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/stable:/v{{ param_k8s_minor_version }}/deb/ /" | sudo tee /etc/apt/sources.list.d/cri-o.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install -y cri-o kubelet={{ param_k8s_patch_version }}-00 kubeadm={{ param_k8s_patch_version }}-00 kubectl={{ param_k8s_patch_version }}-00
+sudo apt-get install -y cri-o cri-tools kubelet={{ param_k8s_patch_version }}-00 kubeadm={{ param_k8s_patch_version }}-00 kubectl={{ param_k8s_patch_version }}-00
 sudo systemctl enable crio kubelet --now
