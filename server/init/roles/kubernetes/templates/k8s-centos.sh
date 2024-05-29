38464d9053cd26ad4a659b97df64621edce8d984
Bruce CHEN <805899926@qq.com>
2024-05-29 18:04:29 +0800

    # use stable cri-o

--- server/init/roles/kubernetes/templates/k8s-centos.sh
+++ server/init/roles/kubernetes/templates/k8s-centos.sh
@@ -11,14 +11,13 @@ gpgkey=https://pkgs.k8s.io/core:/stable:/v{{ param_k8s_minor_version }}/rpm/repo
 exclude=kubelet kubeadm kubectl kubernetes-cni
 EOF
 
-PROJECT_PATH=prerelease:/main
 cat <<EOF | sudo tee /etc/yum.repos.d/cri-o.repo
 [cri-o]
 name=CRI-O
-baseurl=https://pkgs.k8s.io/addons:/cri-o:/$PROJECT_PATH/rpm/
+baseurl=https://pkgs.k8s.io/addons:/cri-o:/stable:/v{{ param_k8s_minor_version }}/rpm/
 enabled=1
 gpgcheck=1
-gpgkey=https://pkgs.k8s.io/addons:/cri-o:/$PROJECT_PATH/rpm/repodata/repomd.xml.key
+gpgkey=https://pkgs.k8s.io/addons:/cri-o:/stable:/v{{ param_k8s_minor_version }}/rpm/repodata/repomd.xml.key
 EOF
 
 sudo yum install -y cri-o cri-tools kubelet-{{ param_k8s_patch_version }} kubeadm-{{ param_k8s_patch_version }} kubectl-{{ param_k8s_patch_version }} --disableexcludes=kubernetes
