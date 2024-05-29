bfbe0b32abbecf3b8c8c4af5d3d593d080a5ecb8
Bruce CHEN <805899926@qq.com>
2024-05-29 15:39:33 +0800

    # upgrade k8s to 1.30.1

--- server/init/roles/kubernetes/templates/k8s-centos.sh
+++ server/init/roles/kubernetes/templates/k8s-centos.sh
@@ -8,7 +8,18 @@ baseurl=https://pkgs.k8s.io/core:/stable:/v{{ param_k8s_minor_version }}/rpm/
 enabled=1
 gpgcheck=1
 gpgkey=https://pkgs.k8s.io/core:/stable:/v{{ param_k8s_minor_version }}/rpm/repodata/repomd.xml.key
-exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+exclude=kubelet kubeadm kubectl kubernetes-cni
 EOF
-sudo yum install -y kubelet-{{ param_k8s_patch_version }} kubeadm-{{ param_k8s_patch_version }} kubectl-{{ param_k8s_patch_version }} --disableexcludes=kubernetes
-sudo systemctl enable kubelet --now
+
+PROJECT_PATH=prerelease:/main
+cat <<EOF | sudo tee /etc/yum.repos.d/cri-o.repo
+[cri-o]
+name=CRI-O
+baseurl=https://pkgs.k8s.io/addons:/cri-o:/$PROJECT_PATH/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/addons:/cri-o:/$PROJECT_PATH/rpm/repodata/repomd.xml.key
+EOF
+
+sudo yum install -y cri-o cri-tools kubelet-{{ param_k8s_patch_version }} kubeadm-{{ param_k8s_patch_version }} kubectl-{{ param_k8s_patch_version }} --disableexcludes=kubernetes
+sudo systemctl enable crio kubelet --now
