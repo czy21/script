bc777d6aea1e47b56d9243361651055240542480
Bruce CHEN <805899926@qq.com>
2024-07-18 10:49:35 +0800

    #

--- test/concurrent_test.py
+++ test/concurrent_test.py
@@ -17,14 +17,14 @@ def get_session(sid):
 
 
 if __name__ == '__main__':
+    
+    # start_time = datetime.now()
 
-    start_time = datetime.now()
-
-    p = Pool(multiprocessing.cpu_count() * 2)
-    for f in range(0, 10000):
-        p.apply_async(get_session, args=(str(f + 1),))
-    p.close()
-    p.join()
-    end_time = datetime.now()
-    seconds = (end_time - start_time).seconds
-    print("总耗时: ", seconds)
+    # p = Pool(multiprocessing.cpu_count() * 2)
+    # for f in range(0, 10000):
+    #     p.apply_async(get_session, args=(str(f + 1),))
+    # p.close()
+    # p.join()
+    # end_time = datetime.now()
+    # seconds = (end_time - start_time).seconds
+    # print("总耗时: ", seconds)
