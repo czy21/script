a89ff838848eadde24a62adaeeaed1a0d71fa252
Bruce CHEN <805899926@qq.com>
2024-08-12 16:38:11 +0800

    # improve

--- server/router/watchcat/init.sh
+++ server/router/watchcat/init.sh
@@ -4,6 +4,7 @@ param_command="{{ param_command }}"
 etc_app_path="{{ param_app_path }}"
 if [ "install" = "${param_command}" ];then
   mkdir -p ${etc_app_path} && cp -r {{ param_role_output_path }}/conf/* ${etc_app_path}
+  chmod +x ${etc_app_path}/*.sh
 fi
 
 if [ "backup" = "${param_command}" ];then
@@ -12,4 +13,5 @@ fi
 
 if [ "restore" = "${param_command}" ];then
   mkdir -p ${etc_app_path} && cp -r {{ param_role_bak_path }}/* ${etc_app_path}
+  chmod +x ${etc_app_path}/*.sh
 fi
