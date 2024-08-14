2372ea266f48800e27db18e2a83672f37b9da38f
Bruce Chen <805899926@qq.com>
2024-08-14 18:52:28 +0800

    #

--- server/tls/docker/build.sh
+++ server/tls/docker/build.sh
@@ -4,13 +4,13 @@ tmp_ca={{ param_role_temp_path }}/ca
 
 # server
 for t in {{ param_docker_host_ips | join (" ")}};do
-  pem_dir={{ param_role_temp_path }}/$t
-  mkdir -p $pem_dir
+  t_dir={{ param_role_temp_path }}/$t
+  mkdir -p $t_dir
 
-  openssl genrsa -out $pem_dir/server.key 4096
-  openssl req -subj "/CN=server" -new -key $pem_dir/server.key -out $pem_dir/server.csr
-  echo "subjectAltName = IP:$t,IP:127.0.0.1\nextendedKeyUsage = serverAuth" > $pem_dir/extfile.cnf
-  openssl x509 -req -days {{ param_docker_tls_days }} -in $pem_dir/server.csr -CA $tmp_ca/ca.crt -CAkey $tmp_ca/ca.key -CAcreateserial -out $pem_dir/server.crt -extfile $pem_dir/extfile.cnf
+  openssl genrsa -out $t_dir/server.key 4096
+  openssl req -subj "/CN=server" -new -key $t_dir/server.key -out $t_dir/server.csr
+  echo "subjectAltName = IP:$t,IP:127.0.0.1\nextendedKeyUsage = serverAuth" > $t_dir/extfile.cnf
+  openssl x509 -req -days {{ param_docker_tls_days }} -in $t_dir/server.csr -CA $tmp_ca/ca.crt -CAkey $tmp_ca/ca.key -CAcreateserial -out $t_dir/server.crt -extfile $t_dir/extfile.cnf
 done
 
 # client
