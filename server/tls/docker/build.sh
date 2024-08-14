f34e7fd5d57494a74cc9b13df29e7a31520f697f
Bruce CHEN <805899926@qq.com>
2024-08-14 17:34:42 +0800

    #  improve docker tls auth

--- server/tls/docker/build.sh
+++ server/tls/docker/build.sh
@@ -7,20 +7,16 @@ for t in {{ param_docker_host_ips | join (" ")}};do
   pem_dir={{ param_role_temp_path }}/$t
   mkdir -p $pem_dir
 
-  openssl genrsa -out $pem_dir/server-key.pem 4096
-  openssl req -subj "/CN=server" -sha256 -new -key $pem_dir/server-key.pem -out $pem_dir/server.csr
+  openssl genrsa -out $pem_dir/server.key 4096
+  openssl req -subj "/CN=server" -new -key $pem_dir/server.key -out $pem_dir/server.csr
   echo "subjectAltName = IP:$t,IP:127.0.0.1\nextendedKeyUsage = serverAuth" > $pem_dir/extfile.cnf
-  openssl x509 -req -days {{ param_docker_tls_days }} -sha256 -passin pass:{{ param_docker_tls_ca_password }} \
-    -in $pem_dir/server.csr -CA $tmp_ca/ca.pem -CAkey $tmp_ca/ca-key.pem -CAcreateserial \
-    -out $pem_dir/server-cert.pem -extfile $pem_dir/extfile.cnf
+  openssl x509 -req -days {{ param_docker_tls_days }} -in $pem_dir/server.csr -CA $tmp_ca/ca.crt -CAkey $tmp_ca/ca.key -CAcreateserial -out $pem_dir/server.crt -extfile $pem_dir/extfile.cnf
 done
 
 # client
 client_dir={{ param_role_temp_path }}/client
 mkdir -p $client_dir
-openssl genrsa -out $client_dir/client-key.pem 4096
-openssl req -subj '/CN=client' -new -key $client_dir/client-key.pem -out $client_dir/client.csr
+openssl genrsa -out $client_dir/client.key 4096
+openssl req -subj '/CN=client' -new -key $client_dir/client.key -out $client_dir/client.csr
 echo "extendedKeyUsage = clientAuth" > $client_dir/extfile.cnf
-openssl x509 -req -days {{ param_docker_tls_days }} -sha256 -passin pass:{{ param_docker_tls_ca_password }} \
-  -in $client_dir/client.csr -CA $tmp_ca/ca.pem -CAkey $tmp_ca/ca-key.pem -CAcreateserial \
-  -out $client_dir/client-cert.pem -extfile $client_dir/extfile.cnf
+openssl x509 -req -days {{ param_docker_tls_days }} -in $client_dir/client.csr -CA $tmp_ca/ca.crt -CAkey $tmp_ca/ca.key -CAcreateserial -out $client_dir/client.crt -extfile $client_dir/extfile.cnf
