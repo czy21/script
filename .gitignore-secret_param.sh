1df2ca2a1a9215f62119e6a3ae7750918589c1d7
Bruce CHEN <805899926@qq.com>
2024-08-27 12:31:46 +0800

    #

--- .gitignore-secret_param.sh
+++ .gitignore-secret_param.sh
@@ -1,2 +1,3 @@
 
-sed -e "s|^\(param_mail_smtp_password:\)\(.*\)|\1|" -e "s|^\(param_vaultwarden_admin_token:\)\(.*\)|\1|" $1
+sed -e "s|^\(param_mail_smtp_password:\)\(.*\)|\1|" \
+    -e "s|^\(param_vaultwarden_admin_token:\)\(.*\)|\1|" $1
