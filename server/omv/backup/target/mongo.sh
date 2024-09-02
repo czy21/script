e86c8b38ef261604e6b0b4514aa9517d575f4edc
Bruce Chen <805899926@qq.com>
2024-09-02 20:03:23 +0800

    #

--- server/omv/backup/target/mongo.sh
+++ server/omv/backup/target/mongo.sh
@@ -5,7 +5,7 @@
 DB_USERNAME={{ param_db_mongo_username }}
 DB_PASSWORD={{ param_db_mongo_password }}
 
-databases=$(docker exec mongo mongo mongodb://$DB_USERNAME:$DB_PASSWORD@127.0.0.1:27017/?authSource=admin --eval 'db.adminCommand({ listDatabases: 1, nameOnly: true,filter: {"name":{$nin:["admin","config","local"]}}})' --quiet | xargs)
+databases=$(docker exec mongo mongo mongodb://$DB_USERNAME:$DB_PASSWORD@127.0.0.1:27017/?authSource=admin --eval 'db.adminCommand({ listDatabases: 1,filter: {"name":{$nin:["admin","config","local"]}}}).databases.map(t=>t.name).join(" ")' --quiet | xargs)
 
 DB_BACKUP_DIR=/volume2/backup/mongo
 
@@ -13,6 +13,7 @@ mkdir -p $DB_BACKUP_DIR
 
 for t in $databases;do
   echo "mongo backuping $t"
+  docker exec mongo mongodump --uri=mongodb://$DB_USERNAME:$DB_PASSWORD@127.0.0.1:27017/?authSource=admin --db=$t --archive --gzip > $DB_BACKUP_DIR/$t.gz
 done
 
-# scp -r $DB_BACKUP_DIR dsm:/volume1/public/backup/
+scp -r $DB_BACKUP_DIR dsm:/volume1/public/backup/
