284fa7d99fd94f1d10654c0e65cb90cd1f519cc1
Bruce CHEN <805899926@qq.com>
2024-09-02 16:34:32 +0800

    #

--- server/omv/backup/target/mysql.sh
+++ server/omv/backup/target/mysql.sh
@@ -3,7 +3,7 @@
 DB_USERNAME={{ param_db_mysql_username }}
 DB_PASSWORD={{ param_db_mysql_password }}
 
-databases=$(docker exec mysql mysql --default-character-set=utf8mb4 -u$DB_USERNAME -p$DB_PASSWORD --skip-column-names -s -e 'SHOW DATABASES WHERE `Database` NOT IN ("mysql", "performance_schema","information_schema","sys","elu")' | xargs)
+databases=$(docker exec mysql mysql --default-character-set=utf8mb4 -u$DB_USERNAME -p$DB_PASSWORD --skip-column-names -s -e 'SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME NOT IN ("mysql", "performance_schema","information_schema","sys","elu") ORDER BY `SCHEMA_NAME` ASC' | xargs)
 
 DB_BACKUP_DIR=/volume2/backup/mysql
 
--- server/omv/backup/target/pgsql.sh
+++ server/omv/backup/target/pgsql.sh
@@ -3,7 +3,7 @@
 DB_USERNAME={{ param_db_pgsql_username }}
 DB_PASSWORD={{ param_db_pgsql_password }}
 
-databases=$(docker exec -e PGPASSWORD=$DB_PASSWORD pgsql psql --username=$DB_USERNAME -c "select datname from pg_database where datname not in ('postgres') and datname not like 'template%'" --pset=tuples_only=on | xargs)
+databases=$(docker exec -e PGPASSWORD=$DB_PASSWORD pgsql psql --username=$DB_USERNAME -c "select datname from pg_database where datname not in ('postgres') and datname not like 'template%' order by datname asc" --pset=tuples_only=on | xargs)
 
 DB_BACKUP_DIR=/volume2/backup/pgsql
 
