7cb708d97d325cf263e0f6b2735d014f5dd61715
Bruce CHEN <805899926@qq.com>
2024-09-03 10:01:09 +0800

    #

--- server/omv/backup/target/mongo.sh
+++ server/omv/backup/target/mongo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# https://learn.microsoft.com/zh-cn/sql/linux/sql-server-linux-backup-and-restore-database?view=sql-server-ver16
+# https://www.mongodb.com/zh-cn/docs/database-tools/mongodump/mongodump-examples/
 
 DB_USERNAME={{ param_db_mongo_username }}
 DB_PASSWORD={{ param_db_mongo_password }}
