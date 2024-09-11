e227d13c6e0177e99b9af0c09689b2ba6966f334
Bruce CHEN <805899926@qq.com>
2024-09-11 15:54:39 +0800

    # improve docker rmi cmd

--- jenkins/src/org/ops/Docker.groovy
+++ jenkins/src/org/ops/Docker.groovy
@@ -12,7 +12,7 @@ def build() {
             env.param_docker_build_args.split(",").each { t -> docker_build_cmd += " --build-arg $t" }
         }
         docker_push_cmd = "docker --config ${docker_config_dir} push ${docker_image_tag}"
-        docker_rmi_cmd = "docker rmi ${env.param_release_image} || true"
+        docker_rmi_cmd = "docker rmi ${docker_image_tag} && docker rmi \$(docker image ls --filter reference=${env.param_release_image} --filter dangling=true --format {{.ID}}) || true"
         sh "${docker_build_cmd} && ${docker_push_cmd} && ${docker_rmi_cmd}"
     }
 }
