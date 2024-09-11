9ab986e37cdcd29561a409d1847fdaeeac988851
Bruce CHEN <805899926@qq.com>
2024-09-11 16:10:29 +0800

    #

--- jenkins/src/org/ops/Docker.groovy
+++ jenkins/src/org/ops/Docker.groovy
@@ -13,9 +13,7 @@ def build() {
         }
         docker_push_cmd = "docker --config ${docker_config_dir} push ${docker_image_tag}"
         docker_rmi_cmd = "docker rmi ${docker_image_tag}"
-        docker_rmi_none_dangling = "image_ids=\$(docker image ls --filter \"reference=${env.param_release_image}\" --filter dangling=true --format \"{{.ID}}\" | xargs);[ ! -z \"\$image_ids\" ] && docker rmi \$image_ids || true"
         sh "${docker_build_cmd} && ${docker_push_cmd} && ${docker_rmi_cmd}"
-        sh "${docker_rmi_none_dangling}"
     }
 }
 
