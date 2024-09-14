6a1b9661c58c51f3ddd3c45e6eea9d897d3b1ba9
Bruce CHEN <805899926@qq.com>
2024-09-14 15:18:53 +0800

    # improve os role

--- server/init/roles/os/templates/redhat.sh
+++ server/init/roles/os/templates/redhat.sh
@@ -8,7 +8,7 @@ yum clean all && yum --refresh makecache -v
 yum -y install tar wget vim nfs-utils bash-completion git jq rsync nc net-tools
 [ "centos" = "{{ param_ansible_distribution }}" ] && dnf -y install python39
 
-useradd -m {{ param_user_ops }} && usermod -aG wheel {{ param_user_ops }} && passwd -d {{ param_user_ops }} && chown {{ param_user_ops }}:{{ param_user_ops }} /home/{{ param_user_ops }}
+grep '{{ param_user_ops }}' /etc/passwd -q || useradd -m {{ param_user_ops }} && usermod -aG wheel {{ param_user_ops }} && passwd -d {{ param_user_ops }} && chown {{ param_user_ops }}:{{ param_user_ops }} /home/{{ param_user_ops }}
 
 public_key="set -e;cd;mkdir -p .ssh;chmod 700 .ssh;echo {{ param_user_ops_ssh_public_key }} > .ssh/authorized_keys;chmod 644 .ssh/authorized_keys"
 sudo -u root bash -c "${public_key}"
@@ -23,4 +23,4 @@ sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 echo 'd /var/run/sshd 0755 root' > /usr/lib/tmpfiles.d/sshd.conf
 
 # fedora disable swap
-[ "${os_distribution}" == "fedora" ] && sudo systemctl mask systemd-zram-setup@zram0
+[ "fedora" = "${os_distribution}" ] && systemctl mask systemd-zram-setup@zram0
