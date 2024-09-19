8c431eb0a0507b9504e94bccb20b795d0eee1acf
Bruce CHEN <805899926@qq.com>
2024-09-19 15:46:34 +0800

    #  improve backup db shell

--- server/omv/backup/target/db.sh
+++ server/omv/backup/target/db.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 CURRENT_DIR=$(cd "$(dirname "$0")"; pwd)
 
-sh $CURRENT_DIR/mysql.sh
-sh $CURRENT_DIR/pgsql.sh
-sh $CURRENT_DIR/mssql.sh
-sh $CURRENT_DIR/mongo.sh
+DB_BACKUP_DIR=/volume2/backup/db
+
+sh $CURRENT_DIR/mysql.sh "$DB_BACKUP_DIR"
+sh $CURRENT_DIR/pgsql.sh "$DB_BACKUP_DIR"
+sh $CURRENT_DIR/mssql.sh "$DB_BACKUP_DIR"
+sh $CURRENT_DIR/mongo.sh "$DB_BACKUP_DIR"
+
+scp -r $DB_BACKUP_DIR dsm:/volume1/public/backup/
--- server/omv/backup/target/mongo.sh
+++ server/omv/backup/target/mongo.sh
@@ -7,7 +7,7 @@ DB_PASSWORD={{ param_db_mongo_password }}
 
 databases=$(docker exec mongo mongo mongodb://$DB_USERNAME:$DB_PASSWORD@127.0.0.1:27017/?authSource=admin --eval 'db.adminCommand({ listDatabases: 1,filter: {"name":{$nin:["admin","config","local"]}}}).databases.map(t=>t.name).join(" ")' --quiet | xargs)
 
-DB_BACKUP_DIR=/volume2/backup/mongo
+DB_BACKUP_DIR=$1/mongo
 
 mkdir -p $DB_BACKUP_DIR
 
@@ -15,5 +15,3 @@ for t in $databases;do
   echo "mongo backuping $t"
   docker exec mongo mongodump --uri=mongodb://$DB_USERNAME:$DB_PASSWORD@127.0.0.1:27017/?authSource=admin --db=$t --archive --gzip > $DB_BACKUP_DIR/$t.gz
 done
-
-scp -r $DB_BACKUP_DIR dsm:/volume1/public/backup/
