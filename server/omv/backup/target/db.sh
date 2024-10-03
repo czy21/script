a53ec2ad88733cbb349204444075667df3845fe8
Bruce Chen <805899926@qq.com>
2024-10-03 13:39:54 +0800

    #

--- server/omv/backup/target/db.sh
+++ server/omv/backup/target/db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CURRENT_DIR=$(cd "$(dirname "$0")"; pwd)
 
-DB_BACKUP_DIR=/volume2/backup/db
+DB_BACKUP_DIR=/volume1/backup/db
 
 sh $CURRENT_DIR/mysql.sh "$DB_BACKUP_DIR"
 sh $CURRENT_DIR/pgsql.sh "$DB_BACKUP_DIR"
