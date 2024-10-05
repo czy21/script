969521895eaafeeffe7a2a83e9d3fb1f8e1f8d1a
Bruce Chen <805899926@qq.com>
2024-10-05 22:10:32 +0800

    #

--- server/omv/backup/target/db.sh
+++ server/omv/backup/target/db.sh
@@ -7,5 +7,3 @@ sh $CURRENT_DIR/mysql.sh "$DB_BACKUP_DIR"
 sh $CURRENT_DIR/pgsql.sh "$DB_BACKUP_DIR"
 sh $CURRENT_DIR/mssql.sh "$DB_BACKUP_DIR"
 sh $CURRENT_DIR/mongo.sh "$DB_BACKUP_DIR"
-
-scp -r $DB_BACKUP_DIR dsm:/volume1/public/backup/
