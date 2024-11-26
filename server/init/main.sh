286516ac4abffb1de02d62613858ff299187eb84
Bruce CHEN <805899926@qq.com>
2024-11-26 14:13:56 +0800

    # adjust k3s

--- server/init/main.sh
+++ server/init/main.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# bash main.sh user@host -f [inventory_file] -t [tags]
+# bash main.sh user@host 
+# --ansible-host [ansible host file]
+# -f [inventory_file]
 # -t [tags within inventory_file]
 #    deploy: os | docker | k8s | k8s-master | k8s-worker
 #    general: prune | machine
