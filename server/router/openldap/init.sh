7bdc77f6d19c1dd6a9a3d7efbeb0a8f277f4c541
Bruce CHEN <805899926@qq.com>
2024-12-19 17:52:38 +0800

    # adjust ldap init

--- server/router/openldap/init.sh
+++ server/router/openldap/init.sh
@@ -9,13 +9,18 @@ domain_ldif_bak_file="{{ param_role_bak_path }}/domain.ldif"
 config_ldif_etc_file="${ldap_etc_path}/slapd.d/cn=config/olcDatabase={0}config.ldif"
 mdb_ldif_etc_file="${ldap_etc_path}/slapd.d/cn=config/olcDatabase={1}mdb.ldif"
 mkdir -p {{ param_ldap_data }}/ ${ldap_etc_path}/slapd.d/
+
+cp -rv {{ param_role_output_path }}/conf/ldap.init /etc/init.d/ldap
+
+/etc/init.d/ldap restart
+
 if [ "install" = "${param_command}" ];then
   if [ ! -f "${config_ldif_etc_file}" ]; then
       slaptest -f ${ldap_etc_path}/slapd.conf -F ${ldap_etc_path}/slapd.d
   fi
   sed -i 's|^olcAccess.*|olcAccess: {0}to * by dn.exact=gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth manage by * none|g' ${config_ldif_etc_file}
   sed -i 's|^olcDbDirectory.*|olcDbDirectory: {{ param_ldap_data }}|g' ${mdb_ldif_etc_file}
-  /etc/init.d/openldap restart
+  /etc/init.d/ldap restart
   ldapadd -Y EXTERNAL -H ldapi:/// -f ${config_ldif_file}
   ldapadd -x -D cn=admin,dc="{{ param_ldap_domain }}",dc=com -W -f ${domain_ldif_file}
   find ${ldap_etc_path}/schema/ -regex '.*\(cosine\|nis\|inetorgperson\|log\).ldif' -exec ldapadd -Y EXTERNAL -H ldapi:/// -f {} \;
@@ -27,9 +32,9 @@ if [ "backup" = "${param_command}" ];then
 fi
 
 if [ "restore" = "${param_command}" ];then
-  /etc/init.d/openldap stop
+  /etc/init.d/ldap stop
   rm -rf {{ param_ldap_data }}/* ${ldap_etc_path}/slapd.d/*
   slapadd -F ${ldap_etc_path}/slapd.d -b cn=config -l ${config_ldif_bak_file}
   slapadd -F ${ldap_etc_path}/slapd.d -b dc="{{ param_ldap_domain }}",dc=com -l ${domain_ldif_bak_file}
-  /etc/init.d/openldap start
+  /etc/init.d/ldap start
 fi
