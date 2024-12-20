ed57934a416581aa79d19f4f3f523e399878db30
Bruce CHEN <805899926@qq.com>
2024-12-20 13:12:08 +0800

    # adjust podman playbook

--- server/init/roles/podman/templates/debian.sh
+++ server/init/roles/podman/templates/debian.sh
@@ -2,6 +2,4 @@
 set -e
 
 sudo apt-get update -y
-podman_version=`sudo apt-cache madison podman | awk '{ print $3 }' | grep "{{ param_podman_version }}" | head -n 1`
-podman_compose_version=`sudo apt-cache madison podman-compose | awk '{ print $3 }' | grep "{{ param_podman_compose_version }}" | head -n 1`
-sudo apt-get -y install podman=${podman_version} podman-compose=${podman_compose_version} 
+sudo apt-get -y install podman
