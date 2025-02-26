dd8dc30096d4ac0b6e5f0a556fe3e84c635f16ea
Bruce Chen <805899926@qq.com>
2025-02-26 09:53:03 +0800

    #

--- server/omv/script/conf/backup/db-mysql.sh
+++ server/omv/script/conf/backup/db-mysql.sh
@@ -13,3 +13,11 @@ for t in $databases;do
   echo "mysql backuping $t"
   docker exec mysql mysqldump --default-character-set=utf8mb4 -u$DB_USERNAME -p$DB_PASSWORD --databases $t | gzip > $DB_BACKUP_DIR/$t.gz
 done
+
+# route db
+databases=$(ssh openwrt-hrb "docker exec mysql mysql --default-character-set=utf8mb4 -u$DB_USERNAME -p$DB_PASSWORD --skip-column-names -s -e 'SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME NOT IN (\"mysql\", \"performance_schema\",\"information_schema\",\"sys\",\"elu\") ORDER BY \`SCHEMA_NAME\` ASC' | xargs")
+
+for t in $databases;do
+  echo "mysql backuping $t"
+  ssh openwrt-hrb "docker exec mysql mysqldump --default-character-set=utf8mb4 -u$DB_USERNAME -p$DB_PASSWORD --databases $t" | gzip > $DB_BACKUP_DIR/$t.gz
+done
