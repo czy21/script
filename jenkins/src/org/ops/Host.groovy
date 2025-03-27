4e9fc7612ab5fb01fe9330026e436d30fe732db5
Bruce Chen <805899926@qq.com>
2025-03-27 17:08:00 +0800

    # adjust Host.groovy

--- jenkins/src/org/ops/Host.groovy
+++ jenkins/src/org/ops/Host.groovy
@@ -5,8 +5,8 @@ import org.ops.util.PathUtils
 import org.ops.util.StringUtils
 
 def deploy() {
-    def start_api_file = ".jenkins/start-api.sh"
-    def content = libraryResource "org/ops/start-api.sh"
+    def start_api_file = ".jenkins/host-start-api.sh"
+    def content = libraryResource "org/ops/host-start-api.sh"
     writeFile file: start_api_file, text: content, encoding: 'utf-8'
 }
 
