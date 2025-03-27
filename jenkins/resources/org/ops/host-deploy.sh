b1ed1b9ccd084de4a3d97472658e2d74c27be219
Bruce Chen <805899926@qq.com>
2025-03-27 19:59:27 +0800

    #

--- jenkins/resources/org/ops/host-deploy.sh
+++ jenkins/resources/org/ops/host-deploy.sh
@@ -13,19 +13,20 @@ if [ "${code_type}" == "dotnet" ];then
   )
 fi
 
+SSH_HOST="opsor@${param_deploy_host}"
 SSH_ARGS="-o StrictHostKeyChecking=no -i ${SSH_PRIVATE_KEY}"
-pwd
-scp ${SSH_ARGS} .jenkins/host-start-api.sh opsor@${param_deploy_host}:
+
+scp ${SSH_ARGS} .jenkins/host-start-api.sh ${SSH_HOST}:
 
 if [ -f "${archive_file}" ];then
 
-  ssh ${SSH_ARGS} opsor@${param_deploy_host} "mkdir -p /app/${param_release_name}/"
-  scp ${SSH_ARGS} ${archive_file} opsor@${param_deploy_host}:/app/${param_release_name}/
+  ssh ${SSH_ARGS} ${SSH_HOST} "mkdir -p /app/${param_release_name}/"
+  scp ${SSH_ARGS} ${archive_file} ${SSH_HOST}:/app/${param_release_name}/
 
   if [ "${code_type}" == "dotnet" ];then
-    ssh ${SSH_ARGS} -i ${SSH_PRIVATE_KEY} opsor@${param_deploy_host} "cd /app/${param_release_name}/;tar -zxvf ${archive_name};chmod +x api;rm -fv ${archive_name}"
+    ssh ${SSH_ARGS} -i ${SSH_PRIVATE_KEY} ${SSH_HOST} "cd /app/${param_release_name}/;tar -zxvf ${archive_name};chmod +x api;rm -fv ${archive_name}"
   fi
 
-  ssh ${SSH_ARGS} opsor@${param_deploy_host} "bash script/host-start-api.sh -n ${param_release_name} -c ${code_type} -d /app/${param_release_name}"
+  ssh ${SSH_ARGS} ${SSH_HOST} "chmod +x host-start-api.sh && host-start-api.sh -n ${param_release_name} -c ${code_type} -d /app/${param_release_name}"
 
 fi
