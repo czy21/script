af8d19d8c0e76ec73cdcad6d1514a0cacc24c131
Bruce Chen <805899926@qq.com>
2025-03-27 17:15:02 +0800

    #

--- jenkins/src/org/ops/Host.groovy
+++ jenkins/src/org/ops/Host.groovy
@@ -5,9 +5,16 @@ import org.ops.util.PathUtils
 import org.ops.util.StringUtils
 
 def deploy() {
+    
+    def archive_file = ".jenkins/host-archive.sh"
+    def archive_content = libraryResource "org/ops/host-archive.sh"
+    writeFile file: archive_file, text: archive_content, encoding: 'utf-8'
+
     def start_api_file = ".jenkins/host-start-api.sh"
-    def content = libraryResource "org/ops/host-start-api.sh"
-    writeFile file: start_api_file, text: content, encoding: 'utf-8'
+    def start_api_content = libraryResource "org/ops/host-start-api.sh"
+    writeFile file: start_api_file, text: start_api_content, encoding: 'utf-8'
+
+    sh "chmod +x ${archive_file};${archive_file}"
 }
 
 return this
