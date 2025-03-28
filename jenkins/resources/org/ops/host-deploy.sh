cf9ceaf7196cf5ae7697a99efed8fc363c1af00c
Bruce Chen <805899926@qq.com>
2025-03-28 12:40:32 +0800

    #

--- jenkins/resources/org/ops/host-deploy.sh
+++ jenkins/resources/org/ops/host-deploy.sh
@@ -6,7 +6,7 @@ SSH_HOST="${SSH_HOST:-opsor@${param_deploy_host}}"
 SSH_ARGS="-o StrictHostKeyChecking=no"
 
 if [ -n "${SSH_PRIVATE_KEY}" ];then
-  SSH_ARGS+="-i ${SSH_PRIVATE_KEY}"
+  SSH_ARGS+=" -i ${SSH_PRIVATE_KEY}"
 fi
 
 if [ "${param_code_type}" == "dotnet" ];then
