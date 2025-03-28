c5cf9a206a6e0fc1dc956c6d61ac76d676ed34a2
Bruce Chen <805899926@qq.com>
2025-03-28 11:32:53 +0800

    # add param_app_args

--- jenkins/resources/org/ops/host-deploy.sh
+++ jenkins/resources/org/ops/host-deploy.sh
@@ -2,19 +2,16 @@
 
 set -x
 
-project_root=${param_project_root}
-code_type=${param_code_type}
-
 SSH_HOST="opsor@${param_deploy_host}"
 SSH_ARGS="-o StrictHostKeyChecking=no -i ${SSH_PRIVATE_KEY}"
 
 scp ${SSH_ARGS} .jenkins/host-start-api.sh ${SSH_HOST}:
 
-if [ "${code_type}" == "dotnet" ];then
+if [ "${param_code_type}" == "dotnet" ];then
   (
-    cd ${project_root}/build
+    cd ${param_project_root}/build
     tar -zcf - . | ssh ${SSH_ARGS} ${SSH_HOST} "mkdir -p /app/${param_release_name}/ && tar -zxf - -C /app/${param_release_name}/ && ls -al /app/${param_release_name}/ && chmod +x /app/${param_release_name}/api"
   )
 fi
 
-ssh ${SSH_ARGS} ${SSH_HOST} "chmod +x host-start-api.sh && ./host-start-api.sh -n ${param_release_name} -c ${code_type} -d /app/${param_release_name}"
+ssh ${SSH_ARGS} ${SSH_HOST} "chmod +x host-start-api.sh && ./host-start-api.sh -n ${param_release_name} -c ${param_code_type} -d /app/${param_release_name} -p \"${param_app_args}\""
