f35302f2c504254e0333ab2ecb9ac03ee4494d7b
Bruce Chen <805899926@qq.com>
2025-08-01 12:40:34 +0800

    # use dns record

--- server/omv/script/conf/backup/db-pgsql.sh
+++ server/omv/script/conf/backup/db-pgsql.sh
@@ -13,3 +13,11 @@ for t in $databases;do
   echo "pgsql backuping $t"
   docker exec -e PGPASSWORD=$DB_PASSWORD pgsql pg_dump --username=$DB_USERNAME --dbname=$t | gzip > $DB_BACKUP_DIR/$t.gz
 done
+
+# route db
+databases=$(ssh openwrt-hrb "docker exec -e PGPASSWORD=$DB_PASSWORD pgsql psql --username=$DB_USERNAME -c \"select datname from pg_database where datname not in ('postgres') and datname not like 'template%' order by datname asc\" --pset=tuples_only=on | xargs")
+
+for t in $databases;do
+  echo "pgsql backuping $t"
+  ssh openwrt-hrb "docker exec -e PGPASSWORD=$DB_PASSWORD pgsql pg_dump --username=$DB_USERNAME --dbname=$t" | gzip > $DB_BACKUP_DIR/$t.gz
+done
