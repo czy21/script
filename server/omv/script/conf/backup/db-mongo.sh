ca47a1faf8d4d16c96b5978b14c8f63f2e172b80
Bruce Chen <805899926@qq.com>
2025-08-01 18:39:15 +0800

    remove mariadb/deploy.yml migrate nexus h2 to postgresql

--- server/omv/script/conf/backup/db-mongo.sh
+++ server/omv/script/conf/backup/db-mongo.sh
@@ -5,13 +5,23 @@
 DB_USERNAME={{ param_db_mongo_username }}
 DB_PASSWORD={{ param_db_mongo_password }}
 
-databases=$(docker exec mongo mongo mongodb://$DB_USERNAME:$DB_PASSWORD@127.0.0.1:27017/?authSource=admin --eval 'db.adminCommand({ listDatabases: 1,filter: {"name":{$nin:["admin","config","local"]}}}).databases.map(t=>t.name).join(" ")' --quiet | xargs)
+CLI="docker run --rm mongo:4.4.19"
 
 DB_BACKUP_DIR=$1/mongo
 
 mkdir -p $DB_BACKUP_DIR
 
-for t in $databases;do
-  echo "mongo backuping $t"
-  docker exec mongo mongodump --uri=mongodb://$DB_USERNAME:$DB_PASSWORD@127.0.0.1:27017/?authSource=admin --db=$t --archive --gzip > $DB_BACKUP_DIR/$t.gz
-done
+function backup(){
+
+  local host=$1
+  local port=$2
+
+  databases=$(${CLI} mongo mongodb://$DB_USERNAME:$DB_PASSWORD@$host:$port/?authSource=admin --eval 'db.adminCommand({ listDatabases: 1,filter: {"name":{$nin:["admin","config","local"]}}}).databases.map(t=>t.name).join(" ")' --quiet | xargs)
+  for t in $databases;do
+    echo "mongo backuping $host:$port => $t"
+    ${CLI} mongodump --uri=mongodb://$DB_USERNAME:$DB_PASSWORD@$host:$port/?authSource=admin --db=$t --archive --gzip > $DB_BACKUP_DIR/$host-$port-$t.gz
+  done
+
+}
+
+backup db.czy21.com 27017
--- server/omv/script/conf/backup/db-pgsql.sh
+++ server/omv/script/conf/backup/db-pgsql.sh
@@ -3,21 +3,23 @@
 DB_USERNAME={{ param_db_pgsql_username }}
 DB_PASSWORD={{ param_db_pgsql_password }}
 
-databases=$(docker exec -e PGPASSWORD=$DB_PASSWORD pgsql psql --username=$DB_USERNAME -c "select datname from pg_database where datname not in ('postgres') and datname not like 'template%' order by datname asc" --pset=tuples_only=on | xargs)
+CLI="docker run --rm postgres:14.18-alpine"
 
 DB_BACKUP_DIR=$1/pgsql
 
 mkdir -p $DB_BACKUP_DIR
 
-for t in $databases;do
-  echo "pgsql backuping $t"
-  docker exec -e PGPASSWORD=$DB_PASSWORD pgsql pg_dump --username=$DB_USERNAME --dbname=$t | gzip > $DB_BACKUP_DIR/$t.gz
-done
+function backup(){
 
-# route db
-databases=$(ssh openwrt-hrb "docker exec -e PGPASSWORD=$DB_PASSWORD pgsql psql --username=$DB_USERNAME -c \"select datname from pg_database where datname not in ('postgres') and datname not like 'template%' order by datname asc\" --pset=tuples_only=on | xargs")
+  local host=$1
+  local port=$2
 
-for t in $databases;do
-  echo "pgsql backuping $t"
-  ssh openwrt-hrb "docker exec -e PGPASSWORD=$DB_PASSWORD pgsql pg_dump --username=$DB_USERNAME --dbname=$t" | gzip > $DB_BACKUP_DIR/$t.gz
-done
+  databases=$(${CLI} psql postgresql://$DB_USERNAME:$DB_PASSWORD@$host:$port -c "select datname from pg_database where datname not in ('postgres') and datname not like 'template%' order by datname asc" --pset=tuples_only=on | xargs)
+  for t in $databases;do
+    echo "pgsql backuping $host:$port => $t"
+    ${CLI} pg_dump postgresql://$DB_USERNAME:$DB_PASSWORD@$host:$port/$t | gzip > $DB_BACKUP_DIR/$host-$port-$t.gz
+  done
+}
+
+backup db.czy21.com 5432
+backup db02.czy21.com 5432
