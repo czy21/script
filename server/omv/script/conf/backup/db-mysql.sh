b5f8d8ba0ad6dc058eaad1e1501a5278ba1aa46d
Bruce Chen <805899926@qq.com>
2025-08-01 18:39:15 +0800

    remove mariadb/deploy.yml migrate nexus h2 to postgresql

--- server/omv/script/conf/backup/db-mysql.sh
+++ server/omv/script/conf/backup/db-mysql.sh
@@ -3,21 +3,23 @@
 DB_USERNAME={{ param_db_mysql_username }}
 DB_PASSWORD={{ param_db_mysql_password }}
 
-databases=$(docker exec mysql mysql --default-character-set=utf8mb4 -u$DB_USERNAME -p$DB_PASSWORD --skip-column-names -s -e 'SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME NOT IN ("mysql", "performance_schema","information_schema","sys","elu") ORDER BY `SCHEMA_NAME` ASC' | xargs)
+CLI="docker run --rm mysql:8.4.0"
 
 DB_BACKUP_DIR=$1/mysql
 
 mkdir -p $DB_BACKUP_DIR
 
-for t in $databases;do
-  echo "mysql backuping $t"
-  docker exec mysql mysqldump --default-character-set=utf8mb4 -u$DB_USERNAME -p$DB_PASSWORD --databases $t | gzip > $DB_BACKUP_DIR/$t.gz
-done
+function backup(){
 
-# route db
-databases=$(ssh openwrt-hrb "docker exec mysql mysql --default-character-set=utf8mb4 -u$DB_USERNAME -p$DB_PASSWORD --skip-column-names -s -e 'SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME NOT IN (\"mysql\", \"performance_schema\",\"information_schema\",\"sys\",\"elu\") ORDER BY \`SCHEMA_NAME\` ASC' | xargs")
+  local host=$1
+  local port=$2
 
-for t in $databases;do
-  echo "mysql backuping $t"
-  ssh openwrt-hrb "docker exec mysql mysqldump --default-character-set=utf8mb4 -u$DB_USERNAME -p$DB_PASSWORD --databases $t" | gzip > $DB_BACKUP_DIR/$t.gz
-done
+  databases=$(${CLI} mysql --default-character-set=utf8mb4 -h$host -p$port -u$DB_USERNAME -p$DB_PASSWORD --skip-column-names -s -e 'SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME NOT IN ("mysql", "performance_schema","information_schema","sys","elu") ORDER BY `SCHEMA_NAME` ASC' | xargs)
+  for t in $databases;do
+    echo "mysql backuping $host:$port => $t"
+    ${CLI} mysqldump --default-character-set=utf8mb4 -h$host -p$port -u$DB_USERNAME -p$DB_PASSWORD --databases $t | gzip > $DB_BACKUP_DIR/$host-$port-$t.gz
+  done
+}
+
+backup db.czy21.com 3306
+backup db02.czy21.com 3306
