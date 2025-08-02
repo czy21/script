5854f356f818d26d849945cdfb4d2ae823cbf183
Bruce Chen <805899926@qq.com>
2025-08-02 14:03:22 +0800

    # improve backup

--- server/omv/script/conf/backup/db-mssql.sh
+++ server/omv/script/conf/backup/db-mssql.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
 # https://learn.microsoft.com/zh-cn/sql/linux/sql-server-linux-backup-and-restore-database?view=sql-server-ver16
-
 DB_USERNAME={{ param_db_mssql_username }}
 DB_PASSWORD={{ param_db_mssql_password }}
 
-databases=$(docker exec mssql /opt/mssql-tools/bin/sqlcmd -U $DB_USERNAME -P $DB_PASSWORD -Q "set nocount on;select name from sys.databases where name not in ('master','model','msdb','tempdb')" -h -1 | xargs)
+CLI="docker run --rm mssql/server:2022-latest"
 
 DB_BACKUP_DIR=$1/mssql
 
 mkdir -p $DB_BACKUP_DIR
 
-for t in $databases;do
-  echo "mssql backuping $t"
-  docker exec mssql /opt/mssql-tools/bin/sqlcmd -U $DB_USERNAME -P $DB_PASSWORD -Q "BACKUP DATABASE [$t] TO DISK = N'/var/opt/mssql/backup/$t.bak'"
-  docker cp mssql:/var/opt/mssql/backup/$t.bak $DB_BACKUP_DIR
-  docker exec mssql rm -vrf /var/opt/mssql/backup/$t.bak
-done
+function backup(){
+
+  local host=$1
+  local port=$2
+
+  ${CLI} /opt/mssql-tools/bin/sqlcmd -S $host,$port -U $DB_USERNAME -P $DB_PASSWORD -Q "select 1" -h -1
+
+  if [ $? -ne 0 ];then
+    return
+  fi
+
+  databases=$(${CLI} /opt/mssql-tools/bin/sqlcmd -S $host,$port -U $DB_USERNAME -P $DB_PASSWORD -Q "set nocount on;select name from sys.databases where name not in ('master','model','msdb','tempdb')" -h -1 | sed '1,3d' | xargs)
+  
+  for t in $databases;do
+    echo "mssql backup $host:$port => $t"
+    # ${CLI} /opt/mssql-tools/bin/sqlcmd -S $host,$port -U $DB_USERNAME -P $DB_PASSWORD -Q "BACKUP DATABASE [$t] TO DISK = N'/dev/stdout'" | gzip > $DB_BACKUP_DIR/$host-$port-$t.gz
+  done
+}
+
+backup db.czy21.com 1433
