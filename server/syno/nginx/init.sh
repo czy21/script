f6dcbc8e9cd38d09019ce96259d648bb901c4b55
Bruce Chen <805899926@qq.com>
2025-08-07 20:50:33 +0800

    refactor host script

--- server/syno/nginx/init.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo cp -rv {{ param_role_output_path }}/conf/conf.d/* /etc/nginx/conf.d/
--- server/ubun/main.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# bash main.sh user@host install
-# --help show help
-
-../share.sh $@
