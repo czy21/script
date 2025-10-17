93374213f21cbe3db1afcc691d7c7d0e3398361d
Bruce Chen <805899926@qq.com>
2025-10-17 20:09:40 +0800

    add config

--- jenkins/resources/org/ops/server-deploy.sh
+++ jenkins/resources/org/ops/server-deploy.sh
@@ -7,16 +7,24 @@ SSH_HOST="${SSH_HOST:-opsor@${param_server_deploy_host}}"
 SSH_ARGS="-o StrictHostKeyChecking=no"
 NPM_RUN_SCRIPT="${NPM_RUN_SCRIPT:-"start"}"
 
+[ -n "${param_release_name}" ] && return
+
 if [ -n "${SSH_PRIVATE_KEY}" ];then
   SSH_ARGS+=" -i ${SSH_PRIVATE_KEY}"
 fi
 
-APP_CMD="mkdir -p ${APP_DIR}/${param_release_name}/ && tar -zxvf - -C ${APP_DIR}/${param_release_name}/ && chmod 777 ${APP_DIR}/${param_release_name}/"
+APP_CMD=
+
+if [ "${param_code_type}" = "web" ];then
+  APP_CMD+="rm -rf ${APP_DIR}/${param_release_name}/* && "
+fi
+
+APP_CMD+="mkdir -p ${APP_DIR}/${param_release_name}/ && tar -zxvf - -C ${APP_DIR}/${param_release_name}/ && chmod 777 ${APP_DIR}/${param_release_name}/"
 TAR_SRC="${TAR_SRC:-.}"
 
 if [ "${param_code_type}" = "java" ];then
   if [ "$TAR_SRC" = "." ]; then
-    TAR_SRC=api.jar
+    TAR_SRC="api.jar config"
   fi
 fi
 
@@ -28,7 +36,7 @@ if [ "${param_code_type}" = "nodejs" ];then
   APP_CMD+="&& npm --prefix ${APP_DIR}/${param_release_name}/ install"
 fi
 
-(cd ${param_project_root}/${param_project_module}/build && tar -zcf - ${TAR_SRC} | ssh ${SSH_ARGS} ${SSH_HOST} "${APP_CMD}")
+(cd ${param_project_root}/${param_project_module}/build && tar -zcf - ${TAR_SRC} --ignore-failed-read | ssh ${SSH_ARGS} ${SSH_HOST} "${APP_CMD}")
 
 ssh ${SSH_ARGS} ${SSH_HOST} << SCRIPT
 env_file=${APP_DIR}/${param_release_name}/.env
@@ -91,7 +99,9 @@ WantedBy=multi-user.target
 EOF
 fi
 
-sudo systemctl daemon-reload
-sudo systemctl restart ${param_release_name}
-sudo systemctl enable ${param_release_name}
+if [ -f "/etc/systemd/system/${param_release_name}.service" ];then
+  sudo systemctl daemon-reload
+  sudo systemctl restart ${param_release_name}
+  sudo systemctl enable ${param_release_name}
+fi
 SCRIPT
