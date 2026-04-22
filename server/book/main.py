4890c00b86b5287863269d3862daabfec21040f0
Bruce Chen <805899926@qq.com>
2026-04-22 13:37:59 +0800

    remove nginx-exporter

--- server/book/main.py
+++ server/book/main.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     parser.add_argument('-u', '--user', required=False, type=str, help="connect as this user (default=[param_user_ops])")
     parser.add_argument('--no-step', action="store_true", help="disable one-step-at-a-time")
     args = parser.parse_args()
-    args.param = {k: v for t in args.param for (k, v) in t.items()}
+    args.param = dict(args.param)
     if args.debug:
         logger.setLevel(logging.DEBUG)
     if not private_key.exists():
